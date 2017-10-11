2da8e8df68e32060c847a01da79f64fce3f0d66c
John Abraham <john.abraham@fungible.com>
2017-10-11 09:32:44 -0700

    track common workflow

--- fun_test/web/tools/angular_controllers/stats.js
+++ fun_test/web/tools/angular_controllers/stats.js
@@ -115,7 +115,8 @@
             replaceIpDot: '&',
             setCommonWorkFlow: '&',
             setActiveTab: '&',
-            topologySessionId: '<'
+            topologySessionId: '<',
+            commonTrafficWorkFlow: '<'
         }
     });
 
