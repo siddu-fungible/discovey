bb16e4f26551c50d7b5868d5a3f1cfd4795767c9
John Abraham <john.abraham@fungible.com>
2017-11-01 17:47:50 -0700

    add microservices

--- fun_test/stash/docker/startup.sh
+++ fun_test/stash/docker/startup.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
 set -e
 
-startup_log_file="startup_log_file.log"
-funos_log_file="funos.log"
-qemu_log_file="qemu.log"
+funos_url=$1
+action=$2
+resource_name="funos-posix"
+echo "Fetching $resource_name from $funos_url"
 
-log()
-{
-    message=$1
-    echo "STARTUP: $message"
-}
-startup_sleep()
-{
-    sleep_time=$1
-    log "Sleeping $sleep_time"
-    sleep $sleep_time
+curl_output=`curl -w 'httpcode=%{http_code}\n' $funos_url -O`
+if [[ $curl_output == *"httpcode=200"* ]]; then
+    echo "Fetched $resource_name"
+    chmod 777 $resource_name
+else
+    echo "Fetch failed for $funos_url"
+    exit 1
+fi
 
-}
+echo "Starting SSH server"
+/etc/init.d/ssh restart
+echo "Started SSH Server"
 
-##### Start FunOS
-log "Starting FunOS"
-nohup ./funos-posix app=prem_test nvfile=nvfile &> $funos_log_file &
-startup_sleep 60
+echo "Adding QEMU to PATH"
+export PATH=$PATH:./qemu
+
+if [[ $action == *test* ]]; then
+    echo "Testing: dd"
+    dd if=/dev/zero of=nvfile bs=4096 count=256
+    echo "Testing ulimit"
+    ulimit -Sc unlimited
+    echo "mdt_test"
+    ./funos-posix app=mdt_test nvfile=nvfile
+    echo "prem_test"
+    nohup //funos-posix app=prem_test nvfile=nvfile &>/dev/null &
+    sleep 60
+    echo "start_qemu"
+    cd qemu
+    ./qemu-system-x86_64 -L pc-bios -daemonize -machine q35 -m 256  -device nvme-rem-fe,sim_id=0   -redir tcp:2220::22  -drive file=core-image-full-cmdline-qemux86-64.ext4,if=virtio,format=raw -kernel bzImage -append 'root=/dev/vda rw ip=:::255.255.255.0:qemu-yocto:eth0:on mem=256M oprofile.timer=1'
+
+fi
 
 
-##### Start QEMU
-log "List sockets"
-ls /tmp/*sock &> $startup_log_file || true
-log "List funos process"
-ps -ef | grep fun &> $startup_log_file || true
-log "Starting QEMU"
-nohup ./qemu-system-x86_64 ubuntu_min.img -machine q35 -smp 1 -m 2048 -device nvme-rem-fe,sim_id=0 -redir tcp:2220::22 -nographic -enable-kvm &> $qemu_log_file &
-startup_sleep 60
-
-
-##### Shutdown tap interface
-log "Shutting down tap interface, to remove 10.x routes"
-ifconfig tap down &> $funos_log_file || true
-
-
-##### Idle
-log "Idling"
-startup_sleep 3600
+echo "Idling forever"
+while [ 1 ]
+	do
+		sleep 1
+	done
