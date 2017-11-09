d8fc4d8dd0dc4e1752aca413c42a5f4d3d3f2977
George Liang <george.liang@fungible.com>
2017-11-09 15:37:46 -0800

    Add APIs to get FRR RIB/FIB and Linux FIB

--- fun_test/lib/host/frr.py
+++ fun_test/lib/host/frr.py
@@ -3,11 +3,14 @@ from lib.host.linux import Linux
 import re
 
 
+PAT_IP_ADDR = r'(?:\d{1,3}\.){3}\d{1,3}'
+
 PAT_IP_BGP_LOCAL_AS = re.compile(r'local AS number (\d+)')
 PAT_IP_BGP_NEIGH = re.compile(
-    r'((?:\d{1,3}\.){3}\d{1,3})\s+4\s+(\d+).*?(?:(?:\d{2}:){2}\d{2}|[a-zA-Z]+)\s+(\d+|[a-zA-Z]+)')
+    r'(%s)\s+4\s+(\d+).*?(?:(?:\d{2}:){2}\d{2}|[a-zA-Z]+)\s+(\d+|[a-zA-Z]+)' % PAT_IP_ADDR)
 PAT_ISIS_NEIGH = re.compile(r'(node\S+)\s+(\S+)\s+.*1\s+(\w+)')
 PAT_IP_ROUTE_SUM = re.compile(r'(connected|isis|ebgp|ibgp)\s+(\d+)\s+(\d+)')
+PAT_IP_ROUTE = re.compile(r'\* (%s/\d{1,2}|).*?(%s|directly connected),\s+(\S+)' % (PAT_IP_ADDR, PAT_IP_ADDR))
 
 
 class Frr(Linux):
@@ -107,23 +110,125 @@ class Frr(Linux):
                 result.update({protocol: {'Routes': int(rib), 'FIB': int(fib)}})
         return result
 
+    def get_ip_route(self, fib=False):
+        """Do 'show ip route' and get RIB info, excluding inactive routes
+
+        root@node-1-1:~# vtysh -c 'show ip route'
+        Codes: K - kernel route, C - connected, S - static, R - RIP,
+               O - OSPF, I - IS-IS, B - BGP, P - PIM, E - EIGRP, N - NHRP,
+               T - Table, v - VNC, V - VNC-Direct, A - Babel,
+               > - selected route, * - FIB route
+
+        K>* 0.0.0.0/0 [0/0] via 172.17.0.1, eth0
+        C>* 10.0.0.1/32 is directly connected, lo
+        B>* 10.0.0.16/28 [200/0] via 192.168.0.2, p-65, 00:03:45
+        B>* 10.0.0.32/28 [200/0] via 192.168.0.6, p-67, 00:03:45
+        B>* 10.0.0.48/28 [200/0] via 192.168.0.10, p-69, 00:03:45
+        B>* 10.0.1.0/28 [20/0] via 192.96.0.2, p-1, 00:03:44
+          *                    via 192.96.0.6, p-3, 00:03:44
+        B>* 10.0.1.16/28 [20/0] via 192.96.0.2, p-1, 00:03:44
+          *                     via 192.96.0.6, p-3, 00:03:44
+        B>* 10.0.1.32/28 [20/0] via 192.96.0.2, p-1, 00:03:44
+          *                     via 192.96.0.6, p-3, 00:03:44
+        B>* 10.0.1.48/28 [20/0] via 192.96.0.2, p-1, 00:03:44
+          *                     via 192.96.0.6, p-3, 00:03:44
+        B>* 10.0.2.0/28 [20/0] via 192.96.0.2, p-1, 00:03:44
+          *                    via 192.96.0.6, p-3, 00:03:44
+        B>* 10.0.2.16/28 [20/0] via 192.96.0.6, p-3, 00:03:44
+          *                     via 192.96.0.2, p-1, 00:03:44
+        B>* 10.0.2.32/28 [20/0] via 192.96.0.6, p-3, 00:03:44
+          *                     via 192.96.0.2, p-1, 00:03:44
+        B>* 10.0.2.48/28 [20/0] via 192.96.0.6, p-3, 00:03:44
+          *                     via 192.96.0.2, p-1, 00:03:44
+        B>* 10.0.3.0/28 [20/0] via 192.96.0.2, p-1, 00:03:44
+          *                    via 192.96.0.6, p-3, 00:03:44
+        B>* 10.0.3.16/28 [20/0] via 192.96.0.2, p-1, 00:03:44
+          *                     via 192.96.0.6, p-3, 00:03:44
+        B>* 10.0.3.32/28 [20/0] via 192.96.0.2, p-1, 00:03:44
+          *                     via 192.96.0.6, p-3, 00:03:44
+        B>* 10.0.3.48/28 [20/0] via 192.96.0.2, p-1, 00:03:44
+          *                     via 192.96.0.6, p-3, 00:03:44
+        C>* 172.17.0.0/16 is directly connected, eth0
+        C>* 192.96.0.0/30 is directly connected, p-1
+        C>* 192.96.0.4/30 is directly connected, p-3
+        I   192.168.0.0/30 [115/2] via 192.168.0.2, p-65 inactive, 00:03:17
+        C>* 192.168.0.0/30 is directly connected, p-65
+        I   192.168.0.4/30 [115/2] via 192.168.0.6, p-67 inactive, 00:03:16
+        C>* 192.168.0.4/30 is directly connected, p-67
+        I   192.168.0.8/30 [115/2] via 192.168.0.10, p-69 inactive, 00:03:16
+        C>* 192.168.0.8/30 is directly connected, p-69
+        I>* 192.168.0.12/30 [115/2] via 192.168.0.6, p-67, 00:03:16
+          *                         via 192.168.0.2, p-65, 00:03:16
+        I>* 192.168.0.16/30 [115/2] via 192.168.0.2, p-65, 00:03:16
+          *                         via 192.168.0.10, p-69, 00:03:16
+        I>* 192.168.0.20/30 [115/2] via 192.168.0.6, p-67, 00:03:16
+          *                         via 192.168.0.10, p-69, 00:03:16
+        root@node-1-1:~#
+
+
+        :return: dict {'10.0.0.1/32': {'directly connected': 'p-114'},
+                       '10.0.1.0/28': {'192.96.0.2': 'p-1', '192.96.0.6': 'p-3'},
+                        ..}
+        """
+        cmd = 'show ip %s' % ('fib' if fib else 'route')
+        output = self.vtysh_command(cmd)
+        result = {}
+
+        m = PAT_IP_ROUTE.findall(output)
+        if m:
+            for prefix, nhp, interface in m:
+                if prefix:
+                    prefix_cache = prefix
+                else:
+                    prefix = prefix_cache
+                result.setdefault(prefix, {}).update({nhp: interface.rstrip(',')})
+        return result
+
+    def get_ip_fib(self):
+        """Do 'show ip fib' and get FIB info
+
+        root@node-1-1:~# vtysh -c 'show ip fib'
+        Codes: K - kernel route, C - connected, S - static, R - RIP,
+               O - OSPF, I - IS-IS, B - BGP, P - PIM, E - EIGRP, N - NHRP,
+               T - Table, v - VNC, V - VNC-Direct, A - Babel,
+               > - selected route, * - FIB route
+
+        K>* 0.0.0.0/0 [0/0] via 172.17.0.1, eth0
+        C>* 10.0.0.1/32 is directly connected, lo
+        B>* 10.0.0.16/28 [200/0] via 192.168.0.2, p-65, 00:10:49
+        B>* 10.0.0.32/28 [200/0] via 192.168.0.6, p-67, 00:10:49
+        B>* 10.0.0.48/28 [200/0] via 192.168.0.10, p-69, 00:10:49
+        B>* 10.0.1.0/28 [20/0] via 192.96.0.2, p-1, 00:10:48
+          *                    via 192.96.0.6, p-3, 00:10:48
+        ..
+
+        :return: same as self.get_ip_route()
+        """
+        return self.get_ip_route(fib=True)
+
 
 if __name__ == "__main__":
-    frr_obj = Frr(host_ip="1-1",
+    node = '1-1'
+    frr_obj = Frr(host_ip=node,
                   ssh_username="root",
                   ssh_password="fun123",
                   ssh_port=None)
-    output = frr_obj.vtysh_command('show ip bgp sum')
-    fun_test.log("\nOutput:" + output)
-    output = frr_obj.vtysh_command('show isis neighbor')
-    fun_test.log("\nOutput:" + output)
-    output = frr_obj.vtysh_command('show ip route sum')
-    fun_test.log("\nOutput:" + output)
+
+    for cmd in ('show ip bgp sum',
+                'show isis neighbor',
+                'show ip route sum'):
+        output = frr_obj.vtysh_command(cmd)
+        fun_test.log("\nOutput:" + output)
 
     import pprint
-    result = frr_obj.get_ip_bgp_sum()
-    pprint.pprint(result)
-    result = frr_obj.get_isis_neighbor()
-    pprint.pprint(result)
-    result = frr_obj.get_ip_route_sum()
-    pprint.pprint(result)
+    for func in (frr_obj.get_ip_bgp_sum,
+                 frr_obj.get_isis_neighbor,
+                 frr_obj.get_ip_route_sum,
+                 frr_obj.get_ip_route,
+                 frr_obj.get_ip_fib):
+        pprint.pprint(func())
+
+    linux_obj = Linux(host_ip='192.168.162.128',
+                      ssh_username="root",
+                      ssh_password="fun123")
+    pprint.pprint(linux_obj.get_ip_route(netns=node))
