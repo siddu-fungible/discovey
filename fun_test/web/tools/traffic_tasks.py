a0276f4796e8b921a177f2e1412bcff94e8ff29b
John Abraham <john.abraham@fungible.com>
2017-11-17 12:42:00 -0800

    change relative topo paths

--- fun_test/web/tools/traffic_tasks.py
+++ fun_test/web/tools/traffic_tasks.py
@@ -1,11 +1,11 @@
-import os, django, topo, json, time
+import os, django, json, time
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fun_test.settings")
 django.setup()
 from fun_global import RESULTS
 from fun_settings import *
 from web.tools.models import Session, F1, Tg, TrafficTask
 from lib.utilities.test_dpcsh_tcp_proxy import DpcshClient
-
+from lib.topology.topology_manager.topo_manager import topo
 
 
 def start_fio(session_id, f1_record, fio_info):
