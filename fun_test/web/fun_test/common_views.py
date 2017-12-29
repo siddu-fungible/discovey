5614352fec4204b192f273771d2965b1cd114405
John Abraham <john.abraham@fungible.com>
2017-12-29 01:56:19 -0800

    add common views

---
+++ fun_test/web/fun_test/common_views.py
@@ -0,0 +1,29 @@
+from web.fun_test.settings import COMMON_WEB_LOGGER_NAME
+from django.shortcuts import render
+import logging, json
+from web_global import initialize_result
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.transaction import atomic
+
+logger = logging.getLogger(COMMON_WEB_LOGGER_NAME)
+
+def alerts_page(request):
+    return render(request, 'qa_dashboard/alerts_page.html', locals())
+
+@csrf_exempt
+def add_session_log(request):
+    if not "logs" in request.session:
+        request.session["logs"] = []
+    request_json = json.loads(request.body)
+    log = request_json
+    request.session["logs"].append(log)
+    request.session.save()
+    return HttpResponse("Ok")
+
+def get_session_logs(request):
+    logs = []
+    if "logs" in request.session:
+        logs = request.session["logs"]
+    return HttpResponse(json.dumps(logs))
