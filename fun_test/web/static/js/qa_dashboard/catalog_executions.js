1a5e0d121eee2b79929ae3bbd90a64eff4f84442
John Abraham <john.abraham@fungible.com>
2018-01-02 17:10:13 -0800

    add progress for catalog executions

--- fun_test/web/static/js/qa_dashboard/catalog_executions.js
+++ fun_test/web/static/js/qa_dashboard/catalog_executions.js
@@ -16,11 +16,30 @@ function CatalogExecutionsController($scope, $http, $window, commonService) {
             if (!commonService.validateApiResult(result, message)) {
                 return;
             }
-            let data = result.data.data;
-            $scope.catalogExecutionSummary = data;
+            $scope.catalogExecutionSummary = result.data.data;
             $scope.catalogExecutionSummary.forEach(function (instance) {
                 let instanceName = instance.fields.instance_name;
-                let i = 0;
+                let thisInstance = instance;
+                thisInstance.fields.numPassed = 0;
+                thisInstance.fields.numFailed = 0;
+                thisInstance.fields.numTotal = 0;
+
+                commonService.apiGet("/tcm/catalog_suite_execution_details_with_jira/" + instanceName).then(function (data) {
+
+                    angular.forEach(data.jira_ids, function (info, jiraId) {
+                        info.instances.forEach(function (instance) {
+                           thisInstance.fields.numTotal += 1;
+                           if(instance.result === "PASSED") {
+                               thisInstance.fields.numPassed += 1;
+                           }
+                           if(instance.result === "FAILED") {
+                               thisInstance.fields.numFailed += 1;
+                           }
+
+
+                        });
+                    });
+                })
             });
 
         }).catch(function (result) {
