8d11a32f71ce3aa63016b7d1e184dae4012df7f6
John Abraham <john.abraham@fungible.com>
2018-01-18 20:30:41 -0800

    update docker_test with build_url

--- fun_test/scripts/examples/docker_test.py
+++ fun_test/scripts/examples/docker_test.py
@@ -4,7 +4,7 @@ import pprint
 
 if __name__ == "__main__":
     topology_helper = TopologyHelper()
-    deployed_assets = topology_helper.quick_docker_deploy(num_f1=1, num_tg=1)
+    deployed_assets = topology_helper.quick_docker_deploy(num_f1=2, num_tg=1, build_url="http://10.1.20.99/doc/jenkins/funos/latest/")
     pprint.pprint(deployed_assets)
     time.sleep(15)
     # topology_helper.quick_docker_deploy(cleanup=True)
