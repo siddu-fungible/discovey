59f671a8f2ffee315fb01795a3e6ae61f4c9457c
John Abraham <john.abraham@fungible.com>
2018-01-19 18:35:21 -0800

    let docker test use > 1 F1

--- fun_test/scripts/examples/docker_test.py
+++ fun_test/scripts/examples/docker_test.py
@@ -7,30 +7,28 @@ import os
 
 if __name__ == "__main__":
     os.environ["DOCKER_HOSTS_SPEC_FILE"] = "my-docker-hosts.json"
+    num_f1 = 2
     topology_helper = TopologyHelper()
-    deployed_assets = topology_helper.quick_docker_deploy(num_f1=1,
+    deployed_assets = topology_helper.quick_docker_deploy(num_f1=num_f1,
                                                           num_tg=1,
                                                           build_url="http://10.1.20.99/doc/jenkins/funos/latest/",
                                                           funos_command="'/funos-posix app=load_mods --dpc-server'")
     pprint.pprint(deployed_assets)
     time.sleep(15)
 
-    f1_asset_0 = deployed_assets['f1_assets'][0]
-    f1_external_ip = f1_asset_0["host_ip"]
-    f1_external_dpcsh_proxy_port = f1_asset_0["dpcsh_proxy_port"]["external"]
-
-
-    count = 100
-    while count > 0:
-        try:
-            storage_controller = StorageController(target_ip=f1_external_ip, target_port=f1_external_dpcsh_proxy_port)
-            storage_controller.command("peek /stats")
-            break
-        except:
-            time.sleep(5)
-
-
-
+    for index in range(num_f1):
+        f1_asset = deployed_assets['f1_assets'][index]
+        f1_external_ip = f1_asset["host_ip"]
+        f1_external_dpcsh_proxy_port = f1_asset["dpcsh_proxy_port"]["external"]
+        count = 100
+        while count > 0:
+            try:
+                storage_controller = StorageController(target_ip=f1_external_ip, target_port=f1_external_dpcsh_proxy_port)
+                storage_controller.command("peek /stats")
+                break
+            except:
+                time.sleep(5)
+                count -= 1
 
     topology_helper.quick_docker_deploy(cleanup=True)
 
