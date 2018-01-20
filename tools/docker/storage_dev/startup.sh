a9087210e6e866197fb3098487c1a8f9ce1e2798
John Abraham <john.abraham@fungible.com>
2018-01-19 18:12:53 -0800

    update storage_dev

--- tools/docker/storage_dev/startup.sh
+++ tools/docker/storage_dev/startup.sh
@@ -19,7 +19,7 @@ curl_fetch()
     fi
 }
 
-if [ $# -ne 1 ]; then
+if [ $# -lt 2 ]; then
     usage
     exit 1
 fi
@@ -73,20 +73,21 @@ mv build/$funos_posix_name $funos_posix_name
 rm -rf build
 chmod 777 $dpcsh_name
 
-echo "-----------------------"
-echo "Starting Dpch tcp proxy
-echo "-----------------------"
-if [ "$2" == "True" ]; then
-    cd /; ./dpcsh  --tcp_proxy $dpcsh_tcp_proxy_default_internal_port &> /tmp/dpcsh.log &
-    sleep 5
-fi
-
-
-if [ "$3" != "" ]; then
+if [ "$2" != "" ]; then
     echo "-------------------"
-    echo "funos-posix command
+    echo "funos-posix command"
     echo "-------------------"
-    $3
+    dd if=/dev/zero of=nvfile bs=4096 count=256
+    /$funos_posix_name app=mdt_test nvfile=nvfile &> /tmp/f1mdt.log
+    $2 &> /tmp/f1.log &
+    if [ "$3" == "True" ]; then
+        echo "-----------------------"
+        echo "Starting Dpch tcp proxy"
+        echo "-----------------------"
+        sleep 20
+        cd /; ./dpcsh  --tcp_proxy $dpcsh_tcp_proxy_default_internal_port &> /tmp/dpcsh.log &
+        sleep 5
+    fi
 fi
 
 
