f7a8ab56296ebd2fd90dcf58d9e7542081173475
Alagarswamy Devaraj <alagarswamy.devaraj@fungible.com>
2018-02-01 15:42:27 -0800

    Removed the FIO make from the script and doing it inside the container image

--- tools/docker/tgen/setup_fio.sh
+++ tools/docker/tgen/setup_fio.sh
@@ -2,18 +2,9 @@ if [ -d "fio" ]; then
     rm -rf ./fio
 fi
 
-if [ -f "fio" ]; then
-    echo "fio is already present"
-    exit -1
-fi
 git clone https://github.com/axboe/fio.git
 cd fio
 git checkout df4bf1178ed773986129da6038961388af926971
 git apply ../../../fio-plugin/patch
 mkdir -p Integration/tools/fio-plugin
 cp -r ../../../fio-plugin/* Integration/tools/fio-plugin/
-make
-cd ..
-mv fio/fio fio_exe
-rm -rf fio/
-mv fio_exe fio
