f5ba558aa1bb91c70beef8b91bb73275417f9cfb
John Abraham <john.abraham@fungible.com>
2018-02-06 22:21:51 -0800

    disable site registration for now

--- fun_test/web/fun_test/settings.py
+++ fun_test/web/fun_test/settings.py
@@ -193,3 +193,6 @@ REDIS_PORT = '6379'
 BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
 CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+
+
+# SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
