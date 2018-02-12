c6b6dc9b5dcaca583e39f9883069c5c9e85aaa3c
Divya Bitragunta <divya.bitragunta@fungible.com>
2018-02-12 15:46:43 -0800

    initial version of openssl test template and test cases

---
+++ fun_test/scripts/security/lib_crypto/common_util_helper.py
@@ -0,0 +1,11 @@
+
+class CommonUtils:
+
+    def convert_to_hex(self, input):
+        print input
+
+    def convert_to_ascii(self, input):
+        print input
+
+
+
---
+++ fun_test/scripts/security/openssl_test.py
@@ -0,0 +1,95 @@
+from lib.system.fun_test import *
+from lib.templates.security.openssl_template import OpenSslTemplate
+from lib.templates.security.crypto_template import CryptoTemplate
+from lib.topology.topology_helper import TopologyHelper
+from lib.topology.dut import Dut, DutInterface
+from lib.fun.f1 import F1
+
+topology_dict = {
+    "name": "Basic Security",
+    "dut_info": {
+        0: {
+            "mode": Dut.MODE_SIMULATION,
+            "type": Dut.DUT_TYPE_FSU,
+            "interface_info": {
+                0: {
+                    "vms": 1,
+                    "type": DutInterface.INTERFACE_TYPE_PCIE
+                }
+            },
+            "start_mode": F1.START_MODE_NORMAL
+        }
+
+    }
+}
+
+base_file_name = "/tmp/input"
+vector_path = "/test_vectors/sha1.txt"
+
+
+class OpenSslScript(FunTestScript):
+    def describe(self):
+        self.set_test_details(steps="""
+        1. Deploy the topology. i.e Start POSIM and Allocate a QEMU instance
+        2. Make the QEMU instance available for the testcase
+                              """)
+
+    def setup(self):
+        topology_obj_helper = TopologyHelper(spec=topology_dict)
+        self.topology = topology_obj_helper.deploy()
+        fun_test.test_assert(self.topology, "Ensure deploy is successful")
+        fun_test.shared_variables["topology"] = self.topology
+
+    def cleanup(self):
+        if self.topology:
+            TopologyHelper(spec=self.topology).cleanup()
+        return True
+
+
+class OpenSslTestCase1(FunTestCase):
+    def describe(self):
+        self.set_test_details(id=1,
+                              summary="Check the OpenSSL sha1 digests generated",
+                              steps="""
+        1. Parse input file
+        2. Run OpenSSL sha1 digest commands
+        3. Compare with precomputed digests
+        
+                              """)
+
+    def setup(self):
+        return True
+
+    def cleanup(self):
+        return True
+
+    def compute_all_digests(self, host, openssl_template):
+        # parse the input vectors file
+        file_path = fun_test.get_script_parent_directory() + vector_path
+        digest_dict = openssl_template.parse_input(file_path)
+        input_list = digest_dict.keys()
+
+        # compute and compare digests
+        for index, digest_input in enumerate(input_list):
+            host.create_file("{}{}.txt".format(base_file_name, index), contents=digest_input, newline=False)
+            digest_output = openssl_template.compute_digest(CryptoTemplate.dgst, CryptoTemplate.sha1,
+                                                            "{}{}.txt".format(base_file_name, index))
+            fun_test.simple_assert((digest_output == digest_dict[digest_input]), "Digests match")
+
+        return True
+
+    def run(self):
+        topology = fun_test.shared_variables["topology"]
+
+        host = topology.get_host_instance(dut_index=0, interface_index=0, host_index=0)
+        openssl_template = OpenSslTemplate(host)
+        openssl_template.setup_verify()
+
+        compute_digests_flag = self.compute_all_digests(host, openssl_template)
+        fun_test.test_assert(compute_digests_flag, "Digests match")
+
+
+if __name__ == "__main__":
+    openssl_script = OpenSslScript()
+    openssl_script.add_test_case(OpenSslTestCase1())
+    openssl_script.run()
