05d5f81e8a72886b24924e1d9d1d14db6eb5cfaf
Amit Surana <amit.surana@fungible.com>
2018-02-19 13:54:08 -0800

    New storage docker file for FunSdk

---
+++ tools/docker/storage_x86_64/README.md
@@ -0,0 +1,26 @@
+# storage_basic_x86_64
+
+Purpose:
+```
+Base image to create a container with the following fetched from the latest jenkins build
+1. funos-posix
+2. dpcsh
+3. qemu image
+
+and the following fetched from other sources
+3. QEMU FS, Kernel
+4. Kernel modules
+```
+
+Build:
+```
+sudo docker build . -t storage_basic_x86_64 --build-arg DOCKHUB_FUNGIBLE_LOCAL=10.1.20.99
+```
+
+Run:
+```
+sudo docker run -d -p3220:22 -p2220:2220 --privileged --name="$container_name" storage_basic_x86_64 <build-url>
+
+where build-url=http://dochub.fungible.local/doc/jenkins/funos/latest/
+```
+
---
+++ tools/docker/storage_x86_64/startup.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+set -e
+usage()
+{
+    echo "Usage: startup.sh <build url>"
+}
+
+curl_fetch()
+{
+    url=$1
+    echo "Fetching $url"
+    curl_output=`curl -w 'httpcode=%{http_code}\n' $url -O`
+    if [[ $curl_output == *"httpcode=200"* ]]; then
+        echo "Fetched $url"
+    else
+        echo "Fetch failed for $url"
+        exit 1
+    fi
+}
+
+if [ $# -ne 1 ]; then
+    usage
+    exit 1
+fi
+dochub_fungible_local=10.1.20.99
+base_url=$1
+dpcsh_tgz_name=dpcsh.tgz
+dpcsh_tgz_url=$base_url/$dpcsh_tgz_name
+dpcsh_name=dpcsh
+funos_tgz_name=funos.posix-base.tgz
+funos_tgz_url=$base_url/$funos_tgz_name
+qemu_name=qemu-system-x86_64
+qemu_tgz_name=qemu.tgz
+qemu_tgz_url=$base_url/$qemu_tgz_name
+# pcbios_tgz_name=pc-bios.tgz
+# pcbios_tgz_url=$base_url/$pcbios_tgz_name
+modules_tgz_name=modules.tgz
+modules_tgz_url=http://$dochub_fungible_local/doc/jenkins/fungible-host-drivers/latest/x86_64/modules.tgz
+
+echo "Base url: $base_url"
+echo "Dpsch tgz url: $dpcsh_tgz_url"
+echo "FunOS tgz url: $funos_tgz_url"
+echo "Qemu tgz url: $qemu_tgz_url"
+# echo "PC Bios url: $pcbios_tgz_url"
+echo "Modules url: $modules_tgz_url"
+
+
+arr=($dpcsh_tgz_url $funos_tgz_url $qemu_tgz_url $modules_tgz_url)
+for url in "${arr[@]}"
+    do
+        curl_fetch $url
+    done
+
+echo "Fetched all files"
+
+echo "---------------------"
+echo "Setting up qemu files"
+echo "---------------------"
+qemu_directory=qemu
+tar -xvzf $qemu_tgz_name -C $qemu_directory
+# tar -xvzf $pcbios_tgz_name -C $qemu_directory
+qemu_linux_directory=$qemu_directory/qemu-Linux
+qemu_bin_directory=$qemu_linux_directory/bin
+qemu_name=qemu-system-x86_64
+chmod 777 $qemu_bin_directory/$qemu_name
+export PATH=$PATH:$qemu_bin_directory
+
+
+echo "----------------------"
+echo "Setting up funos files"
+echo "----------------------"
+funos_posix_name=funos-posix
+tar -xf $funos_tgz_name -C /tmp
+mv /tmp/bin/$funos_posix_name $funos_posix_name
+rm -rf /tmp/bin
+
+echo "----------------------"
+echo "Setting up dpcsh files"
+echo "----------------------"
+tar -xf $dpcsh_tgz_name -C /tmp
+mv /tmp/bin/Linux/$dpcsh_name $dpcsh_name
+rm -rf /tmp/bin
+chmod 777 $dpcsh_name
+
+
+echo "-------------------"
+echo "Starting SSH server"
+echo "-------------------"
+/etc/init.d/ssh restart
+echo "Started SSH Server"
+
+echo "Setup is complete"
+echo "Idling forever"
+while [ 1 ]
+    do
+        sleep 1
+    done
+
