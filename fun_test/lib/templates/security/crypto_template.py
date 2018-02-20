4d29c80b28f2e7dd6c8223d62fd5506ecbc90b02
Divya Bitragunta <divya.bitragunta@fungible.com>
2018-02-20 11:01:43 -0800

    updated script to adhere the automation scripting guidelines

--- fun_test/lib/templates/security/crypto_template.py
+++ fun_test/lib/templates/security/crypto_template.py
@@ -5,34 +5,34 @@ class CryptoTemplate:
 
     # common variabes used
 
-    openssl = "openssl"
-    sha1 = "sha1"
-    dgst = "dgst"
-    funcrypto = "funcrypto"
-    version = "version"
-    openssl_version = "1.0.2k"
-    engine = "engine"
-    af_alg = "af_alg"
+    OPENSSL = "openssl"
+    SHA1 = "sha1"
+    DGST = "dgst"
+    FUNCRYPTO = "funcrypto"
+    VERSION = "version"
+    OPENSSL_VERSION = "1.0.2k"
+    ENGINE = "engine"
+    AF_ALG = "af_alg"
 
     def __init__(self, host):
         self.host = host
 
     def verify_openssl_version(self):
         # validate existence of OpenSSL
-        version_output = (self.host.command(self.openssl + " " + self.version)).split(" ")
+        version_output = (self.host.command(self.OPENSSL + " " + self.VERSION)).split(" ")
         fun_test.debug("OpenSSL version output: {}".format(version_output))
         version = version_output[1]
-        fun_test.test_assert_expected(expected=self.openssl_version,
+        fun_test.test_assert_expected(expected=self.OPENSSL_VERSION,
                                       actual=version,
                                       message="OpenSSL version as expected")
         return True
 
     def load_funcrypto(self):
-        self.host.modprobe(CryptoTemplate.funcrypto)
+        self.host.modprobe(self.FUNCRYPTO)
 
     def verify_funcrypto(self):
         # check if funcrypto module is loaded
-        fun_test.test_assert((self.host.lsmod(CryptoTemplate.funcrypto)), message="funcrypto loaded")
+        fun_test.test_assert((self.host.lsmod(self.FUNCRYPTO)), message="funcrypto loaded")
         return True
 
     def parse_input(self, file_path):
--- fun_test/lib/templates/security/openssl_template.py
+++ fun_test/lib/templates/security/openssl_template.py
@@ -1,5 +1,4 @@
 from lib.system.fun_test import fun_test
-import re
 from lib.templates.security.crypto_template import CryptoTemplate
 
 
@@ -9,15 +8,14 @@ class OpenSslTemplate(CryptoTemplate):
         self.host = host
 
     def setup(self):
-        crypto_template = CryptoTemplate(self.host)
-        crypto_template.load_funcrypto()
-        crypto_template.verify_openssl_version()
-        crypto_template.verify_funcrypto()
+        self.load_funcrypto()
+        self.verify_openssl_version()
+        self.verify_funcrypto()
         return True
 
     def compute_digest(self, openssl_type, algorithm, digest_input, engine=None):
-        #openssl_command = CryptoTemplate.openssl + " " + openssl_type + +" -"+ CryptoTemplate.engine + " " + CryptoTemplate.af_alg + " -" ++ algorithm + " " + digest_input
-        openssl_command = CryptoTemplate.openssl + " " + openssl_type + " -" + algorithm + " " + digest_input
+        #openssl_command = CryptoTemplate.OPENSSL + " " + openssl_type + " -" + CryptoTemplate.ENGINE + " " + CryptoTemplate.AF_ALG + " -" + algorithm + " " + digest_input
+        openssl_command = CryptoTemplate.OPENSSL + " " + openssl_type + " -" + algorithm + " " + digest_input
         digest_output = ((self.host.command(openssl_command)).split("="))[1].strip()
         return digest_output
 
