e94b81a506362bff5cfffa244fc1c68ba2fca92e
Divya Bitragunta <divya.bitragunta@fungible.com>
2018-02-22 22:42:25 -0800

    changes for sha1 and sha2 digests

--- fun_test/scripts/security/openssl_test.py
+++ fun_test/scripts/security/openssl_test.py
@@ -1,6 +1,5 @@
 from lib.system.fun_test import *
 from lib.templates.security.openssl_template import OpenSslTemplate
-from lib.templates.security.crypto_template import CryptoTemplate
 from lib.topology.topology_helper import TopologyHelper
 from lib.topology.dut import Dut, DutInterface
 from lib.fun.f1 import F1
@@ -24,7 +23,12 @@ topology_dict = {
 }
 
 base_file_name = "/tmp/input"
-vector_path = "/test_vectors/sha1.txt"
+vector_path = "/test_vectors"
+sha1_path = "/sha1.txt"
+sha224_path = "/sha224.txt"
+sha256_path = "/sha256.txt"
+sha384_path = "/sha384.txt"
+sha512_path = "/sha512.txt"
 
 
 class OpenSslScript(FunTestScript):
@@ -38,7 +42,25 @@ class OpenSslScript(FunTestScript):
         topology_obj_helper = TopologyHelper(spec=topology_dict)
         self.topology = topology_obj_helper.deploy()
         fun_test.test_assert(self.topology, "Ensure deploy is successful")
-        fun_test.shared_variables["topology"] = self.topology
+        host = self.topology.get_host_instance(dut_index=0, interface_index=0, host_index=0)
+        openssl_template = OpenSslTemplate(host)
+        openssl_template.setup()
+        fun_test.shared_variables["host"] = host
+        fun_test.shared_variables["openssl_template"] = openssl_template
+
+    def compute_digests(self, host, openssl_template, path, algorithm):
+        # parse the input vectors file
+        file_path = fun_test.get_script_parent_directory() + vector_path + path
+        digest_dict = openssl_template.parse_input(file_path)
+        input_list = digest_dict.keys()
+
+        # compute and compare digests
+        for index, digest_input in enumerate(input_list):
+            host.create_file_using_printf("{}{}.txt".format(base_file_name, index), contents=digest_input)
+            digest_output = openssl_template.compute_digest(algorithm, "{}{}.txt".format(base_file_name, index), tool_type=OpenSslTemplate.DGST)
+            fun_test.simple_assert((digest_output == digest_dict[digest_input]), "Digests match")
+
+        return True
 
     def cleanup(self):
         if self.topology:
@@ -49,7 +71,7 @@ class OpenSslScript(FunTestScript):
 class OpenSslTestCase1(FunTestCase):
     def describe(self):
         self.set_test_details(id=1,
-                              summary="Check the OpenSSL sha1 digests generated",
+                              summary="Check the OpenSSL SHA-1 digests generated",
                               steps="""
         1. Parse input file
         2. Run OpenSSL sha1 digest commands
@@ -63,31 +85,40 @@ class OpenSslTestCase1(FunTestCase):
     def cleanup(self):
         return True
 
-    def compute_sha1_digests(self, host, openssl_template):
-        # parse the input vectors file
-        file_path = fun_test.get_script_parent_directory() + vector_path
-        digest_dict = openssl_template.parse_input(file_path)
-        input_list = digest_dict.keys()
+    def run(self):
+        openssl_template = fun_test.shared_variables["openssl_template"]
+        host = fun_test.shared_variables["host"]
+        fun_test.test_assert((openssl_script.compute_digests(host, openssl_template, sha1_path, openssl_template.SHA1)), "sha1 digests match")
 
-        # compute and compare digests
-        for index, digest_input in enumerate(input_list):
-            host.create_file("{}{}.txt".format(base_file_name, index), contents=digest_input, newline=False)
-            digest_output = openssl_template.compute_digest(CryptoTemplate.DGST, CryptoTemplate.SHA1,
-                                                            "{}{}.txt".format(base_file_name, index))
-            fun_test.simple_assert((digest_output == digest_dict[digest_input]), "Digests match")
 
+class OpenSslTestCase2(FunTestCase):
+    def describe(self):
+        self.set_test_details(id=2,
+                              summary="Check the OpenSSL SHA-2 digests generated",
+                              steps="""
+        1. Parse input file
+        2. Run OpenSSL sha1 digest commands
+        3. Compare with precomputed digests
+
+                              """)
+
+    def setup(self):
+        return True
+
+    def cleanup(self):
         return True
 
     def run(self):
-        topology = fun_test.shared_variables["topology"]
-
-        host = topology.get_host_instance(dut_index=0, interface_index=0, host_index=0)
-        openssl_template = OpenSslTemplate(host)
-        openssl_template.setup()
-        fun_test.test_assert((self.compute_sha1_digests(host, openssl_template)), "sha1 digests match")
+        openssl_template = fun_test.shared_variables["openssl_template"]
+        host = fun_test.shared_variables["host"]
+        fun_test.test_assert((openssl_script.compute_digests(host, openssl_template, sha224_path, openssl_template.SHA224)), "sha224 digests match")
+        fun_test.test_assert((openssl_script.compute_digests(host, openssl_template, sha256_path, openssl_template.SHA256)), "sha256 digests match")
+        fun_test.test_assert((openssl_script.compute_digests(host, openssl_template, sha384_path, openssl_template.SHA384)), "sha384 digests match")
+        fun_test.test_assert((openssl_script.compute_digests(host, openssl_template, sha512_path, openssl_template.SHA512)), "sha512 digests match")
 
 
 if __name__ == "__main__":
     openssl_script = OpenSslScript()
     openssl_script.add_test_case(OpenSslTestCase1())
+    openssl_script.add_test_case(OpenSslTestCase2())
     openssl_script.run()
