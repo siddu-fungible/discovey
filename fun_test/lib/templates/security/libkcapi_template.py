bc95590230e57a66c370453dd6f5ee7c071dc042
Divya Bitragunta <divya.bitragunta@fungible.com>
2018-03-02 18:57:20 -0800

    added ccm(aes) and authenc for libkcapi

--- fun_test/lib/templates/security/libkcapi_template.py
+++ fun_test/lib/templates/security/libkcapi_template.py
@@ -5,6 +5,8 @@ from lib.templates.security.crypto_template import CryptoTemplate
 class LibkcapiTemplate(CryptoTemplate):
 
     GCM_AES = "gcm(aes)"
+    CCM_AES = "ccm(aes)"
+    AUTH_ENC = "authenc(hmac(sha1),cbc(aes))"
 
     def __init__(self, host):
         self.host = host
@@ -19,10 +21,18 @@ class LibkcapiTemplate(CryptoTemplate):
         input_dict = eval((open(file_path, 'r')).read())
         return input_dict
 
-    def enc_aes(self, algorithm, cipher_type, plain_text, key, iv, assoc_data, tag_len):
-        output = self.host.command("/tmp/libkcapi/bin/kcapi -x " + cipher_type + " -e -c  \"" + algorithm + "\" -p " + plain_text + " -k " + key + " -i " + iv + " -a  " + assoc_data + " -l " + tag_len).strip()
+    def enc_gcm_aes(self, algorithm, cipher_type, plain_text, key, iv, assoc_data, tag_len):
+        output = self.host.command("/tmp/libkcapi/bin/kcapi -x " + cipher_type + " -e -c  \"" + algorithm + "\" -p " + plain_text + " -k " + key + " -i " + iv + " -a " + assoc_data + " -l " + tag_len).strip()
         return output
 
-    def dec_aes(self, algorithm, cipher_type, cipher_text, key, iv, assoc_data, tag):
-        output = self.host.command("/tmp/libkcapi/bin/kcapi -x " + cipher_type + " -c  \"" + algorithm + "\" -q " + cipher_text + " -t " + tag + " -i " + iv + " -k " + key + " -a  " + assoc_data).strip()
+    def dec_gcm_aes(self, algorithm, cipher_type, cipher_text, key, iv, assoc_data, tag):
+        output = self.host.command("/tmp/libkcapi/bin/kcapi -x " + cipher_type + " -c  \"" + algorithm + "\" -q " + cipher_text + " -t " + tag + " -i " + iv + " -k " + key + " -a " + assoc_data).strip()
+        return output
+
+    def dec_gcm_ccm(self, algorithm, cipher_type, cipher_text, nonce, key, assoc_data, tag):
+        output = self.host.command("/tmp/libkcapi/bin/kcapi -x " + cipher_type + " -c  \"" + algorithm + "\" -q " + cipher_text + " -t " + tag + " -n " + nonce + " -k " + key + " -a " + assoc_data).strip()
+        return output
+
+    def auth_enc(self, algorithm, cipher_type, plain_text, key, iv, assoc_data, tag_len):
+        output = self.host.command("/tmp/libkcapi/bin/kcapi -x " + cipher_type + " -e -c  \"" + algorithm + "\" -p " + plain_text + " -k " + key + " -i " + iv + " -a " + assoc_data + " -l " + tag_len).strip()
         return output
--- fun_test/scripts/security/libkcapi_test.py
+++ fun_test/scripts/security/libkcapi_test.py
@@ -24,7 +24,7 @@ topology_dict = {
 }
 
 libkcapi_template = ""
-vector_path = "/test_vectors/kcapi_gcm.txt"
+vector_path = "/test_vectors/kcapi_vectors.txt"
 
 
 class LibkcapiScript(FunTestScript):
@@ -62,8 +62,8 @@ class LibkcapiTestCase1(FunTestCase):
         self.set_test_details(id=1,
                               summary="Run gcm(aes) with libkcapi",
                               steps="""
-        1. Run kcapi command for gcm(aes) with a sample plain text
-        3. Compare with the known output and ensure they are equal
+        1. Encrypt and decrypt gcm(aes) with a sample plain text using kcapi command 
+        2. Compare with the known output and ensure they are equal
         
                              """)
 
@@ -83,16 +83,78 @@ class LibkcapiTestCase1(FunTestCase):
                 enc_dict = input_dict[dict]
             elif dict == "dec_gcm(aes)":
                 dec_dict = input_dict[dict]
-        enc_output = (libkcapi_template.enc_aes(LibkcapiTemplate.GCM_AES, enc_dict['cipher_type'], enc_dict['plain_text'],
+        enc_output = (libkcapi_template.enc_gcm_aes(LibkcapiTemplate.GCM_AES, enc_dict['cipher_type'], enc_dict['plain_text'],
                                       enc_dict['key'], enc_dict['iv'], enc_dict['assosc_data'],
                                       enc_dict['tag_len'])).strip()
-        dec_output = (libkcapi_template.dec_aes(LibkcapiTemplate.GCM_AES, dec_dict['cipher_type'], dec_dict['cipher_text'],
+        dec_output = (libkcapi_template.dec_gcm_aes(LibkcapiTemplate.GCM_AES, dec_dict['cipher_type'], dec_dict['cipher_text'],
                                       dec_dict['key'], dec_dict['iv'], dec_dict['assosc_data'],
                                       dec_dict['tag'])).strip()
         fun_test.test_assert((enc_output == enc_dict['result']), "gcm(aes) encryption verified ")
         fun_test.test_assert((dec_output == dec_dict['result']), "gcm(aes) decryption verified ")
 
+
+class LibkcapiTestCase2(FunTestCase):
+    def describe(self):
+        self.set_test_details(id=2,
+                              summary="Run ccm(aes) with libkcapi",
+                              steps="""
+        1. Decrypt ccm(aes) with a sample plain text using kcapi command 
+        2. Compare with the known output and ensure they are equal
+
+                             """)
+
+    def setup(self):
+        pass
+
+    def cleanup(self):
+        pass
+
+    def run(self):
+        libkcapi_template = fun_test.shared_variables["libkcapi_template"]
+        input_dict = fun_test.shared_variables["input_dict"]
+        dec_dict = {}
+        for dict in input_dict:
+            if dict == "dec_ccm(aes)":
+                dec_dict = input_dict[dict]
+        dec_output = (libkcapi_template.dec_gcm_ccm(LibkcapiTemplate.CCM_AES, dec_dict['cipher_type'], dec_dict['cipher_text'],
+                                      dec_dict['nonce'], dec_dict['key'], dec_dict['assosc_data'],
+                                      dec_dict['tag'])).strip()
+        fun_test.test_assert((dec_output == dec_dict['result']), "ccm(aes) decryption verified ")
+
+
+class LibkcapiTestCase3(FunTestCase):
+    def describe(self):
+        self.set_test_details(id=3,
+                              summary="Run authenc with libkcapi",
+                              steps="""
+        1. Compute authenc with a sample plain text using kcapi command 
+        2. Compare with the known output and ensure they are equal
+
+                             """)
+
+    def setup(self):
+        pass
+
+    def cleanup(self):
+        pass
+
+    def run(self):
+        libkcapi_template = fun_test.shared_variables["libkcapi_template"]
+        input_dict = fun_test.shared_variables["input_dict"]
+        enc_dict = {}
+        for dict in input_dict:
+            if dict == "auth_enc":
+                enc_dict = input_dict[dict]
+
+        enc_output = (libkcapi_template.auth_enc(LibkcapiTemplate.AUTH_ENC, enc_dict['cipher_type'], enc_dict['plain_text'],
+                                          enc_dict['key'], enc_dict['iv'], enc_dict['assosc_data'],
+                                          enc_dict['tag_len'])).strip()
+
+        fun_test.test_assert((enc_output == enc_dict['result']), "authenc verified ")
+
 if __name__ == "__main__":
     libkcapi_script = LibkcapiScript()
     libkcapi_script.add_test_case(LibkcapiTestCase1())
+    libkcapi_script.add_test_case(LibkcapiTestCase2())
+    libkcapi_script.add_test_case(LibkcapiTestCase3())
     libkcapi_script.run()
