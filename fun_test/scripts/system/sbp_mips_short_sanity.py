7bc2cf2d462573119a548e390a20a946bbb9e281
John Abraham <john.abraham@fungible.com>
2018-03-26 15:39:02 -0700

    add secure boot off too

--- fun_test/scripts/system/sbp_mips_short_sanity.py
+++ fun_test/scripts/system/sbp_mips_short_sanity.py
@@ -64,7 +64,7 @@ class ContainerSetup(FunTestScript):
 class TestCase1(FunTestCase):
     def describe(self):
         self.set_test_details(id=1,
-                              summary="secureboot=off, ",
+                              summary="secureboot=off ",
                               steps="""
         1. Do something on the container.
                               """)
@@ -117,6 +117,75 @@ class TestCase1(FunTestCase):
         fun_test.simple_assert(stimuli_files, "Atleast one stimuli file")
 
         command = 'python ./run_test.py --cpu={} --board_type={} --bitstream={} --board={} -vv --secureboot=off {} &> {}'.format(CPU, BOARD_TYPE, BIT_STREAM, ZYNC_BOARD_IP, stimuli_file, TEST_LOG_FILE)
+        output = linux_obj.command(command, timeout=900)
+
+        linux_obj.command('cd {}/software/board_tests'.format(LOCAL_REPOSITORY_DIR))
+        for stimuli_file in stimuli_files:
+            just_file_name = os.path.basename(stimuli_file["filename"])
+            local_log_file = "log/{}".format(just_file_name.replace(".py", ".log"))
+            last_line = linux_obj.command("tail {}".format(local_log_file))
+            fun_test.test_assert("[EXIT] SUCCESS" in last_line, message="{}: [EXIT] SUCCESS message found".format(just_file_name))
+
+    def cleanup(self):
+        pass
+
+
+class TestCase2(FunTestCase):
+    def describe(self):
+        self.set_test_details(id=1,
+                              summary="secureboot=on ",
+                              steps="""
+        1. Do something on the container.
+                              """)
+
+    def setup(self):
+        pass
+
+    def run(self):
+
+        container_asset = fun_test.shared_variables["container_asset"]
+
+        linux_obj = Linux(host_ip=container_asset["host_ip"],
+                          ssh_username=container_asset["mgmt_ssh_username"],
+                          ssh_password=container_asset["mgmt_ssh_password"],
+                          ssh_port=container_asset["mgmt_ssh_port"])
+
+        sbp_setup = SbpZynqSetupTemplate(host=linux_obj,
+                                         local_repository=LOCAL_REPOSITORY_DIR,
+                                         zynq_board_ip=ZYNC_BOARD_IP)
+
+        localhost = Linux(host_ip="127.0.0.1", localhost=True)
+        localhost.command("cd {}; git pull".format(SBP_FIRMWARE_REPO_DIR))
+        fun_test.test_assert_expected(expected=0, actual=localhost.exit_status(), message="Git pull")
+        fun_test.test_assert(sbp_setup.setup(), "Setup")
+        linux_obj.command('cd {}/software/board_tests'.format(LOCAL_REPOSITORY_DIR))
+        linux_obj.command("ls -l")
+        stimuli_dir = "{}/validation/stimuli/short".format(LOCAL_REPOSITORY_DIR)
+        files = linux_obj.list_files(stimuli_dir)
+
+
+        '''
+        for file in files:
+            filename = file["filename"]
+            if not filename.endswith(".py"):
+                continue
+            stimuli_file = stimuli_dir + "/" + filename
+            fun_test.add_checkpoint(checkpoint="Stimuli file: {}".format(filename))
+            command = 'python ./run_test.py --cpu={} --board_type={} --bitstream={} --board={} -vv --secureboot=off {}'.format(
+                CPU, BOARD_TYPE, BIT_STREAM, ZYNC_BOARD_IP, stimuli_file)
+            output = linux_obj.command(command, timeout=260)
+            fun_test.test_assert(not re.search("ERROR:run_test.py:*errors", output),
+                                 "Error not found: {}".format(filename))
+
+            # stimuli_file = "{}/cmd_debug_access_chlg.py".format(stimuli_dir)
+
+        '''
+
+        stimuli_file = "{}/cmd_AES*.py".format(stimuli_dir)
+        stimuli_files = linux_obj.list_files(stimuli_file)
+        fun_test.simple_assert(stimuli_files, "Atleast one stimuli file")
+
+        command = 'python ./run_test.py --cpu={} --board_type={} --bitstream={} --board={} -vv --secureboot=on {} &> {}'.format(CPU, BOARD_TYPE, BIT_STREAM, ZYNC_BOARD_IP, stimuli_file, TEST_LOG_FILE)
         output = linux_obj.command(command, timeout=900)
 
         linux_obj.command('cd {}/software/board_tests'.format(LOCAL_REPOSITORY_DIR))
