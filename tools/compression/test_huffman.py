73a044f941af3eac5a57bafec4c6718648d9cf15
rnaik-fungible <radhika.naik@fungible.com>
2018-04-13 15:30:00 -0700

    Add test for LZ77 lengths

--- tools/compression/test_huffman.py
+++ tools/compression/test_huffman.py
@@ -11,14 +11,14 @@ a   b   c   d   e   a   b   c
                     
 """
 def duplicate_pattern_in_window(size, file_size=32):
-    random_uniq_str = generate_random_literals(file_size)
+    random_str = generate_random_literals(file_size * 1024)
 
-    substr = random_uniq_str[:int(size * 1024)]
-    rest_data = random_uniq_str[int(size * 1024):]
+    substr = random_str[:int(size * 1024)]
+    rest_data = random_str[int(size * 1024):]
     repeated_bytes = substr[:259]
-    dup_patterns_in_window = substr[:-258] + repeated_bytes + rest_data
+    pattern = substr[:-258] + repeated_bytes + rest_data
 
-    return dup_patterns_in_window
+    return pattern
 
 
 """
@@ -28,14 +28,14 @@ a   b   c   d   e   a   b   c
 "Sliding window" "Should match 'a' in window"  
 """
 def duplicate_pattern_at_window_edge(size, file_size=32):
-    random_uniq_str = generate_random_literals(file_size)
+    random_str = generate_random_literals(file_size * 1024)
 
-    substr = random_uniq_str[:int(size * 1024)]
-    rest_data = random_uniq_str[int(size * 1024):]
+    substr = random_str[:int(size * 1024)]
+    rest_data = random_str[int(size * 1024):]
     repeated_bytes = substr[:259]
-    dup_patterns_in_window = substr + repeated_bytes + rest_data
+    pattern = substr + repeated_bytes + rest_data
 
-    return dup_patterns_in_window
+    return pattern
 
 
 """
@@ -45,14 +45,14 @@ a   b   c   d   e   #   a   b   c
     "Sliding window" "Should not match any char in window"  
 """
 def duplicate_pattern_outside_window(size, file_size=32):
-    random_uniq_str = generate_random_literals(file_size)
+    random_str = generate_random_literals(file_size * 1024)
 
-    substr = random_uniq_str[:int(size * 1024)]
-    rest_data = random_uniq_str[int(size * 1024):]
+    substr = random_str[:int(size * 1024)]
+    rest_data = random_str[int(size * 1024):]
     repeated_bytes = substr[:259]
-    dup_patterns_in_window = substr + os.urandom(1) + repeated_bytes + rest_data
+    pattern = substr + os.urandom(1) + repeated_bytes + rest_data
 
-    return dup_patterns_in_window
+    return pattern
 
 
 """
@@ -65,22 +65,33 @@ a   b   c   d   e   a   b   c   d
 
 """
 def duplicate_spanning_window(size, file_size=32):
-    random_uniq_str = generate_random_literals(file_size)
+    random_str = generate_random_literals(file_size * 1024)
 
-    substr = random_uniq_str[:size * 1024]
-    rest_data = random_uniq_str[size * 1024:]
+    substr = random_str[:size * 1024]
+    rest_data = random_str[size * 1024:]
     repeated_bytes = substr[:259]
-    dup_patterns_in_window = substr[:-(len(repeated_bytes) / 2)] + repeated_bytes + rest_data
+    pattern = substr[:-(len(repeated_bytes) / 2)] + repeated_bytes + rest_data
 
-    return dup_patterns_in_window
+    return pattern
 
 
 def largest_uncompressed_win_size(size, file_size=32):
-    random_uniq_str = generate_random_literals(size)
-    repeat = file_size / size
-    dup_patterns_in_window = random_uniq_str * repeat
+    repeat = ((size * 1024) / 258) + 1  # Window size passed in units of K
+    random_str = []
+    for n in range(0, repeat):
+        random_str.append(generate_random_literals(258))
+    pattern = ''.join(random_str) * (file_size / size)
 
-    return dup_patterns_in_window
+    return pattern
+
+
+def duplicate_len(length, window):
+    random_str = generate_random_literals(window * 1024)
+    substr = random_str[:-length]
+    repeated_bytes = random_str[:length]
+    pattern = substr + repeated_bytes
+
+    return pattern
 
 
 if __name__ == "__main__":
@@ -94,32 +105,34 @@ if __name__ == "__main__":
         infile.write(random_uniq_str)
     """
 
+    """
     ## For duplicate pattern spanning window
-    size = 128
-    file_size = size * 2
-    dup_patterns = duplicate_spanning_window(size, file_size=file_size)
-    filename = "/Users/radhika/Documents/test-scripts/cntbry-crps-tst/cust-corpus/gen-data/split-patn-in-hist-%sk-fsize-%sk.in" % \
-               (str(size), str(file_size))
-    with open(filename, "w+") as infile:
-        infile.write(dup_patterns)
-
+    window_size = [4, 8, 16, 32, 64, 128, 256]
+    for size in window_size:
+        file_size = size * 2
+        dup_patterns = duplicate_spanning_window(size, file_size=file_size)
+        filename = "/Users/radhika/Documents/test-scripts/cntbry-crps-tst/cust-corpus/gen-data/duplicate-spanning-win/" \
+                   "duplicate-span-win-%sk.in" % (str(size))
+        with open(filename, "w+") as infile:
+            infile.write(dup_patterns)
+    
     ## For duplicate patterns within the window
     window_size = [4, 8, 16, 32, 64, 128, 256]
     for size in window_size:
         file_size = size * 2
         dup_patterns = duplicate_pattern_in_window(size, file_size=file_size)
-        filename = "/Users/radhika/Documents/test-scripts/cntbry-crps-tst/cust-corpus/gen-data/duplicate-%sk.in" % \
-                   str(size)
+        filename = "/Users/radhika/Documents/test-scripts/cntbry-crps-tst/cust-corpus/gen-data/duplicate-inside-win/" \
+                   "duplicate-%sk.in" % str(size)
         with open(filename, "w+") as infile:
             infile.write(dup_patterns)
-
+    
     ## For duplicate patterns just at window edge
     window_size = [4, 8, 16, 32, 64, 128, 256]
     for size in window_size:
         file_size = size * 2
         dup_patterns = duplicate_pattern_at_window_edge(size, file_size=file_size)
-        filename = "/Users/radhika/Documents/test-scripts/cntbry-crps-tst/cust-corpus/gen-data/duplicate-at-win-edge-%sk.in" % \
-                   str(size)
+        filename = "/Users/radhika/Documents/test-scripts/cntbry-crps-tst/cust-corpus/gen-data/duplicate-at-win-edge/" \
+                   "duplicate-at-win-edge-%sk.in" % str(size)
         with open(filename, "w+") as infile:
             infile.write(dup_patterns)
 
@@ -128,17 +141,29 @@ if __name__ == "__main__":
     for size in window_size:
         file_size = size * 2
         dup_patterns = duplicate_pattern_outside_window(size, file_size=file_size)
-        filename = "/Users/radhika/Documents/test-scripts/cntbry-crps-tst/cust-corpus/gen-data/duplicate-out-win-%sk.in" % \
-                   str(size)
+        filename = "/Users/radhika/Documents/test-scripts/cntbry-crps-tst/cust-corpus/gen-data/duplicate-out-win/" \
+                   "duplicate-out-win-%sk.in" % str(size)
         with open(filename, "w+") as infile:
             infile.write(dup_patterns)
-
+    """
+    """
     ## Largest uncompressed size for each history size
     window_size = [4, 8, 16, 32, 64, 128, 256]
     for size in window_size:
         file_size = size * 16
         dup_patterns = largest_uncompressed_win_size(size, file_size)
-        filename = "/Users/radhika/Documents/test-scripts/cntbry-crps-tst/cust-corpus/gen-data/largest-uncompressed-win-%sk.in" % \
-                   str(size)
+        filename = "/Users/radhika/Documents/test-scripts/cntbry-crps-tst/cust-corpus/gen-data/largest-uncompress-win/" \
+                   "largest-uncompressed-win-%sk.in" % str(size)
+        with open(filename, "w+") as infile:
+            infile.write(dup_patterns)
+    """
+    ## Duplicate patterns with different lengths
+    start = 2
+    end = 274
+    window = 128     #Translates to window * 1024
+    for length in range(2, end + 1):
+        dup_patterns = duplicate_len(length, window)
+        filename = "/Users/radhika/Documents/test-scripts/cntbry-crps-tst/cust-corpus/gen-data/duplicate-lens-lzma/" \
+                   "duplicate-len-%s.in" % str(length)
         with open(filename, "w+") as infile:
             infile.write(dup_patterns)
