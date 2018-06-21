e61687178e4b4e0824ad5dc3df36c52eadff36a9
RP1807 <rushikesh.pendse@fungible.com>
2018-06-21 11:16:21 +0530

    Ensure at least 1 test case in bringup/cleanup scripts

--- fun_test/scripts/networking/palladium_bringup.py
+++ fun_test/scripts/networking/palladium_bringup.py
@@ -4,60 +4,80 @@ from lib.host.palladium import DpcshProxy, Palladium
 
 PALLADIUM_HOST_FILE = "/palladium_hosts.json"
 
+config = {}
+palladium_boot_up_obj = None
+dpcsh_proxy_obj = None
+is_cleanup_needed = True
+
 
 class PalladiumBringup(FunTestScript):
-    config = {}
-    palladium_boot_up_obj = None
-    dpcsh_proxy_obj = None
-    is_cleanup_needed = True
-
+    
     def describe(self):
         self.set_test_details(steps="""
-        1. Read Palladium host configs
-        2. Build Palladium image from master and pushed it to desired location
-        3. Boot up palladium with FunOS image
-        4. Start dpcsh in tcp proxy mode and ensure dpcsh is started
+        1. Initialize objects and read boot up config
         """)
 
     def setup(self):
+        global config, palladium_boot_up_obj, dpcsh_proxy_obj
         fun_test.log("In script setup")
+
+        config = parse_file_to_json(ASSET_DIR + PALLADIUM_HOST_FILE)[0]
+        fun_test.log("Palladium Host Config: %s" % config)
+
+        palladium_boot_up_obj = Palladium(ip=config['boot_up_server_ip'],
+                                          model=config['model'],
+                                          design=config['design'])
+
+        dpcsh_proxy_obj = DpcshProxy(ip=config['dpcsh_tcp_proxy_ip'],
+                                     port=config['dpcsh_tcp_proxy_port'],
+                                     usb=config['dpcsh_usb'])
 
     def run(self):
         fun_test.log("In palladium bring up run")
 
-        self.config = parse_file_to_json(ASSET_DIR + PALLADIUM_HOST_FILE)[0]
-        fun_test.log("Palladium Host Config: %s" % self.config)
+    def cleanup(self):
+        fun_test.log("In script cleanup")
 
-        self.palladium_boot_up_obj = Palladium(ip=self.config['boot_up_server_ip'],
-                                               model=self.config['model'],
-                                               design=self.config['design'])
 
-        self.dpcsh_proxy_obj = DpcshProxy(ip=self.config['dpcsh_tcp_proxy_ip'],
-                                          port=self.config['dpcsh_tcp_proxy_port'],
-                                          usb=self.config['dpcsh_usb'])
+class TestCase1(FunTestCase):
 
-        checkpoint = "Boot up palladium with FunOS design %s Model %s" % (self.config['design'],
-                                                                          self.config['model'])
-        result = self.palladium_boot_up_obj.boot()
+    def describe(self):
+        self.set_test_details(id=1, summary="Load Palladium Image",
+                              steps="""
+                              1. Read Palladium host configs
+                              2. Build Palladium image from master and pushed it to desired location
+                              3. Boot up palladium with FunOS image
+                              4. Start dpcsh in tcp proxy mode and ensure dpcsh is started
+                              """)
+
+    def setup(self):
+        fun_test.log("In test case setup")
+
+    def run(self):
+        global is_cleanup_needed
+
+        checkpoint = "Boot up palladium with FunOS design %s Model %s" % (config['design'],
+                                                                          config['model'])
+        result = palladium_boot_up_obj.boot()
         fun_test.test_assert(result, checkpoint)
 
-        checkpoint = "Start dpcsh with %s in tcp proxy mode" % self.config['dpcsh_usb']
-        result = self.dpcsh_proxy_obj.start()
+        checkpoint = "Start dpcsh with %s in tcp proxy mode" % config['dpcsh_usb']
+        result = dpcsh_proxy_obj.start()
         fun_test.test_assert(result, checkpoint)
 
-        self.is_cleanup_needed = False
+        is_cleanup_needed = False
 
         checkpoint = "Ensure dpcsh started in TCP proxy mode"
-        result = self.dpcsh_proxy_obj.ensure_started()
+        result = dpcsh_proxy_obj.ensure_started()
         fun_test.test_assert(result, checkpoint)
 
     def cleanup(self):
-        if not self.is_cleanup_needed:
-            self.palladium_boot_up_obj.cleanup_job()
-    # TODO: Check script result here if it is fail then clean up the palladium resources
+        if not is_cleanup_needed:
+            palladium_boot_up_obj.cleanup_job()
 
 
 if __name__ == '__main__':
     ts = PalladiumBringup()
+    ts.add_test_case(TestCase1())
     ts.run()
 
--- fun_test/scripts/networking/palladium_cleanup.py
+++ fun_test/scripts/networking/palladium_cleanup.py
@@ -3,6 +3,10 @@ from lib.host.palladium import Palladium, DpcshProxy
 
 PALLADIUM_HOST_FILE = "/palladium_hosts.json"
 
+config = {}
+palladium_boot_up_obj = None
+dpcsh_proxy_obj = None
+
 
 class PalladiumCleanup(FunTestScript):
     config = {}
@@ -11,52 +15,70 @@ class PalladiumCleanup(FunTestScript):
 
     def describe(self):
         self.set_test_details(steps="""
-        1. Halt FunOS by executing dpc_shutdown cmd
-        2. Cleanup palladium resources and release boards
-        3. Ensure boards are released by the user
-        4. Stop DpcProxy
+        1. Initialize objects and read boot up config
         """)
 
     def setup(self):
+        global config, palladium_boot_up_obj, dpcsh_proxy_obj
         fun_test.log("In script setup")
+        
+        config = parse_file_to_json(ASSET_DIR + PALLADIUM_HOST_FILE)[0]
+        fun_test.log("Palladium Host Config: %s" % config)
+
+        palladium_boot_up_obj = Palladium(ip=config['boot_up_server_ip'],
+                                          model=config['model'],
+                                          design=config['design'])
+
+        dpcsh_proxy_obj = DpcshProxy(ip=config['dpcsh_tcp_proxy_ip'],
+                                     port=config['dpcsh_tcp_proxy_port'],
+                                     usb=config['dpcsh_usb'])
 
     def run(self):
         fun_test.log("In script run")
 
-        self.config = parse_file_to_json(ASSET_DIR + PALLADIUM_HOST_FILE)[0]
-        fun_test.log("Palladium Host Config: %s" % self.config)
+    def cleanup(self):
+        fun_test.log("In script cleanup")
+        
 
-        self.palladium_boot_up_obj = Palladium(ip=self.config['boot_up_server_ip'],
-                                               model=self.config['model'],
-                                               design=self.config['design'])
+class TestCase1(FunTestCase):
+    
+    def describe(self):
+        self.set_test_details(id=1, summary="Release Palladium boards and stop dpcsh",
+                              steps="""
+                              1. Halt FunOS by executing dpc_shutdown cmd
+                              2. Cleanup palladium resources and release boards
+                              3. Ensure boards are released by the user
+                              4. Stop DpcProxy
+                              """)
 
-        self.dpcsh_proxy_obj = DpcshProxy(ip=self.config['dpcsh_tcp_proxy_ip'],
-                                          port=self.config['dpcsh_tcp_proxy_port'],
-                                          usb=self.config['dpcsh_usb'])
+    def setup(self):
+        fun_test.log("In test case setup")
+
+    def run(self):
 
         checkpoint = "Halt FunOS by executing dpc_shutdown cmd"
-        result = self.dpcsh_proxy_obj.run_dpc_shutdown()
+        result = dpcsh_proxy_obj.run_dpc_shutdown()
         fun_test.test_assert(result, checkpoint)
 
         checkpoint = "Cleanup palladium resources and release boards"
-        result = self.palladium_boot_up_obj.cleanup_job()
+        result = palladium_boot_up_obj.cleanup_job()
         fun_test.test_assert(result, checkpoint)
 
         checkpoint = "Ensure boards are released by the user"
-        # TODO: Confirm boards are released by regression user. Extend palladium host lib for this
-        result = self.palladium_boot_up_obj.ensure_boards_released()
+        result = palladium_boot_up_obj.ensure_boards_released()
         fun_test.test_assert(result, checkpoint)
 
         checkpoint = "Stop Dpcproxy"
-        result = self.dpcsh_proxy_obj.stop()
+        result = dpcsh_proxy_obj.stop()
         fun_test.test_assert(result, checkpoint)
 
     def cleanup(self):
-        fun_test.log("In script cleanup")
+        fun_test.log("In test case cleanup")
 
 
 if __name__ == '__main__':
     ts = PalladiumCleanup()
+    ts.add_test_case(TestCase1())
     ts.run()
 
 
