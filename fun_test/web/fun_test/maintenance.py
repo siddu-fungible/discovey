bfc87cc092b94079aec83d1e67f9d30547f9a763
Ash41291 <ashwin.s@fungible.com>
2018-09-18 12:16:20 -0700

    Added script to change the scores

--- fun_test/web/fun_test/maintenance.py
+++ fun_test/web/fun_test/maintenance.py
@@ -288,8 +288,15 @@ if __name__ == "__main__":
                         print(entry.metric_model_name, outputName, data["output"]["expected"])
                         model = apps.get_model(app_label='fun_test', model_name=entry.metric_model_name)
                         mcs_entries = model.objects.filter(**d)
-                        lastData = mcs_entries[len(mcs_entries)-1]
-                        newExpectedOutput = getattr(lastData, outputName)
+                        i = len(mcs_entries) - 1
+                        while i >= 0:
+                            lastData = mcs_entries[i]
+                            newExpectedOutput = getattr(lastData, outputName)
+                            if newExpectedOutput < 0:
+                                i = i-1
+                            else:
+                                break
+                        print newExpectedOutput
                         data["output"]["expected"] = newExpectedOutput
                         print(data["output"]["expected"])
             entry.data_sets = json.dumps(jsonData)
