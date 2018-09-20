3213eae6b3e5df897fc030cfff783c0016fcd6d0
John Abraham <john.abraham@fungible.com>
2018-09-19 21:45:03 -0700

    api service

--- fun_test/web/angular/qadashboard/src/app/app.module.ts
+++ fun_test/web/angular/qadashboard/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule }     from './app-routing.module';
 import { PerformanceComponent } from './performance/performance.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { PerformanceComponent } from './performance/performance.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
 
--- fun_test/web/angular/qadashboard/src/app/performance/performance.component.ts
+++ fun_test/web/angular/qadashboard/src/app/performance/performance.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from "../api.service";
 
 @Component({
   selector: 'app-performance',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerformanceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService: ApiService) {
+
+  }
 
   ngOnInit() {
+
+    let payload = {"metric_id": 122, "date_range": ["2018-04-01T07:00:01.000Z", "2018-09-13T06:59:59.765Z"]};
+    this.apiService.post('/metrics/scores', payload).subscribe(response => {
+        console.log(response.data);
+      },
+      error => {
+        console.log(error);
+      });
+    this.apiService.post('/metrics/7scores', payload).subscribe(response => {
+        console.log(response.data);
+      },
+      error => {
+        console.log(error);
+      });
+
+
   }
 
 }
