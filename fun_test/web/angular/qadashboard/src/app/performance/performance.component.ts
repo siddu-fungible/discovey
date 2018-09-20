726be5e3589abff3afa04a78a072755fd6c9dc0f
John Abraham <john.abraham@fungible.com>
2018-09-20 13:15:09 -0700

    move api service to under api

--- fun_test/web/angular/qadashboard/src/app/performance/performance.component.ts
+++ fun_test/web/angular/qadashboard/src/app/performance/performance.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ApiService} from "../services/api.service";
+import {ApiService} from "../services/api/api.service";
 
 @Component({
   selector: 'app-performance',
