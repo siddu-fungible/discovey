7d5003ff46f94ae4b4507a45aab13dd2c784e9b9
John Abraham <john.abraham@fungible.com>
2018-09-21 11:22:41 -0700

    introduce ngx-bootstrap

--- fun_test/web/angular/qadashboard/src/app/app.module.ts
+++ fun_test/web/angular/qadashboard/src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AppRoutingModule }     from './app-routing.module';
-import { PerformanceComponent } from './performance/performance.component';
-import { HttpClientModule } from '@angular/common/http';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {AppRoutingModule} from './app-routing.module';
+import {PerformanceComponent} from './performance/performance.component';
+import {HttpClientModule} from '@angular/common/http';
 import {ApiService} from "./services/api/api.service";
 import {LoggerService} from "./services/logger/logger.service";
+import { TestComponent } from './test/test.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    PerformanceComponent
+    PerformanceComponent,
+    TestComponent
 
   ],
   imports: [
@@ -26,4 +28,5 @@ import {LoggerService} from "./services/logger/logger.service";
   providers: [ApiService, LoggerService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
