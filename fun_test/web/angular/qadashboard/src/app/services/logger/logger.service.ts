79c1b76701b134218ead754630b937ab8cf6ece7
John Abraham <john.abraham@fungible.com>
2018-10-03 13:27:00 -0700

    logger service use only error

--- fun_test/web/angular/qadashboard/src/app/services/logger/logger.service.ts
+++ fun_test/web/angular/qadashboard/src/app/services/logger/logger.service.ts
@@ -1,102 +1,17 @@
 import { Injectable } from '@angular/core';
 
-export enum LogLevel {
-  All = 0,
-  Debug = 1,
-  Info = 2,
-  Warn = 3,
-  Error = 4,
-  Fatal = 5,
-  Off = 6
-}
 
-export class LogEntry {
-  // Public properties
-  entryDate: Date = new Date();
-  message = '';
-  level: LogLevel = LogLevel.Debug;
-  extraInfo: any[] = [];
-  logWithDate = true;
-
-  buildLogString(): string {
-    let ret = '';
-
-    if (this.logWithDate) {
-      ret = new Date() + ' - ';
-    }
-    ret += 'Type: ' + LogLevel[this.level];
-    ret += ' - Message: ' + this.message;
-    if (this.extraInfo.length) {
-      ret += ' - Extra Info: ' + LogEntry.formatParams(this.extraInfo);
-    }
-
-    return ret;
-  }
-
-  private static formatParams(params: any[]): string {
-    let ret: string = params.join(',');
-
-    if (params.some(p => typeof p === 'object')) {
-      ret = '';
-      for (const item of params) {
-        ret += JSON.stringify(item) + ',';
-      }
-    }
-
-    return ret;
-  }
-}
+export let isDebugMode = true;
+const noop = (): any => undefined;
 
 @Injectable()
 export class LoggerService {
-  // Public properties
-  level: LogLevel = LogLevel.All;
-  logWithDate = true;
 
-  private shouldLog(level: LogLevel): boolean {
-    let ret = false;
+  error(args: any) {
 
-    if (this.level !== LogLevel.Off && level >= this.level) {
-      ret = true;
-    }
-
-    return ret;
+    let stack = (new Error).stack;
+    //console.error.bind(console, arguments);
+    console.error(stack + args);
   }
 
-  debug(msg: string, ...optionalParams: any[]) {
-    this.writeToLog(msg, LogLevel.Debug, optionalParams);
-  }
-
-  info(msg: string, ...optionalParams: any[]) {
-    this.writeToLog(msg, LogLevel.Info, optionalParams);
-  }
-
-  warn(msg: string, ...optionalParams: any[]) {
-    this.writeToLog(msg, LogLevel.Warn, optionalParams);
-  }
-
-  error(msg: string, ...optionalParams: any[]) {
-    this.writeToLog(msg, LogLevel.Error, optionalParams);
-  }
-
-  fatal(msg: string, ...optionalParams: any[]) {
-    this.writeToLog(msg, LogLevel.Debug, optionalParams);
-  }
-
-  log(msg: any, ...optionalParams: any[]) {
-    this.writeToLog(msg, LogLevel.All, optionalParams);
-  }
-
-  private writeToLog(msg: string, level: LogLevel, params: any[]) {
-    if (this.shouldLog(level)) {
-      const entry: LogEntry = new LogEntry();
-
-      entry.message = msg;
-      entry.level = level;
-      entry.extraInfo = params;
-      entry.logWithDate = this.logWithDate;
-
-      console.log(entry);
-    }
-  }
 }
