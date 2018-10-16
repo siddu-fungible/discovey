9ba692d3f5538226b3f029c9a3dc60d9b0f1fbdf
John Abraham <john.abraham@fungible.com>
2018-10-15 23:10:14 -0700

    more pool commits

--- fun_test/web/angular/demo1/src/app/workflows/pools/pools.component.ts
+++ fun_test/web/angular/demo1/src/app/workflows/pools/pools.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActionGroup, DpuElement} from "../../dpus/dpus.component";
 import {FormControl} from "@angular/forms";
+import {animate, state, style, transition, trigger} from "@angular/animations";
+import {SelectionModel} from "@angular/cdk/collections";
 
 export interface PoolElement {
   id: number;
@@ -15,10 +17,44 @@ const ELEMENT_DATA: PoolElement[] = [
 ];
 
 
+export interface VolumeElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const VOLUME_DATA: VolumeElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'}
+];
+
+
 @Component({
   selector: 'app-pools',
   templateUrl: './pools.component.html',
-  styleUrls: ['./pools.component.css']
+  styleUrls: ['./pools.component.css'],
+    animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+    trigger('enterAnimation', [
+      transition(':enter', [
+        style({transform: 'translateX(100%)', opacity: 0}),
+        animate('500ms')
+      ]),
+      transition(':leave', [
+        style({transform: 'translateX(0)', opacity: 1}),
+        animate('500ms')
+      ])
+    ]),
+  trigger('nextStage', [
+      state('stage1', style({opacity: 1, 'width': '100%'})),
+      state('stage2', style({ opacity: 0, 'width': 0, 'visibility': 'hidden', transform: 'translateX(100%)'})),
+      transition('stage1 <=> stage2', animate('225ms')),
+    ])]
 })
 export class PoolsComponent implements OnInit {
   displayedColumns: string[] = ['name', 'capacity', 'volumes'];
@@ -26,6 +62,12 @@ export class PoolsComponent implements OnInit {
   actionControl = new FormControl();
   actionSelected: string = null;
   selectedRowIndex: number = null;
+    selection = new SelectionModel<PoolElement>(true, []);
+  displayedVolumesColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+
+    expandedElement: PoolElement;
+  volumes = VOLUME_DATA;
+
   constructor() { }
 
     actionGroups: ActionGroup[] = [
@@ -56,7 +98,7 @@ export class PoolsComponent implements OnInit {
     this.dataSource = [...this.dataSource];
     this.actionSelected = null;
     this.selectedRowIndex = this.dataSource.length - 1;
-    setTimeout(()=>{    //<<<---    using ()=> syntax
+    setTimeout(()=> {
       this.selectedRowIndex = null;
     }, 2000);
 
