4790b69ec3310010a2e255c7610d3b48e7bf9248
John Abraham <john.abraham@fungible.com>
2018-10-15 18:32:20 -0700

    add pools

---
+++ fun_test/web/angular/demo1/src/app/workflows/pools/pools.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import {ActionGroup} from "../../dpus/dpus.component";
+import {FormControl} from "@angular/forms";
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
+
+@Component({
+  selector: 'app-pools',
+  templateUrl: './pools.component.html',
+  styleUrls: ['./pools.component.css']
+})
+export class PoolsComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+    actionControl = new FormControl();
+  actionSelected: string = null;
+  constructor() { }
+
+    actionGroups: ActionGroup[] = [
+    {name: "Storage", actions: [{value: 1, viewValue: "Add a new pool"}]}
+  ];
+
+
+  ngOnInit() {
+  }
+
+    step = 0;
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+
+}
