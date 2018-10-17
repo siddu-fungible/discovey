3a5d4a9e43ac51789c1e093cd3f653eb82cfe975
Ash41291 <ashwin.s@fungible.com>
2018-10-17 13:25:24 -0700

    regresion files

---
+++ fun_test/web/angular/qadashboard/src/app/regression/regression.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-regression',
+  templateUrl: './regression.component.html',
+  styleUrls: ['./regression.component.css']
+})
+export class RegressionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
