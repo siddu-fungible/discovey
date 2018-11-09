2bbb3f1d97e76b97aec33bf450d382bca456af32
Ash41291 <ashwin.s@fungible.com>
2018-11-09 13:47:42 -0800

    cancel button component

---
+++ fun_test/web/angular/qadashboard/src/app/btn-link/btn-link.component.ts
@@ -0,0 +1,27 @@
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'cancel-btn',
+  templateUrl: './btn-link.component.html',
+  styleUrls: ['./btn-link.component.css']
+})
+export class BtnLinkComponent implements OnInit {
+@Input() text: string = "close";
+@Input() prompt: boolean = false;
+@Output() onClick = new EventEmitter();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  buttonClose(): void {
+    if(this.prompt) {
+      if(confirm("Are you sure, you want to "+this.text)) {
+        this.onClick.emit();
+      }
+    } else {
+      this.onClick.emit();
+    }
+  }
+
+}
--- fun_test/web/angular/qadashboard/src/app/dashboard/dashboard.component.ts
+++ fun_test/web/angular/qadashboard/src/app/dashboard/dashboard.component.ts
@@ -7,10 +7,18 @@ import { PerformanceComponent } from "../performance/performance.component";
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  testClose: boolean = true;
+  x: number = 20;
+  y: number = 30;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  buttonClose(x, y): void{
+    this.testClose = !this.testClose;
+    let sum = x + y;
+    console.log(sum);
+  }
 }
