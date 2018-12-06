6cd20e02889ac62f38d535e412fe05837d76d381
RP1807 <rushikesh.pendse@fungible.com>
2018-12-06 11:23:57 +0530

    Added an example of Network Controller Lib

---
+++ fun_test/scripts/examples/network_controller_example.py
@@ -0,0 +1,18 @@
+from lib.host.network_controller import *
+from argparse import ArgumentParser
+
+dpc_proxy_ip = "10.1.21.120"
+dpc_proxy_port = 40221
+try:
+    fun_test.log("Connecting to Remote DPC %s on port %d" % (dpc_proxy_ip, dpc_proxy_port))
+    network_controller_obj = NetworkController(dpc_server_ip=dpc_proxy_ip, dpc_server_port=dpc_proxy_port,
+                                               verbose=True)
+
+    psw_stats = network_controller_obj.peek_psw_global_stats()
+    print psw_stats
+
+    network_controller_obj.echo_hello()
+
+except Exception as ex:
+    print ex
+
