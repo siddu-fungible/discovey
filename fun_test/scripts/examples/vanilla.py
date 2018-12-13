afd5c6139d6b0671f256f8a170087639af02c4df
John Abraham <john.abraham@fungible.com>
2018-12-13 14:30:29 +0530

    add support for test-case-ids

--- fun_test/scripts/examples/vanilla.py
+++ fun_test/scripts/examples/vanilla.py
@@ -40,7 +40,49 @@ class FunTestCase1(FunTestCase):
 class FunTestCase2(FunTestCase):
     def describe(self):
         self.set_test_details(id=2,
-                              summary="Sanity Test 1",
+                              summary="Sanity Test 2",
+                              steps="""
+        1. Steps 1
+        2. Steps 2
+        3. Steps 3
+                              """)
+
+    def setup(self):
+        fun_test.log("Testcase setup")
+        fun_test.sleep("demo", seconds=1)
+
+    def cleanup(self):
+        fun_test.log("Testcase cleanup")
+
+    def run(self):
+        fun_test.log("Some log: vanilla 2")
+        fun_test.test_assert_expected(expected=2, actual=2, message="Some message2")
+
+class FunTestCase3(FunTestCase):
+    def describe(self):
+        self.set_test_details(id=3,
+                              summary="Sanity Test 3",
+                              steps="""
+        1. Steps 1
+        2. Steps 2
+        3. Steps 3
+                              """)
+
+    def setup(self):
+        fun_test.log("Testcase setup")
+        fun_test.sleep("demo", seconds=1)
+
+    def cleanup(self):
+        fun_test.log("Testcase cleanup")
+
+    def run(self):
+        fun_test.log("Some log: vanilla 2")
+        fun_test.test_assert_expected(expected=2, actual=2, message="Some message2")
+
+class FunTestCase4(FunTestCase):
+    def describe(self):
+        self.set_test_details(id=4,
+                              summary="Sanity Test 4",
                               steps="""
         1. Steps 1
         2. Steps 2
@@ -63,4 +105,6 @@ if __name__ == "__main__":
     myscript = MyScript()
     myscript.add_test_case(FunTestCase1())
     myscript.add_test_case(FunTestCase2())
+    myscript.add_test_case(FunTestCase3())
+    myscript.add_test_case(FunTestCase4())
     myscript.run()
