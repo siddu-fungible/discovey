7763816e83e8aae0c55bb251ad8801cc6c7f4bb2
RP1807 <rushikesh.pendse@fungible.com>
2019-01-17 14:15:51 +0530

    Added lib to display perf table and create output JSON. Fetching results using database method

--- fun_test/lib/templates/traffic_generator/spirent_rfc2544_template.py
+++ fun_test/lib/templates/traffic_generator/spirent_rfc2544_template.py
@@ -7,12 +7,13 @@ User needs to create .tcc spirent config with the test parameters and RFC-2544 w
 from lib.system.fun_test import *
 from lib.host.spirent_manager import *
 from lib.templates.traffic_generator.spirent_traffic_generator_template import *
+import sqlite3
 
 
 class Rfc2544Template(SpirentTrafficGeneratorTemplate):
     USER_WORKING_DIR = "USER_WORKING_DIR"
-    FRAME_SIZE_64 = "64"
-    FRAME_SIZE_1500 = "1500"
+    FRAME_SIZE_64 = "64.0"
+    FRAME_SIZE_1500 = "1500.0"
     FRAME_SIZE_IMIX = "IMIX"
     PASSED = "Passed"
     FAILED = "Failed"
@@ -87,10 +88,11 @@ class Rfc2544Template(SpirentTrafficGeneratorTemplate):
     def wait_until_complete(self):
         result = False
         try:
-            fun_test.log("Sequencer running...")
-            result_dict = self.stc_manager.wait_until_complete()
-            # TODO: check the status of it if finish
+            fun_test.log("Sequencer running... This might take little while")
+            status = self.stc_manager.wait_until_complete()
+            fun_test.simple_assert(status == 'PASSED', message="Ensure Sequencer stopped")
             fun_test.log("Sequencer Stopped...")
+            result = True
         except Exception as ex:
             fun_test.critical(str(ex))
         return result
@@ -158,11 +160,121 @@ class Rfc2544Template(SpirentTrafficGeneratorTemplate):
             fun_test.critical(str(ex))
         return records
 
-    def validate_performance_result(self, records):
+    def _get_base_db_path(self, db_path):
+        base_path = None
+        try:
+            base_path = '/'.join(db_path.split('/')[:7])
+        except Exception as ex:
+            fun_test.critical(str(ex))
+        return base_path
+
+    def _get_throughput_summary_db_list(self, base_path):
+        db_list = []
+        try:
+            run_dirs = os.listdir(base_path)
+            for run_dir in run_dirs:
+                if re.search(r'2544-Tput', run_dir.strip()):
+                    path = base_path + "/%s" % run_dir
+                    dbs = glob.glob(path + "/*.db")
+                    for db in dbs:
+                        if re.search(r'2544-Tput-Summary', db.strip()):
+                            db_list.append(db)
+        except Exception as ex:
+            fun_test.critical(str(ex))
+        return db_list
+
+    def get_throughput_summary_results_by_frame_size(self):
+        output = {"status": False}
+        result = OrderedDict()
+        result[self.FRAME_SIZE_64] = []
+        result[self.FRAME_SIZE_1500] = []
+        result[self.FRAME_SIZE_IMIX] = []
+        try:
+            spirent_path = self.retrieve_database_file_name()
+            # spirent_path = "/home/rushikesh/Spirent/TestCenter 4.81/Results/transit_bidirectional_palladium_2019-01-15_04-24-47/2544-Tput_2019-01-15_04-30-41/2544-Tput-Summary-2_2019-01-15_04-30-41.db"
+            base_path = self._get_base_db_path(db_path=spirent_path)
+            dbs = self._get_throughput_summary_db_list(base_path=base_path)
+            for db in dbs:
+                records = self.fetch_summary_result(db_name=db)
+                for record in records:
+                    data_dict = OrderedDict(record)
+                    if 'AvgFrameSize' in data_dict and self.FRAME_SIZE_64 == data_dict['AvgFrameSize']:
+                        result[self.FRAME_SIZE_64].append(data_dict)
+                    elif 'AvgFrameSize' in data_dict and self.FRAME_SIZE_1500 == data_dict['AvgFrameSize']:
+                        result[self.FRAME_SIZE_1500].append(data_dict)
+                    elif 'AvgFrameSize' in data_dict and self.FRAME_SIZE_IMIX == data_dict['AvgFrameSize']:
+                        result[self.FRAME_SIZE_IMIX].append(data_dict)
+            output['status'] = True
+            output['summary_result'] = result
+        except Exception as ex:
+            fun_test.critical(str(ex))
+        return output
+
+    def validate_result(self, result_dict):
+        result = False
+        try:
+            if self.FRAME_SIZE_64 in result_dict:
+                for record in result_dict[self.FRAME_SIZE_64]:
+                    fun_test.test_assert_expected(expected=self.PASSED, actual=record['Result'],
+                                                  message="Ensure Result for %s frame size trial num: %s" % (
+                                                      self.FRAME_SIZE_64, record['TrialNumber']))
+            if self.FRAME_SIZE_1500 in result_dict:
+                for record in result_dict[self.FRAME_SIZE_1500]:
+                    fun_test.test_assert_expected(expected=self.PASSED, actual=record['Result'],
+                                                  message="Ensure Result for %s frame size trial num: %s" % (
+                                                      self.FRAME_SIZE_1500, record['TrialNumber']))
+            if self.FRAME_SIZE_IMIX in result_dict:
+                for record in result_dict[self.FRAME_SIZE_IMIX]:
+                    fun_test.test_assert_expected(expected=self.PASSED, actual=record['Result'],
+                                                  message="Ensure Result for %s frame size trial num: %s" % (
+                                                      self.FRAME_SIZE_IMIX, record['TrialNumber']))
+            result = True
+        except Exception as ex:
+            fun_test.critical(str(ex))
+        return result
+
+    def _get_table_data(self, result_dict):
+        table_data = {'headers': [], 'rows': []}
+        try:
+            headers = []
+            exclude_header_list = ['UnexpectedRxSignatureFrames', 'MaxLatencyThresholdExceeded', 'FrameLoss',
+                                   'FloodFrameCount', 'ConfiguredFrameSize', 'OfferedLoad(fps)', 'FrameSizeType',
+                                   'OutOfSeqThresholdExceeded', 'IntendedLoad(Mbps)']
+            for key in result_dict:
+                if result_dict[key]:
+                    all_headers = result_dict[key][0].keys()
+                    headers = [val for val in all_headers if val not in exclude_header_list]
+                    break
+            rows = []
+            for key in result_dict:
+                for record in result_dict[key]:
+                    row = []
+                    for _key in record.keys():
+                        if _key in headers:
+                            row.append(record[_key])
+                    rows.append(row)
+            table_data = {'headers': headers, 'rows': rows}
+        except Exception as ex:
+            fun_test.critical(str(ex))
+        return table_data
+
+    def create_performance_table(self, result_dict, table_name):
+        result = False
+        try:
+            table_data = self._get_table_data(result_dict=result_dict)
+            fun_test.add_table(panel_header="RFC-2544 Performance Detailed Summary",
+                               table_name=table_name,
+                               table_data=table_data)
+            result = True
+        except Exception as ex:
+            fun_test.critical(str(ex))
+        return result
+
+    def validate_performance_result(self, summary_result):
         result = False
         try:
             checkpoint = "Verify RFC-2544 Result for each trial"
-            for record in records:
+            for record in summary_result:
                 data_dict = dict(record)
                 trial_num = data_dict['TrialNumber']
                 frame_size = data_dict['AvgFrameSize']
@@ -170,7 +282,7 @@ class Rfc2544Template(SpirentTrafficGeneratorTemplate):
                                               message="Ensure test result PASSED for trial %d and "
                                                       "frame size %s B" % (trial_num, frame_size))
             fun_test.add_checkpoint(checkpoint)
-
+            '''
             checkpoint = "Verify TxFrameCount == RxFrameCount and FrameLoss is 0 for each trial"
             for record in records:
                 data_dict = dict(record)
@@ -186,6 +298,7 @@ class Rfc2544Template(SpirentTrafficGeneratorTemplate):
                                               message="Ensure FrameLoss is 0 for trial %d and frame size %s B" % (
                                                   trial_num, frame_size))
             fun_test.add_checkpoint(checkpoint)
+            '''
             result = True
         except Exception as ex:
             fun_test.critical(str(ex))
@@ -204,66 +317,59 @@ class Rfc2544Template(SpirentTrafficGeneratorTemplate):
             fun_test.critical(str(ex))
         return records
 
-    def create_performance_table(self, records, table_name):
-        result = False
+    def _calculate_throughput_in_mbps(self, forwarding_rate, frame_size):
+        throughput = None
         try:
-            table_headers = []
-            for record in records:
-                for data in record:
-                    table_headers.append(data[0])
-
-            table_rows = []
-            for record in records:
-                row = []
-                for data in record:
-                    row.append(data[1])
-                table_rows.append(row)
-
-            table_data = {'headers': table_headers, 'rows': table_rows}
-            fun_test.add_table(panel_header="RFC-2544 Performance Detailed Summary",
-                               table_name=table_name,
-                               table_data=table_data)
-            result = True
+            throughput = float((forwarding_rate * frame_size) / 100000)
         except Exception as ex:
             fun_test.critical(str(ex))
-        return result
+        return throughput
 
-    def _is_frame_size_found(self, result_list, frame_size):
-        frame_size_found = False
+    def _get_max_forwarding_rate(self, records, frame_size):
+        max_rate_record = None
         try:
-            for result in result_list:
-                if 'frame_size' in result:
-                    if result['frame_size'] == frame_size:
-                        frame_size_found = True
-                        break
+            forwarding_rates = []
+            for record in records:
+                if float(record['AvgFrameSize']) == frame_size:
+                    forwarding_rates.append(record['ForwardingRate(fps)'])
+            max_rate = max(forwarding_rates)
+            for record in records:
+                if max_rate == record['ForwardingRate(fps)']:
+                    max_rate_record = record
+                    break
         except Exception as ex:
             fun_test.critical(str(ex))
-        return frame_size_found
+        return max_rate_record
 
-    def populate_performance_json_file(self, records, timestamp, flow_direction, mode=DUT_MODE_25G):
+    def populate_performance_json_file(self, result_dict, timestamp, flow_direction, mode=DUT_MODE_25G,
+                                       bidirectional=False):
         results = []
         try:
-            for record in records:
-                record_dict = dict(record)
+            for key in result_dict:
+                records = result_dict[key]
                 data_dict = OrderedDict()
                 data_dict['mode'] = mode
                 data_dict['version'] = fun_test.get_version()
                 data_dict['timestamp'] = timestamp
-                frame_size = int(record_dict['AvgFrameSize'])
-                if self._is_frame_size_found(result_list=results, frame_size=frame_size):
-                    pass
-                else:
+                frame_size = float(records[0]['AvgFrameSize']) if records else None
+                if frame_size:
                     data_dict['flow_type'] = flow_direction
                     data_dict['frame_size'] = frame_size
-                    data_dict['pps'] = record_dict['Forwarding Rate (fps)']
-                    data_dict['throughput'] = record_dict['Throughput (Mbps)']
-                    data_dict['latency_min'] = record_dict['Minimum Latency (us)']
-                    data_dict['latency_max'] = record_dict['Maximum Latency (us)']
-                    data_dict['latency_avg'] = record_dict['Average Latency (us)']
 
-                    data_dict['jitter_min'] = record_dict['Minimum Jitter (us)']
-                    data_dict['jitter_max'] = record_dict['Maximum Jitter (us)']
-                    data_dict['jitter_avg'] = record_dict['Average Jitter (us)']
+                    max_rate_record = self._get_max_forwarding_rate(records=records, frame_size=frame_size)
+                    data_dict['pps'] = float(max_rate_record['ForwardingRate(fps)'])
+                    throughput = self._calculate_throughput_in_mbps(forwarding_rate=data_dict['pps'],
+                                                                    frame_size=frame_size)
+                    if bidirectional:
+                        throughput = throughput / 2
+                    data_dict['throughput'] = throughput
+                    data_dict['latency_min'] = max_rate_record['MinimumLatency(us)']
+                    data_dict['latency_max'] = max_rate_record['MaximumLatency(us)']
+                    data_dict['latency_avg'] = max_rate_record['AverageLatency(us)']
+
+                    data_dict['jitter_min'] = max_rate_record['MinimumJitter(us)']
+                    data_dict['jitter_max'] = max_rate_record['MaximumJitter(us)']
+                    data_dict['jitter_avg'] = max_rate_record['AverageJitter(us)']
 
                     results.append(data_dict)
                     fun_test.debug(results)
@@ -282,6 +388,73 @@ class Rfc2544Template(SpirentTrafficGeneratorTemplate):
         except Exception as ex:
             fun_test.critical(str(ex))
         return results
+
+# TODO: We might need this sqlite wrapper later on to fetch more detail test data
+
+
+class SqliteWrapper(object):
+
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.conn = None
+        self._connect()
+        self.cursor = None
+        self._get_cursor()
+
+    def _connect(self):
+        try:
+            if not self.conn:
+                self.conn = sqlite3.connect(self.db_path)
+        except Exception as ex:
+            fun_test.critical(str(ex))
+
+    def _get_cursor(self):
+        try:
+            if not self.cursor:
+                self.cursor = self.conn.cursor()
+        except Exception as ex:
+            fun_test.critical(str(ex))
+
+    def get_max_fps(self):
+        pass
+
+    def _create_query(self, column_list, table_name):
+        query = None
+        try:
+            columns = ', '.join(column_list)
+            query = "select %s from %s" % (columns, table_name)
+            fun_test.log("Running query: %s" % query)
+        except Exception as ex:
+            fun_test.critical(str(ex))
+        return query
+
+    def fetch_throughput_results(self):
+        results = []
+        try:
+            columns = ['TrialNum', 'IterationNum', 'FrameSize', 'FrameSizeType', 'Result',
+                       'IntendedFPSLoad', 'IntendedMbpsLoad', 'OfferedFPSLoad', 'OfferedMbpsLoad', 'LoadSize',
+                       'LoadSizeType', 'TxFrameCount', 'RxFrameCount', 'ExpectedFrameCount',
+                       'FrameLoss', 'PercentLoss', 'TxFrameRate', 'ForwardingRate', 'ThroughputRate', 'Throughput',
+                       'MinLatency', 'AvgLatency', 'MaxLatency', 'MinJitter', 'AvgJitter', 'MaxJitter', 'DurationinSec']
+
+            query = self._create_query(column_list=columns, table_name="Rfc2544ThroughputPerFrameSizeResult")
+
+            for row in self.cursor.execute(query):
+                index = 0
+                data_dict = OrderedDict()
+                for col in columns:
+                    data_dict[col] = row[index]
+                    index += 1
+                results.append(data_dict)
+        except Exception as ex:
+            fun_test.critical(str(ex))
+        return results
+
+
+
+
+
+
 
 
 
--- fun_test/scripts/networking/performance/test_performance_using_rfc2544.py
+++ fun_test/scripts/networking/performance/test_performance_using_rfc2544.py
@@ -8,6 +8,7 @@ network_controller_obj = None
 spirent_config = None
 TIMESTAMP = None
 
+
 class ScriptSetup(FunTestScript):
 
     def describe(self):
@@ -64,17 +65,19 @@ class TestTransitPerformance(FunTestCase):
     tc_id = 1
     template_obj = None
     flow_direction = NuConfigManager.FLOW_DIRECTION_NU_NU
-    # tcc_config_path = "/tmp/transit_bidirectional_palladium.tcc"
-    tcc_config_path = "/tmp/test_all_frame_sizes.tcc"
+    summary = "%s RFC-2544 Bi-directional" % flow_direction
+    # tcc_file_name = "transit_bidirectional_palladium_64_only.tcc"
+    tcc_file_name = "transit_bidirectional_palladium.tcc"
+    bidirectional = True
 
     def describe(self):
-        self.set_test_details(id=1, summary="%s RFC-2544 Latency/Throughput Bi-directional" % self.flow_direction,
+        self.set_test_details(id=self.tc_id, summary=self.summary,
                               steps="""
-                              1. Dump PSW, BAM stats before tests 
+                              1. Dump PSW, BAM and vppkts stats before tests 
                               2. Initialize RFC-2544 and load existing tcc configuration 
                               3. Start Sequencer
                               4. Wait for sequencer to complete
-                              5. Dump PSW, BAM stats after tests
+                              5. Dump PSW, BAM and vppkts stats after tests
                               5. Fetch Results and validate that test result for each frame size [64, 1500, IMIX]
                               """)
 
@@ -83,14 +86,22 @@ class TestTransitPerformance(FunTestCase):
         self.template_obj = Rfc2544Template(spirent_config=spirent_config)
         fun_test.test_assert(self.template_obj, checkpoint)
 
+        tcc_config_path = fun_test.get_helper_dir_path() + '/palladium_configs/%s_flow/%s' % (
+            self.flow_direction.lower(), self.tcc_file_name)
+
         checkpoint = "Load existing tcc configuration"
-        result = self.template_obj.setup(tcc_config_path=self.tcc_config_path)
+        result = self.template_obj.setup(tcc_config_path=tcc_config_path)
         fun_test.test_assert(result['result'], checkpoint)
 
     def run(self):
-        fun_test.log("----------------> Start RFC-2544 test for %s Flow <----------------" % self.flow_direction)
+        fun_test.log("----------------> Start RFC-2544 test using %s <----------------" % self.tcc_file_name)
+
+        # TODO: Dump Per VP stats before and after traffic
         fun_test.log("Fetching PSW Global stats before test")
         network_controller_obj.peek_psw_global_stats()
+
+        fun_test.log("Fetching VP packets before test")
+        network_controller_obj.peek_vp_packets()
 
         fun_test.log("Fetching BAM stats before test")
         network_controller_obj.peek_bam_stats()
@@ -107,45 +118,64 @@ class TestTransitPerformance(FunTestCase):
         result = self.template_obj.wait_until_complete()
         fun_test.test_assert(result, checkpoint)
 
-        fun_test.sleep('After traffic before fetching DUT stats', seconds=5)
-
         fun_test.log("Fetching PSW Global stats before test")
         network_controller_obj.peek_psw_global_stats()
+
+        fun_test.log("Fetching VP packets after test")
+        network_controller_obj.peek_vp_packets()
 
         fun_test.log("Fetching BAM stats before test")
         network_controller_obj.peek_bam_stats()
 
-        checkpoint = "Fetch DB name to read stats"
-        db_name = self.template_obj.retrieve_database_file_name()
-        fun_test.simple_assert(db_name, checkpoint)
-
         checkpoint = "Fetch summary result for latency and throughput for all frames and all iterations"
-        records = self.template_obj.fetch_summary_result(db_name=db_name)
-        fun_test.test_assert(records, checkpoint)
+        result_dict = self.template_obj.get_throughput_summary_results_by_frame_size()
+        fun_test.test_assert(result_dict['status'], checkpoint)
 
         checkpoint = "Validate results of current run"
-        result = self.template_obj.validate_performance_result(records=records)
+        result = self.template_obj.validate_result(result_dict=result_dict['summary_result'])
         fun_test.test_assert(result, checkpoint)
 
         checkpoint = "Display Performance Table"
-        result = self.template_obj.create_performance_table(records=records,
-                                                            table_name="Performance Numbers for %s flow "
-                                                                       "(Bi-directional)" % self.flow_direction)
+        table_name = "Performance Numbers for %s flow " % self.flow_direction
+        if self.bidirectional:
+            table_name += " (Bi-directional)"
+        result = self.template_obj.create_performance_table(result_dict=result_dict['summary_result'],
+                                                            table_name=table_name)
+        fun_test.simple_assert(result, checkpoint)
+        checkpoint = "Ensure output JSON populated for performance dashboard"
+        result = self.template_obj.populate_performance_json_file(result_dict=result_dict['summary_result'],
+                                                                  timestamp=TIMESTAMP,
+                                                                  flow_direction=self.flow_direction,
+                                                                  bidirectional=self.bidirectional)
         fun_test.simple_assert(result, checkpoint)
 
-        checkpoint = "Ensure output JSON populated for performance dashboard"
-        result = self.template_obj.populate_performance_json_file(records=records, timestamp=TIMESTAMP,
-                                                                  flow_direction=self.flow_direction)
-        fun_test.simple_assert(result, checkpoint)
-        fun_test.log("----------------> End RFC-2544 test for %s Flow <----------------" % self.flow_direction)
+        fun_test.log("----------------> End RFC-2544 test using %s  <----------------" % self.tcc_file_name)
 
     def cleanup(self):
         self.template_obj.cleanup()
+
+
+class TestNuHnuPerformance(TestTransitPerformance):
+    tc_id = 2
+    flow_direction = NuConfigManager.FLOW_DIRECTION_FPG_HNU
+    summary = "%s RFC-2544" % flow_direction
+    tcc_file_name = "nu_hnu_palladium.tcc"
+    bidirectional = False
+
+
+class TestHnuNuPerformance(TestTransitPerformance):
+    tc_id = 3
+    flow_direction = NuConfigManager.FLOW_DIRECTION_HNU_FPG
+    summary = "%s RFC-2544" % flow_direction
+    tcc_file_name = ""
+    bidirectional = False
 
 
 if __name__ == '__main__':
     ts = ScriptSetup()
 
     ts.add_test_case(TestTransitPerformance())
+    ts.add_test_case(TestNuHnuPerformance())
+    ts.add_test_case(TestHnuNuPerformance())
 
     ts.run()
