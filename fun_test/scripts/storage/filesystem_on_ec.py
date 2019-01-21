45cb5ae2ce0d7ba106e0a585315598893f55aa78
Alagarswamy Devaraj <alagarswamy.devaraj@fungible.com>
2019-01-21 21:38:14 +0530

    * Adding a sleep between EC volume creation and reloading the nvme driver

--- fun_test/scripts/storage/filesystem_on_ec.py
+++ fun_test/scripts/storage/filesystem_on_ec.py
@@ -352,6 +352,7 @@ class FSOnECTestcase(FunTestCase):
                 fun_test.test_assert_expected(actual=int(command_result["data"]["error_inject"]), expected=0,
                                               message="Ensuring error_injection got disabled")
 
+                fun_test.sleep("Waiting for FunOS to create the EC volume", 10)
                 # Reloading the nvme driver before checking the disk
                 if self.reload_after_config:
                     command_result = self.host.nvme_restart()
