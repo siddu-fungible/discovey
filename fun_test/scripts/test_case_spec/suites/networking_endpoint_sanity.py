6d92a90024b97b8e2ae7c29b1f3d4281924c8cc5
George Liang <george.liang@fungible.com>
2019-01-23 21:23:55 -0800

    Add endpoint test suites

--- fun_test/scripts/test_case_spec/suites/networking_endpoint_sanity.py
+++ fun_test/scripts/test_case_spec/suites/networking_endpoint_sanity.py
@@ -1,4 +1,6 @@
 [
-  {"path": "networking/funeth/sanity.py"}
+  {"info": {"tags": ["networking-endpoint-sanity"]}},
+  {"path": "networking/funeth/sanity.py"},
+  {"path": "networking/endpoint/endpoint.py"}
 
 ]
