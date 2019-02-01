3cf3dd2f8b10ff01d434906e36b7da4ff65137ff
George Liang <george.liang@fungible.com>
2019-01-31 20:08:42 -0800

    Update username

--- fun_test/scripts/networking/ptf/ptf.py
+++ fun_test/scripts/networking/ptf/ptf.py
@@ -23,7 +23,7 @@ class PTFTestSuite(FunTestScript):
                               """)
 
     def setup(self):
-        linux_obj = Linux(host_ip='localhost', ssh_username=REGRESSION_USER, ssh_password=REGRESSION_USER_PASSWORD)
+        linux_obj = Linux(host_ip='localhost', ssh_username='localadmin', ssh_password='Precious1*')
         workspace = '%s/tmp/' % os.getenv('HOME')
         linux_obj.command('WSTMP=$WORKSPACE; export WORKSPACE=%s' % workspace)
         funcp_obj = funcp.FunControlPlane(linux_obj, ws=workspace)
