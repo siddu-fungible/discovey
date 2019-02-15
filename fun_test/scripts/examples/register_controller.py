3710a0f92d744a796c390f3c0c1db63a97e8f1ee
Onkar Sarmalkar <onkar.sarmalkar@fungible.com>
2019-02-16 01:41:18 +0530

    Changes for displaying bits

--- fun_test/scripts/examples/register_controller.py
+++ fun_test/scripts/examples/register_controller.py
@@ -2,6 +2,9 @@ from lib.system.fun_test import *
 from lib.host.dpcsh_client import DpcshClient
 import time
 
+hsu_pwp_core0_csr_test_outl_dict = {"LTSSM state": [96, 100], "Data link layer": [101], "TLP transmission": [102],
+                     "DLLP Transmission": [103], "Low-Power state": [104, 107]}
+
 class RegisterController(DpcshClient):
     VERB_TYPE_PEEK = 'peek'
     VERB_TYPE_POKE = 'poke'
@@ -37,9 +40,9 @@ class RegisterController(DpcshClient):
         return result
 
     def form_args(self, register_name, rinst, ring="hsu", field=None):
-        args = "{name: %s, ring:%s, rinst:%s," % (register_name, ring, rinst)
+        args = "{name: %s, ring:%s, rinst:%s" % (register_name, ring, rinst)
         if field:
-            args = args + " field:%s" % field
+            args = args + ", field:%s" % field
         args = args + "}"
         return args
 
@@ -51,3 +54,37 @@ class RegisterController(DpcshClient):
             fun_test.log("Output seen for register '%s' with field '%s' is '%s'" % (register_name, field, output['data']))
         else:
             fun_test.log("Output seen for register '%s' is '%s'" % (register_name, output['data']))
+        if register_name == self.hsu_pwp_core0_csr_test_outl and rinst == 3 and field == "csr_test_outl":
+            self.print_simplified_data(register_name, output['data'][2], hsu_pwp_core0_csr_test_outl_dict)
+
+    def _convert_decimal_to_binary(self, decimal, zfill_val):
+        result = None
+        try:
+            binary = bin(decimal)
+            to_add = zfill_val - len(binary)
+            to_prepend = to_add + 2
+            zeros = '0' * to_prepend
+            result = binary.replace('0b', zeros)
+        except Exception as ex:
+            fun_test.critical(str(ex))
+        return result
+
+    def extract_value(self, output, start_index, end_index=None):
+        if end_index:
+            return output[start_index:end_index]
+        else:
+            return output[start_index]
+
+    def print_simplified_data(self, register_name, decimal_data, display_bits_dict):
+        if register_name == self.hsu_pwp_core0_csr_test_outl:
+            zfill_val = 64
+            binary_output = self._convert_decimal_to_binary(decimal_data, zfill_val=zfill_val)
+            binary_output = binary_output[::-1]
+            for key, val in display_bits_dict.iteritems():
+                start_index = val[0] - zfill_val
+                end_index = None
+                if len(val) == 2:
+                    start_index = val[0] - zfill_val
+                    end_index = val[1] - zfill_val + 1
+                binary_bits = self.extract_value(binary_output, start_index,end_index)
+                fun_test.log("Output for %s in range %s in binary bits is %s" % (key, val[::-1], binary_bits[::-1]))
