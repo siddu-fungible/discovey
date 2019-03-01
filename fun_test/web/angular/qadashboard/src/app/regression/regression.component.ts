019a7f6dfc74b6c734d56446a7ef806ccb08b909
John Abraham <john.abraham@fungible.com>
2019-02-28 22:51:15 -0800

    add a time localization service

--- fun_test/web/angular/qadashboard/src/app/regression/regression.component.ts
+++ fun_test/web/angular/qadashboard/src/app/regression/regression.component.ts
@@ -144,13 +144,7 @@ export class RegressionComponent implements OnInit {
   };
 
   trimTime(t) {
-    //console.log(new Date().toLocaleString());
-    //return t.toLocaleString().replace(/\..*$/, "").replace(/T/, " ");;
-    //return t.replace(/\..*$/, "").replace(/T/, " ");
-    let d = new Date(t.replace(/\s+/g, 'T'));
-    let epochValue = d.getTime();
-    return new Date(epochValue).toLocaleString().replace(/\..*$/, "");
-
+    return this.regressionService.convertToLocalTimezone(t).toLocaleString().replace(/\..*$/, "");
   }
 
   getSuiteDetail(suiteId) {
