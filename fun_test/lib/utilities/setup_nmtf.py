14ab32cb1bf00bb6941934ba20661a09646a0eb2
Yajat Singh <yajat.singh@fungible.com>
2019-03-06 16:00:00 +0530

    Add SDK to Workspace

--- fun_test/lib/utilities/setup_nmtf.py
+++ fun_test/lib/utilities/setup_nmtf.py
@@ -7,7 +7,7 @@ from lib.system.fun_test import *
 
 
 def setup_nmtf(username='auto_admin', password='fun123', host_ip="localhost", localhost=False):
-    linux_obj = Linux(host_ip=host_ip, ssh_username=username, ssh_password=password, localhost=localhost)
+    linux_obj = Linux(host_ip=host_ip, ssh_username=username, ssh_password=password)
     workspace = '/tmp'
     linux_obj.command('WSTMP=$WORKSPACE; export WORKSPACE=%s' % workspace)
     funcp_obj = funcp.FunControlPlane(linux_obj, ws=workspace)
@@ -15,10 +15,10 @@ def setup_nmtf(username='auto_admin', password='fun123', host_ip="localhost", lo
     fun_test.test_assert(funcp_obj.clone(), 'git clone FunControlPlane repo')
     fun_test.test_assert(funcp_obj.pull(), 'git pull FunControlPlane repo')
     fun_test.test_assert(funcp_obj.get_prebuilt(), 'Get FunControlPlane prebuilt pkg')
-    fun_test.test_assert(funcp_obj.make_gen_files(), "Gen Files")
     fun_test.test_assert(funsdk_obj.clone(), 'git clone FunSDK repo')
     fun_test.test_assert(funsdk_obj.sdkup(), 'FunSDK script/bob --sdkup')
+    fun_test.test_assert(funcp_obj.make_gen_files(), "Gen Files")
 
 
 if __name__ == "__main__":
-    setup_nmtf(username='yajat', password='messi3006', host_ip="localhost", localhost=True)
+    setup_nmtf(username='yajat', password='messi3006', host_ip="localhost", localhost=False)
