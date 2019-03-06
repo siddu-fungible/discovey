7c98e2c89b091ec55be38128e13665bef19df294
Ash41291 <ashwin.s@fungible.com>
2019-03-06 15:25:02 -0800

    added cache valid as false

--- fun_test/web/fun_test/db_fixup.py
+++ fun_test/web/fun_test/db_fixup.py
@@ -441,9 +441,9 @@ if __name__ == "__main__":
     # total_chart = MetricChart.objects.get(metric_model_name="MetricContainer", internal_chart_name="Networking_Teramarks")
     # prepare_status(chart=total_chart, purge_old_status=False, cache_valid=False)
     total_chart = MetricChart.objects.get(metric_model_name="MetricContainer", chart_name="Total")
-    prepare_status(chart=total_chart, purge_old_status=False, cache_valid=True)
+    prepare_status(chart=total_chart, purge_old_status=False, cache_valid=False)
     all_metrics_chart = MetricChart.objects.get(metric_model_name="MetricContainer", internal_chart_name="All metrics")
-    prepare_status(chart=all_metrics_chart, purge_old_status=False, cache_valid=True)
+    prepare_status(chart=all_metrics_chart, purge_old_status=False, cache_valid=False)
 
 if __name__ == "__main2__":
     pass
