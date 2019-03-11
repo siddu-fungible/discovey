a07a66976e889ce982c0e72ada7adc9872099f61
John Abraham <john.abraham@fungible.com>
2019-03-11 01:14:46 -0700

    add queue viewer

---
+++ fun_test/web/angular/qadashboard/src/app/regression/queue-viewer/queue-viewer.component.ts
@@ -0,0 +1,151 @@
+import { Component, OnInit } from '@angular/core';
+import {ApiService} from "../../services/api/api.service";
+import {Observable, of, interval} from "rxjs";
+import {switchMap, switchMapTo} from "rxjs/operators";
+import {LoggerService} from "../../services/logger/logger.service";
+
+class JobSpec {
+  suite_type: string;
+  script_path: string;
+  suite_path: string;
+  emails: string[];
+
+}
+
+
+class QueueEntry {
+  job_id: number;
+  job_spec: JobSpec;
+  priority: number;
+  test_bed_type: string;
+}
+
+
+
+@Component({
+  selector: 'app-queue-viewer',
+  templateUrl: './queue-viewer.component.html',
+  styleUrls: ['./queue-viewer.component.css']
+})
+export class QueueViewerComponent implements OnInit {
+  priorityCategories = ["high", "normal", "low"];
+  priorityRanges: object = {};
+  queueOccupancy = {};
+  highPriorityQueueOccupancy: QueueEntry[] = [];
+  normalPriorityQueueOccupancy: QueueEntry[] = [];
+  lowPriorityQueueOccupancy: QueueEntry[] = [];
+
+  constructor(private apiService: ApiService, private loggerService: LoggerService) { }
+
+  ngOnInit() {
+    this.priorityCategories.forEach(category => {
+      this.queueOccupancy[category] = [];
+    });
+
+    this.getQueueInfo().subscribe(response => {
+    });
+  }
+
+  getQueueInfo(): Observable<boolean> {
+    return new Observable(observer => {
+      observer.next(true);
+
+      //observer.complete();
+      //observer.error("Error");
+      return () => {
+      };
+    }).pipe(switchMap(() => {
+      return this.getPriorityRanges();
+    }), switchMap(()=> {
+      return interval(5000).pipe(switchMap ( () => {
+        return this.getCurrentQueueOccupancy();
+      }))
+    }));
+  }
+
+
+  getPriorityRanges(): Observable<boolean> {
+    return this.apiService.get('/regression/scheduler/queue_priorities').pipe(switchMap((response) => {
+      let responseData = response.data;
+      let ranges = responseData.RANGES;
+      for (let key in ranges) {
+        let [low, high] = ranges[key];
+        this.priorityRanges[key] = {low: low, high: high};
+        let i = 0;
+      }
+      return of(true);
+    }));
+
+  }
+
+  getPriorityCategory(priority): string {
+    let category: string = null;
+    for (let key in this.priorityRanges) {
+      if (priority >= this.priorityRanges[key].low && priority <= this.priorityRanges[key].high) {
+        category = key;
+        break;
+      }
+    }
+    return category;
+  }
+
+  getCurrentQueueOccupancy(): Observable<boolean> {
+
+    return this.apiService.get('/regression/scheduler/queue').pipe(switchMap(response => {
+
+      this.priorityCategories.forEach((category) => {
+        this.queueOccupancy[category] = [];
+      });
+
+      let queueOccupancy = response.data;
+      for (let i = 0; i < queueOccupancy.length; i++) {
+        let oneQueueEntry = queueOccupancy[i] as QueueEntry;
+        const category = this.getPriorityCategory(oneQueueEntry.priority);
+        let categoryLower = category.toLowerCase();
+        if (categoryLower === 'normal') {
+          this.queueOccupancy['normal'].push(oneQueueEntry);
+        } else if (categoryLower === 'high') {
+          this.queueOccupancy['high'].push(oneQueueEntry);
+        } else if (categoryLower === 'low') {
+          this.queueOccupancy['low'].push(oneQueueEntry);
+        }
+      }
+      return of(true);
+    }))
+
+  }
+
+  toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+  }
+
+  moveUpDown(jobId, up=true) {
+    let payload = {job_id: jobId};
+    if (up) {
+      payload["operation"] = "move_up";
+    } else {
+      payload["operation"] = "move_down";
+    }
+    this.apiService.post('/regression/scheduler/queue', payload).subscribe(response => {
+      this.loggerService.success("Priority modification submitted");
+    }, error => {
+      this.loggerService.error(`Unable to change the priority of ${jobId}`);
+    })
+  }
+
+  moveToTop(jobId) {
+    let payload = {job_id: jobId, operation: "move_to_top"};
+    this.apiService.post('/regression/scheduler/queue', payload).subscribe(response => {
+      this.loggerService.success("Priority modification submitted");
+    }, error => {
+      this.loggerService.error(`Unable to change the priority of ${jobId} to top`);
+    })
+
+  }
+
+}
