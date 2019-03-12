e754da2dca3b9fb348ae24edcb1a6b03a2ca6350
RP1807 <rushikesh.pendse@fungible.com>
2019-03-12 15:33:06 +0530

    snapshot lib fixes

--- fun_test/scripts/networking/snapshot_helper.py
+++ fun_test/scripts/networking/snapshot_helper.py
@@ -1,6 +1,7 @@
 from dpcsh.dpcsh_client import DpcshClient
 from dpcsh.snapshot import Snapshot
 from lib.system.fun_test import *
+from lib.utilities.setup_nmtf import WORKSPACE
 
 
 def setup_snapshot(smac=None, psw_stream=None, stream=None, unit=None, dpc_tcp_proxy_ip='127.0.0.1', dpc_tcp_proxy_port=40221):
@@ -8,6 +9,10 @@ def setup_snapshot(smac=None, psw_stream=None, stream=None, unit=None, dpc_tcp_p
     global snapshot_obj
     dpc_client_obj = None
     snapshot_obj = None
+
+    workspace = fun_test.get_environment_variable(variable="WORKSPACE")
+    if not workspace or workspace != WORKSPACE:
+        os.environ['WORKSPACE'] = WORKSPACE
 
     if dpc_client_obj is None:
         dpc_client_obj = DpcshClient(target_ip=dpc_tcp_proxy_ip, target_port=dpc_tcp_proxy_port,
@@ -42,6 +47,7 @@ def exit_snapshot():
         return snapshot_obj.do_exit()
     else:
         return None
+
 
 def get_snapshot_main_sfg(snapshot_output, prv=False, md=False, frv=False, psw_ctl=False):
     result = {}
