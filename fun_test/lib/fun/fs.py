90d6202051b48de35ce21ff4f35a46f96414598f
John Abraham <john.abraham@fungible.com>
2019-03-18 15:38:16 -0700

    more fs additions

--- fun_test/lib/fun/fs.py
+++ fun_test/lib/fun/fs.py
@@ -34,17 +34,74 @@ class BootPhases:
 
 
 class F1InFs:
+    SERIAL_SPEED_DEFAULT = 1000000
+    ELF_ADDRESS = "0xffffffff99000000"
     def __init__(self, bmc, serial_device_path, serial_sbp_device_path):
         self.bmc = bmc.clone()
+        self.bmc._connect()
         self.serial_device_path = serial_device_path
         self.serial_sbp_device_path = serial_sbp_device_path
 
     def bootup(self):
-        pass
+        fun_test.test_assert(self.u_boot_load_image(), "u-boot load image")
+        return True
+
+    def set_boot_phase(self, phase):
+        self.boot_phase = phase
+        fun_test.log_section(phase)
+
+    def u_boot_load_image(self,
+                          tftp_load_address="0xa800000080000000",
+                          tftp_server=TFTP_SERVER):
+        result = None
+        handle = self.bmc.handle
+        self.set_boot_phase(BootPhases.U_BOOT_INIT)
+        handle.sendline("microcom -s {} {}".format(self.SERIAL_SPEED_DEFAULT, self.serial_device_path))
+        handle.sendline("\n")
+        i = handle.expect('f1 # ', timeout=20)
+        self.set_boot_phase(BootPhases.U_BOOT_MICROCOM_STARTED)
+
+        handle.sendline("lfw; lmpg; ltrain; lstatus")
+        i = handle.expect('Fifo Out of Reset')
+        i = handle.expect("f1 # ")
+        self.set_boot_phase(BootPhases.U_BOOT_TRAIN)
+
+        handle.sendline("setenv bootargs app=hw_hsu_test sku=SKU_FS1600_0")
+        i = handle.expect("f1 # ")
+        self.set_boot_phase(BootPhases.U_BOOT_SET_BOOT_ARGS)
+
+        handle.sendline("dhcp")
+        i = handle.expect("f1 # ")
+        self.set_boot_phase(BootPhases.U_BOOT_DHCP)
+
+        handle.sendline("tftpboot {} {}:funos-f1.stripped.gz".format(tftp_load_address, tftp_server))
+        i = handle.expect(r'Bytes transferred = (\d+)')
+        bytes_transferred = handle.match.group(1)
+        self.set_boot_phase(BootPhases.U_BOOT_TFTP_DOWNLOAD)
+
+        handle.sendline("unzip {} {};".format(tftp_load_address, self.ELF_ADDRESS))
+        i = handle.expect(r'Uncompressed size: (\d+) =')
+        uncompressed_size = handle.match.group(1)
+        self.set_boot_phase(BootPhases.U_BOOT_UNCOMPRESS_IMAGE)
+
+        handle.sendline("bootelf -p {}".format(self.ELF_ADDRESS))
+        i = handle.expect(r'Welcome to FunOS')
+        i = handle.expect(r'Version=(\S+), Branch=(\S+)')
+        version = handle.match.group(1)
+        branch = handle.match.group(2)
+        self.set_boot_phase(BootPhases.U_BOOT_ELF)
+
+        i = handle.expect(r'NETWORK_STARTED')
+        self.set_boot_phase(BootPhases.F1_BOOT_NETWORK_STARTED)
+
+        i = handle.expect(r'PCI_STARTED')
+        self.set_boot_phase(BootPhases.F1_BOOT_PCI_STARTED)
+
+        result = True
+        return result
 
 
 class Fs():
-    ELF_ADDRESS = "0xffffffff99000000"
     BMC_SCRIPT_DIRECTORY = "/mnt/sdmmc0p1/scripts"
 
     def __init__(self, bmc_mgmt_ip, bmc_mgmt_ssh_username, bmc_mgmt_ssh_password):
@@ -70,12 +127,11 @@ class Fs():
                   bmc_mgmt_ssh_username=bmc_spec["mgmt_ssh_username"],
                   bmc_mgmt_ssh_password=bmc_spec["mgmt_ssh_password"])
 
-    def set_boot_phase(self, phase):
-        self.boot_phase = phase
-        fun_test.log_section(phase)
-
     def bootup(self):
         fun_test.simple_assert(self.bmc_initialize(), "BMC initialize")
+        for f1_index, f1 in self.f1s.iteritems():
+            fun_test.add_checkpoint("Booting up f1: {}".format(f1_index))
+            fun_test.test_assert(f1.bootup(), "Bootup f1: {} complete".format(f1_index))
 
         # fun_test.
 
@@ -122,55 +178,7 @@ class Fs():
         fun_test.simple_assert(len(self.f1s.keys()), "Both F1 device paths found")
         return result
 
-    def u_boot_load_image(self,
-                          handle,
-                          tftp_load_address="0xa800000080000000",
-                          tftp_server=TFTP_SERVER):
-        result = None
-        self.set_boot_phase(BootPhases.U_BOOT_INIT)
-        handle.sendline("microcom -s 1000000 /dev/ttyS0")
-        handle.sendline("\n")
-        i = handle.expect('f1 # ', timeout=20)
-        self.set_boot_phase(BootPhases.U_BOOT_MICROCOM_STARTED)
 
-        handle.sendline("lfw; lmpg; ltrain; lstatus")
-        i = handle.expect('Fifo Out of Reset')
-        i = handle.expect("f1 # ")
-        self.set_boot_phase(BootPhases.U_BOOT_TRAIN)
-
-        handle.sendline("setenv bootargs app=hw_hsu_test sku=SKU_FS1600_0")
-        i = handle.expect("f1 # ")
-        self.set_boot_phase(BootPhases.U_BOOT_SET_BOOT_ARGS)
-
-        handle.sendline("dhcp")
-        i = handle.expect("f1 # ")
-        self.set_boot_phase(BootPhases.U_BOOT_DHCP)
-
-        handle.sendline("tftpboot {} {}:funos-f1.stripped.gz".format(tftp_load_address, tftp_server))
-        i = handle.expect(r'Bytes transferred = (\d+)')
-        bytes_transferred = handle.match.group(1)
-        self.set_boot_phase(BootPhases.U_BOOT_TFTP_DOWNLOAD)
-
-        handle.sendline("unzip {} {};".format(tftp_load_address, self.ELF_ADDRESS))
-        i = handle.expect(r'Uncompressed size: (\d+) =')
-        uncompressed_size = handle.match.group(1)
-        self.set_boot_phase(BootPhases.U_BOOT_UNCOMPRESS_IMAGE)
-
-        handle.sendline("bootelf -p {}".format(self.ELF_ADDRESS))
-        i = handle.expect(r'Welcome to FunOS')
-        i = handle.expect(r'Version=(\S+), Branch=(\S+)')
-        version = handle.match.group(1)
-        branch = handle.match.group(2)
-        self.set_boot_phase(BootPhases.U_BOOT_ELF)
-
-        i = handle.expect(r'NETWORK_STARTED')
-        self.set_boot_phase(BootPhases.F1_BOOT_NETWORK_STARTED)
-
-        i = handle.expect(r'PCI_STARTED')
-        self.set_boot_phase(BootPhases.F1_BOOT_PCI_STARTED)
-
-        result = True
-        return result
 
 
 fs_json = ASSET_DIR + "/fs.json"
