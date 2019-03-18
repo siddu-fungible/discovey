a912726cd8e34b0efc5f20828d51dfa9ab32fe29
John Abraham <john.abraham@fungible.com>
2019-03-18 14:56:02 -0700

    fs initial commit

--- fun_test/lib/fun/fs.py
+++ fun_test/lib/fun/fs.py
@@ -1,20 +1,22 @@
 from lib.system.fun_test import fun_test
+from lib.system.utils import parse_file_to_json
 from lib.host.linux import Linux
-from fun_settings import TFTP_SERVER
+from fun_settings import TFTP_SERVER, ASSET_DIR
+import re
 
-BMC_IP = "10.1.20.149"
-BMC_USERNAME = "sysadmin"
-BMC_PASSWORD = "superuser"
-SCRIPT_DIRECTORY = "/mnt/sdmmc0p1/scripts"
-l = Linux(host_ip=BMC_IP, ssh_username=BMC_USERNAME, ssh_password=BMC_PASSWORD)
-l.set_prompt_terminator(r'# $')
-l.command("cd {}".format(SCRIPT_DIRECTORY))
-l.command("./f1_uartmux.sh 1")
-l.command("./f1_console.sh 1")
-process_id = l.get_process_id_by_pattern("microcom ")
-if process_id:
-    l.kill_process(process_id)
-process_id = l.get_process_id_by_pattern("microcom ")
+
+# BMC_IP = "10.1.20.149"
+# BMC_USERNAME = "sysadmin"
+# BMC_PASSWORD = "superuser"
+# l = Linux(host_ip=BMC_IP, ssh_username=BMC_USERNAME, ssh_password=BMC_PASSWORD)
+# l.set_prompt_terminator(r'# $')
+# l.command("cd {}".format(SCRIPT_DIRECTORY))
+# l.command("./f1_uartmux.sh 1")
+# l.command("./f1_console.sh 1")
+# process_id = l.get_process_id_by_pattern("microcom ")
+# if process_id:
+#    l.kill_process(process_id)
+# process_id = l.get_process_id_by_pattern("microcom ")
 
 class BootPhases:
     U_BOOT_INIT = "u-boot: init"
@@ -30,15 +32,36 @@ class BootPhases:
     F1_BOOT_PCI_STARTED = "f1: pci started"
     F1_BOOT_READY = "f1: ready"
 
+
+class F1InFs:
+    def __init__(self, bmc, serial_device_path, serial_sbp_device_path):
+        self.bmc = bmc.clone()
+        self.serial_device_path = serial_device_path
+        self.serial_sbp_device_path = serial_sbp_device_path
+
+    def bootup(self):
+        pass
+
+
 class Fs():
     ELF_ADDRESS = "0xffffffff99000000"
+    BMC_SCRIPT_DIRECTORY = "/mnt/sdmmc0p1/scripts"
 
     def __init__(self, bmc_mgmt_ip, bmc_mgmt_ssh_username, bmc_mgmt_ssh_password):
         self.bmc_mgmt_ip = bmc_mgmt_ip
         self.bmc_mgmt_ssh_username = bmc_mgmt_ssh_username
         self.bmc_mgmt_ssh_password = bmc_mgmt_ssh_password
-
         self.bmc = None
+        self.f1s = {}
+
+    def get_f1_0(self):
+        return self.get_f1(index=0)
+
+    def get_f1_1(self):
+        return self.get_f1(index=1)
+
+    def get_f1(self, index):
+        pass
 
     @staticmethod
     def get(spec):
@@ -51,17 +74,53 @@ class Fs():
         self.boot_phase = phase
         fun_test.log_section(phase)
 
-
     def bootup(self):
         fun_test.simple_assert(self.bmc_initialize(), "BMC initialize")
+
         # fun_test.
+
+    def get_bmc(self):
+        if not self.bmc:
+            self.bmc_initialize()
+        return self.bmc
 
     def bmc_initialize(self):
         self.bmc = Linux(host_ip=self.bmc_mgmt_ip,
                          ssh_username=self.bmc_mgmt_ssh_username,
                          ssh_password=self.bmc_mgmt_ssh_password)
+        self.bmc.set_prompt_terminator(r'# $')
         fun_test.simple_assert(self.bmc._connect(), "BMC connected")
+        self.bmc.command("cd {}".format(self.BMC_SCRIPT_DIRECTORY))
+        self.bmc.command("./f1_console.sh 1")
+        self._set_f1s()
         return self.bmc
+
+    def _set_f1s(self):
+        result = None
+        output = self.bmc.command("./f1_uartmux.sh 1")
+        lines = output.split("\n")
+        f1_info = {}
+        for line in lines:
+            m = re.search(r'F1\[(\d+)\]\s+(\S+)\s+(\S+)', line)
+            if m:
+                f1_index = int(m.group(1))
+                console_or_sbp = m.group(2)
+                device_path = m.group(3)
+                if f1_index not in f1_info:
+                    f1_info[f1_index] = {}
+                if console_or_sbp == "console":
+                    f1_info[f1_index]["f1_device_path"] = device_path
+                if console_or_sbp == "SBP":
+                    f1_info[f1_index]["sbp_device_path"] = device_path
+        # F1InFs()
+        # flatten
+        for f1_index in sorted(f1_info.keys()):
+            self.f1s[f1_index] = F1InFs(bmc=self.bmc,
+                                        serial_device_path=f1_info[f1_index]["f1_device_path"],
+                                        serial_sbp_device_path=f1_info[f1_index]["sbp_device_path"])
+
+        fun_test.simple_assert(len(self.f1s.keys()), "Both F1 device paths found")
+        return result
 
     def u_boot_load_image(self,
                           handle,
@@ -113,5 +172,10 @@ class Fs():
         result = True
         return result
 
-fs = Fs(bmc_ip=BMC_IP)
-#fs.u_boot_load_image(fs.)
+
+fs_json = ASSET_DIR + "/fs.json"
+json_spec = parse_file_to_json(file_name=fs_json)
+
+fs = Fs.get(spec=json_spec[0])
+fs.bootup()
+# fs.u_boot_load_image(fs.)
