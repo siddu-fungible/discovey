af45178b3389686313afe0d2a805faea03b716d9
John Abraham <john.abraham@fungible.com>
2019-03-21 15:22:12 -0700

    use tftp and test_bed defaults

--- fun_test/scripts/examples/test_fs.py
+++ fun_test/scripts/examples/test_fs.py
@@ -36,17 +36,13 @@ class FunTestCase1(FunTestCase):
         fun_test.shared_variables["fs"].cleanup()
 
     def run(self):
-        tftp_image_path = fun_test.get_job_environment_variable("tftp_image_path")
-        test_bed_type = fun_test.get_job_environment_variable("test_bed_type")
-        fun_test.log("Tftp-image path: {}".format(tftp_image_path))
-        fun_test.log("Testbed-type: {}".format(test_bed_type))
-        fun_test.simple_assert(tftp_image_path and test_bed_type, "Testbed type and TFTP image path are available")
-
-        fs = Fs.get(test_bed_spec=AssetManager().get_fs_by_name(test_bed_type), tftp_image_path=tftp_image_path)
+        fs = Fs.get()
         fun_test.shared_variables["fs"] = fs
         fun_test.test_assert(fs.bootup(reboot_bmc=False), "FS bootup")
         f1 = fs.get_f1(index=0)
         f1.get_dpc_client().json_execute(verb="peek", data="stats/vppkts", command_duration=4)
+
+
 
 if __name__ == "__main__":
     myscript = MyScript()
