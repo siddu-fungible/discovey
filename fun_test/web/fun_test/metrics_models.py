70500d396eca2849ea84f367adb0a8fcd45ffb9e
Ash41291 <ashwin.s@fungible.com>
2019-03-25 15:01:24 -0700

    added flowtest on f1

--- fun_test/web/fun_test/metrics_models.py
+++ fun_test/web/fun_test/metrics_models.py
@@ -1619,6 +1619,23 @@ class FlowTestPerformance(models.Model):
             s += "{}:{} ".format(key, value)
         return s
 
+class F1FlowTestPerformance(models.Model):
+    interpolation_allowed = models.BooleanField(default=False)
+    interpolated = models.BooleanField(default=False)
+    status = models.CharField(max_length=30, verbose_name="Status", default=RESULTS["PASSED"])
+    input_date_time = models.DateTimeField(verbose_name="Date", default=datetime.now)
+    input_app = models.CharField(max_length=30, default="hw_hsu_test", choices=[(0, "hw_hsu_test")])
+    input_iterations = models.IntegerField(default=-1)
+    output_time = models.IntegerField(verbose_name="seconds", default=-1)
+    output_time_unit = models.TextField(default="secs")
+    tag = "analytics"
+
+    def __str__(self):
+        s = ""
+        for key, value in self.__dict__.iteritems():
+            s += "{}:{} ".format(key, value)
+        return s
+
 class BootTimePerformance(models.Model):
     interpolation_allowed = models.BooleanField(default=False)
     interpolated = models.BooleanField(default=False)
