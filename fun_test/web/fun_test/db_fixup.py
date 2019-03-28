29106fc2f0cd24983f6d56938916285d8e02e5d0
Ash41291 <ashwin.s@fungible.com>
2019-03-27 18:28:05 -0700

    uncommeted score calc for total and all metrics

--- fun_test/web/fun_test/db_fixup.py
+++ fun_test/web/fun_test/db_fixup.py
@@ -551,9 +551,9 @@ def convert_to_base_unit(output_value, output_unit):
 
 if __name__ == "__main__":
     # "Malloc agent rate : FunMagentPerformanceTest : 185"
-    total_chart = MetricChart.objects.get(metric_model_name="MetricContainer", chart_name="FunOS")
-    prepare_status(chart=total_chart, purge_old_status=False, cache_valid=False)
-    # total_chart = MetricChart.objects.get(metric_model_name="MetricContainer", chart_name="Total")
+    # total_chart = MetricChart.objects.get(metric_model_name="MetricContainer", chart_name="FunOS")
     # prepare_status(chart=total_chart, purge_old_status=False, cache_valid=False)
-    # all_metrics_chart = MetricChart.objects.get(metric_model_name="MetricContainer", internal_chart_name="All metrics")
-    # prepare_status(chart=all_metrics_chart, purge_old_status=False, cache_valid=False)
+    total_chart = MetricChart.objects.get(metric_model_name="MetricContainer", chart_name="Total")
+    prepare_status(chart=total_chart, purge_old_status=False, cache_valid=False)
+    all_metrics_chart = MetricChart.objects.get(metric_model_name="MetricContainer", internal_chart_name="All metrics")
+    prepare_status(chart=all_metrics_chart, purge_old_status=False, cache_valid=False)
