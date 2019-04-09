6cfd814609213f94b79f64237a19e86e24cce84f
Yajat Singh <yajat.singh@fungible.com>
2019-04-09 12:45:13 +0530

    Boot FS

--- fun_test/scripts/networking/fs_boot.py
+++ fun_test/scripts/networking/fs_boot.py
@@ -1,19 +1,21 @@
 from lib.system.fun_test import *
+from lib.templates.traffic_generator.spirent_rfc2544_template import *
+from scripts.networking.nu_config_manager import *
+from lib.host.network_controller import *
 from scripts.networking.helper import *
+from lib.host.linux import *
 from lib.fun.fs import *
 
 
 class ScriptSetup(FunTestScript):
 
     def describe(self):
-        self.set_test_details(steps="1. Get FS system : %s credentials, image and bootargs "
-                              % fun_test.get_job_environment_variable('test_bed_type'))
+        self.set_test_details(steps="1. Make sure correct FS system is selected")
 
     def setup(self):
-        global fs
-        if fun_test.get_job_environment_variable('test_bed_type') == 'fs-7':
-            fs = Fs.get()
-        fun_test.simple_assert(fs, "Succesfully fetched image, credentials and bootargs")
+
+        fun_test.test_assert_expected(expected="fs-7", actual=fun_test.get_job_environment_variable('test_bed_type'),
+                                      message="Correct FS was selected")
 
     def cleanup(self):
         pass
@@ -24,7 +26,8 @@ class BootFS(FunTestCase):
         self.set_test_details(id=1,
                               summary=" Boot FS",
                               steps="""
-                              1. Boot up FS system : %s
+                              1. Get FS system : %s credentials, image and bootargs
+                              2. Boot up FS system
                               """ % fun_test.get_job_environment_variable('test_bed_type'))
 
     def setup(self):
