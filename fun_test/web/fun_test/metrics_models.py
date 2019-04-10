b8c6f4282bff8684a2e8dae678bbeba9894e2377
Ash41291 <ashwin.s@fungible.com>
2019-04-10 09:53:39 -0700

    added model

--- fun_test/web/fun_test/metrics_models.py
+++ fun_test/web/fun_test/metrics_models.py
@@ -1624,6 +1624,34 @@ class TeraMarkZipLzmaPerformance(models.Model):
             s += "{}:{} ".format(key, value)
         return s
 
+class TeraMarkRcnvmeReadPerformance(models.Model):
+    interpolation_allowed = models.BooleanField(default=False)
+    interpolated = models.BooleanField(default=False)
+    status = models.CharField(max_length=30, verbose_name="Status", default=RESULTS["PASSED"])
+    input_date_time = models.DateTimeField(verbose_name="Date", default=datetime.now)
+    input_type = models.CharField(max_length=30, default="", choices=[(0, "LZMA")])
+    input_operation = models.CharField(max_length=30, default="", choices=[(0, "Compress"), (1, "Decompress")])
+    input_effort = models.CharField(max_length=30, default="", choices=[(0, "8"), (1, "9"), (2, "10"), (3, "11")])
+    output_bandwidth_avg = models.FloatField(verbose_name="Gbps", default=-1)
+    output_bandwidth_total = models.IntegerField(verbose_name="Kbps", default=-1)
+    output_latency_min = models.IntegerField(verbose_name="ns", default=-1)
+    output_latency_avg = models.BigIntegerField(verbose_name="ns", default=-1)
+    output_latency_max = models.IntegerField(verbose_name="ns", default=-1)
+    output_iops = models.IntegerField(verbose_name="ops per sec", default=-1)
+    output_bandwidth_avg_unit = models.TextField(default="Gbps")
+    output_bandwidth_total_unit = models.TextField(default="Gbps")
+    output_latency_min_unit = models.TextField(default="nsecs")
+    output_latency_avg_unit = models.TextField(default="nsecs")
+    output_latency_max_unit = models.TextField(default="nsecs")
+    output_iops_unit = models.TextField(default="ops")
+    tag = "analytics"
+
+    def __str__(self):
+        s = ""
+        for key, value in self.__dict__.iteritems():
+            s += "{}:{} ".format(key, value)
+        return s
+
 class TeraMarkDfaPerformance(models.Model):
     interpolation_allowed = models.BooleanField(default=False)
     interpolated = models.BooleanField(default=False)
