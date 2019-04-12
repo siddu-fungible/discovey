4d7607c856d532275b81aa14c59f3f1923e02640
John Abraham <john.abraham@fungible.com>
2019-04-11 22:00:30 -0700

    WIP

--- fun_test/web/fun_test/api/regression.py
+++ fun_test/web/fun_test/api/regression.py
@@ -1,8 +1,11 @@
+from fun_global import get_current_time
 from web.web_global import api_safe_json_response
 from django.views.decorators.csrf import csrf_exempt
 from web.fun_test.models import TestBed
 from django.db.models import Q
 from web.fun_test.models import SuiteExecution
+import json
+from datetime import datetime, timedelta
 
 @csrf_exempt
 @api_safe_json_response
@@ -12,8 +15,27 @@ def test_beds(request, id):
         all_test_beds = TestBed.objects.all().order_by('name')
         result = []
         for test_bed in all_test_beds:
-            t = {"name": test_bed.name, "description": test_bed.description}
+            t = {"name": test_bed.name,
+                 "description": test_bed.description,
+                 "id": test_bed.id,
+                 "manual_lock": test_bed.manual_lock,
+                 "manual_lock_expiry_time": str(test_bed.manual_lock_expiry_time),
+                 "manual_lock_submitter": test_bed.manual_lock_submitter}
             result.append(t)
+    if request.method == "PUT":
+        test_bed = TestBed.objects.get(id=int(id))
+        request_json = json.loads(request.body)
+        extension_hour = request_json["manual_lock_extension_hour"]
+        extension_minute = request_json["manual_lock_extension_minute"]
+        submitter_email = request_json["manual_lock_submitter_email"]
+
+        future_time = get_current_time() + timedelta(hours=int(extension_hour),
+                                                     minutes=int(extension_minute))
+        test_bed.manual_lock_expiry_time = future_time
+        test_bed.manual_lock_submitter = submitter_email
+        test_bed.manual_lock = True
+        test_bed.save()
+        pass
     return result
 
 @csrf_exempt
