4bdca2b184def3e7416966cdbeb2f93e612e8b7b
John Abraham <john.abraham@fungible.com>
2019-04-16 06:17:38 -0700

    WIP

--- fun_test/lib/utilities/fun_test_client_test.py
+++ fun_test/lib/utilities/fun_test_client_test.py
@@ -1,5 +1,7 @@
 from lib.utilities.fun_test_client import FunTestClient
 base_url = "http://0.0.0.0:5000"
+base_url = "http://server17:5000"
+
 fun_test_client = FunTestClient(base_url=base_url)
 
 suite_name = "test.json"
