5438766625b7bc1dc5173601c8ba22638415393c
manuks88 <manu.ks@fungible.com>
2019-04-19 22:20:24 +0530

    Avg tps sent to post_results should be divided by 1000

--- fun_test/scripts/storage/stripe_vol_fs_perf.py
+++ fun_test/scripts/storage/stripe_vol_fs_perf.py
@@ -661,7 +661,7 @@ class BLTVolumePerformanceTestcase(FunTestCase):
                                          format(op, field, actual, row_data_dict[op + field][1:]))
 
                 row_data_dict["fio_job_name"] = fio_job_name
-                row_data_dict["readiops"] = avg_tps
+                row_data_dict["readiops"] = avg_tps / 1000
                 row_data_dict["readbw"] = avg_kbs_read / 1000
 
                 # TODO: SWOS-4554 - As dpcsh is not working we are unable to pull internal stats, hence commenting
