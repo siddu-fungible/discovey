188acd956ddb4df6c10290acd9e05464288ec50f
John Abraham <john.abraham@fungible.com>
2019-04-19 19:27:39 -0700

    increase email address length

--- fun_test/web/fun_test/models.py
+++ fun_test/web/fun_test/models.py
@@ -372,7 +372,7 @@ class KilledJob(models.Model):
 class User(FunModel):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=30, unique=True)
+    email = models.EmailField(max_length=60, unique=True)
 
     def __str__(self):
         return "{} {} {}".format(self.first_name, self.last_name, self.email)
