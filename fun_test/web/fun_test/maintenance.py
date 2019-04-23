458a49bbd779c449f96b237916219560ebc491c2
Ash41291 <ashwin.s@fungible.com>
2019-04-22 18:28:29 -0700

    added charts and scripts for multi tunnel

--- fun_test/web/fun_test/maintenance.py
+++ fun_test/web/fun_test/maintenance.py
@@ -603,7 +603,7 @@ if __name__ == "__main__network_revamped__":
                     work_in_progress=False).save()
     print "created charts for the new networking teramarks"
 
-if __name__ == "__main__":
+if __name__ == "__main_removed_offloads__":
     internal_chart_names = ["HU_NU_NFCP_1TCP_offloads_disabled_output_throughput",
                             "HU_NU_NFCP_1TCP_offloads_disabled_output_pps",
                             "HU_NU_NFCP_8TCP_offloads_disabled_output_throughput",
@@ -617,3 +617,53 @@ if __name__ == "__main__":
         entry = MetricChart.objects.get(internal_chart_name=internal_chart_name)
         ml.remove_attribute_from_data_sets(chart=entry, key="input_offloads")
     print "removed the input offloads from datasets"
+
+if __name__ == "__main__":
+    internal_chart_names = ["juniper_crypto_single_tunnel_output_throughput", "juniper_crypto_single_tunnel_output_pps", "juniper_crypto_multi_tunnel_output_throughput", "juniper_crypto_multi_tunnel_output_pps"]
+    model_name = "JuniperCryptoTunnelPerformance"
+    input_algorithm = "AES_GCM"
+    description = "TBD"
+    positive = True
+    base_line_date = datetime(year=2019, month=1, day=26, minute=0, hour=0, second=0)
+    for internal_chart_name in internal_chart_names:
+        if "throughput" in internal_chart_name:
+            chart_name = "Throughput"
+            output_name = "output_throughput"
+            y1_axis_title = "Gbps"
+        else:
+            chart_name = "Packets per sec"
+            output_name = "output_packets_per_sec"
+            y1_axis_title = "Mpps"
+
+        if "single_tunnel" in internal_chart_name:
+            num_tunnels = 1
+        else:
+            num_tunnels = 64
+
+        data_sets = []
+        name = "GCM-" + str(num_tunnels) + "tunnel(s)"
+        one_data_set = {}
+        one_data_set["name"] = name
+        one_data_set["inputs"] = {}
+        one_data_set["inputs"]["input_num_tunnels"] = num_tunnels
+        one_data_set["inputs"]["input_algorithm"] = input_algorithm
+        one_data_set["inputs"]["input_pkt_size"] = 354
+        one_data_set["output"] = {"name": output_name, 'min': 0, "max": -1, "expected": -1, "reference": -1}
+        data_sets.append(one_data_set)
+
+        metric_id = LastMetricId.get_next_id()
+        MetricChart(chart_name=chart_name,
+                    metric_id=metric_id,
+                    internal_chart_name=internal_chart_name,
+                    data_sets=json.dumps(data_sets),
+                    leaf=True,
+                    description=description,
+                    owner_info="Suren Madineni (suren.madineni@fungible.com)",
+                    source="https://github.com/fungible-inc/FunOS/blob/5c090e8bbe9f6826c9bb1467f5e4642dadbd9d82/apps/cryptotest/crypto_dp_tunnel_perf.c",
+                    positive=positive,
+                    y1_axis_title=y1_axis_title,
+                    visualization_unit=y1_axis_title,
+                    metric_model_name=model_name,
+                    base_line_date=base_line_date,
+                    work_in_progress=False).save()
+    print "created charts for the IPSEC juniper customer teramarks"
