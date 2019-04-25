a41a97970cfb4b734dea07086a8b9e356d62b790
Yajat Singh <yajat.singh@fungible.com>
2019-04-26 00:57:39 +0530

    Create boot_custom_fs.py

---
+++ fun_test/scripts/networking/boot_custom_fs.py
@@ -0,0 +1,52 @@
+from lib.system.fun_test import *
+from lib.templates.traffic_generator.spirent_rfc2544_template import *
+from scripts.networking.nu_config_manager import *
+from lib.host.network_controller import *
+from scripts.networking.helper import *
+from lib.host.linux import *
+from lib.fun.fs import *
+
+
+class ScriptSetup(FunTestScript):
+
+    def describe(self):
+        self.set_test_details(steps="1. Make sure correct FS system is selected")
+
+    def setup(self):
+
+        fun_test.test_assert_expected(expected="fs-7", actual=fun_test.get_job_environment_variable('test_bed_type'),
+                                      message="Correct FS was selected")
+
+    def cleanup(self):
+        pass
+
+
+class BootFS(FunTestCase):
+    def describe(self):
+        self.set_test_details(id=1,
+                              summary=" Boot FS",
+                              steps="""
+                              1. Get FS system : %s credentials, image and bootargs
+                              2. Boot up FS system
+                              """ % fun_test.get_job_environment_variable('test_bed_type'))
+
+    def setup(self):
+        pass
+
+    def run(self):
+
+        bootargs = "app=hw_hsu_test cc_huid=3 --fec sku=SKU_FS1600_0 --dis-stats --csr-replay --dpc-server --dpc-uart --serdesinit"
+        test_bed_spec = fun_test.get_asset_manager().get_fs_by_name("fs-7")
+        img_path = "funos-f1.stripped_24apr_funcp.gz"
+        fs = Fs.get(test_bed_spec=test_bed_spec, tftp_image_path=img_path, boot_args=bootargs)
+        fun_test.simple_assert(fs, "Succesfully fetched image, credentials and bootargs")
+        fun_test.test_assert(fs.bootup(reboot_bmc=False), 'FS bootup')
+
+    def cleanup(self):
+        pass
+
+
+if __name__ == '__main__':
+    ts = ScriptSetup()
+    ts.add_test_case(BootFS())
+    ts.run()
