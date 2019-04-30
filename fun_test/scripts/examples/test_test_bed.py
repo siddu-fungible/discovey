430d3b11389e5d3cf0e4ff8acd6ef768667a24fd
John Abraham <john.abraham@fungible.com>
2019-04-29 18:43:15 -0700

    remove disable wu-watchdog

--- fun_test/scripts/examples/test_test_bed.py
+++ fun_test/scripts/examples/test_test_bed.py
@@ -38,7 +38,7 @@ class FunTestCase1(FunTestCase):
     def run(self):
         # fs = Fs.get(disable_f1_index=1)
         topology_helper = TopologyHelper()
-        topology_helper.set_dut_parameters(dut_index=0, custom_boot_args="app=hw_hsu_test --disable-wu-watchdog --dpc-uart --dpc-server --csr-replay --retimer --all_100g")
+        topology_helper.set_dut_parameters(dut_index=0, custom_boot_args="app=hw_hsu_test --dpc-uart --dpc-server --csr-replay --retimer --all_100g")
         topology = topology_helper.deploy()
         fun_test.test_assert(topology, "Topology deployed")
         fs = topology.get_dut_instance(index=0)
