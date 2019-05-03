220ad0e6042f0b0c1cd46a4c1bc4dd3dba4be746
John Abraham <john.abraham@fungible.com>
2019-05-03 13:55:59 -0700

    fixes

--- fun_test/lib/topology/topology.py
+++ fun_test/lib/topology/topology.py
@@ -46,8 +46,7 @@ class ExpandedTopology(ToDictMixin):
     def _get_host_instances_on_interfaces(self, dut, interfaces):
         hosts = {}
         for interface_index, interface_obj in interfaces.iteritems():
-            interface_index = interface_index
-            host = dut.get_host_on_interface(interface_index=interface_index, host_index=0)
+            host = dut.get_host_on_interface_obj(interface_obj=interface_obj, host_index=0)
             if host.host_ip not in hosts:
                 hosts[host.host_ip] = {"interfaces": [interface_obj], "host_obj": host}
             else:
