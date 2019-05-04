fab80902c67b093975b3b2d756e516eae64275c5
Ash41291 <ashwin.s@fungible.com>
2019-05-04 09:41:40 -0700

    fixed Le metrics

--- fun_test/scripts/system/metric_parser.py
+++ fun_test/scripts/system/metric_parser.py
@@ -39,6 +39,8 @@ class MetricParser():
             return self.hu_networking(logs=logs, date_time=date_time)
         elif "JuniperCrypto" in model_name:
             return self.crypto_tunnel(logs=logs, date_time=date_time)
+        elif "LookupEngine" in model_name:
+            return self.lookup_engine(logs=logs, date_time=date_time)
         else:
             return {}
 
@@ -298,6 +300,47 @@ class MetricParser():
                 self.status = RESULTS["PASSED"]
                 d = self.metrics_to_dict(metrics=metrics, result=self.status, date_time=date_time)
                 result["data"].append(d)
+        result["match_found"] = match_found
+        result["status"] = self.status == RESULTS["PASSED"]
+        return result
+
+    def lookup_engine(self, logs, date_time):
+        match_found = False
+        result = {}
+        result["data"] = []
+        teramark_begin = False
+        self.status = RESULTS["FAILED"]
+        metrics = collections.OrderedDict()
+        for line in logs:
+            if "TeraMark Begin" in line:
+                teramark_begin = True
+                continue
+            if "TeraMark End" in line:
+                teramark_begin = False
+            if teramark_begin:
+                m = re.search(
+                    r'(?P<value_json>{.*})',
+                    line)
+                if m:
+                    value_json = json.loads(m.group("value_json"))
+                    match_found = True
+                    metrics["input_test"] = "le_test_perf"
+                    metrics["input_memory"] = value_json["memory"]
+                    metrics["input_operation"] = value_json["operation"]
+                    if "value" in value_json:
+                        metrics["output_lookup_per_sec_min"] = -1
+                        metrics["output_lookup_per_sec_avg"] = int(value_json["value"])
+                        metrics["output_lookup_per_sec_max"] = -1
+                    else:
+                        metrics["output_lookup_per_sec_min"] = int(value_json["min"])
+                        metrics["output_lookup_per_sec_avg"] = int(value_json["avg"])
+                        metrics["output_lookup_per_sec_max"] = int(value_json["max"])
+                    metrics["output_lookup_per_sec_min_unit"] = value_json["unit"]
+                    metrics["output_lookup_per_sec_avg_unit"] = value_json["unit"]
+                    metrics["output_lookup_per_sec_max_unit"] = value_json["unit"]
+                    self.status = RESULTS["PASSED"]
+                    d = self.metrics_to_dict(metrics=metrics, result=self.status, date_time=date_time)
+                    result["data"].append(d)
         result["match_found"] = match_found
         result["status"] = self.status == RESULTS["PASSED"]
         return result
