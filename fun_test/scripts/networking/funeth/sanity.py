7bb71390d3ae4f8dae06f27a2a28f17131c5336f
George Liang <george.liang@fungible.com>
2019-05-13 16:35:22 -0700

    Eanble TSO

--- fun_test/scripts/networking/funeth/sanity.py
+++ fun_test/scripts/networking/funeth/sanity.py
@@ -73,7 +73,7 @@ def setup_hu_host(funeth_obj, update_driver=True):
         fun_test.test_assert(funeth_obj.load(sriov=4), 'Load funeth driver.')
     for hu in funeth_obj.hu_hosts:
         linux_obj = funeth_obj.linux_obj_dict[hu]
-        fun_test.test_assert(funeth_obj.enable_tso(hu, disable=True), 'Disable HU host {} funeth interfaces TSO.'.format(
+        fun_test.test_assert(funeth_obj.enable_tso(hu, disable=False), 'Enable HU host {} funeth interfaces TSO.'.format(
             linux_obj.host_ip))
         fun_test.test_assert(funeth_obj.configure_interfaces(hu), 'Configure HU host {} funeth interfaces.'.format(
             linux_obj.host_ip))
