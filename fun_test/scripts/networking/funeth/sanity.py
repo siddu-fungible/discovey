4ef8abac71dd3f3d4b37c186fafffb887c874dca
George Liang <george.liang@fungible.com>
2019-05-13 21:17:49 -0700

    Temporarily add boot arg --rxlog

--- fun_test/scripts/networking/funeth/sanity.py
+++ fun_test/scripts/networking/funeth/sanity.py
@@ -98,7 +98,7 @@ class FunethSanity(FunTestScript):
         # Boot up FS1600
         if fun_test.get_job_environment_variable('test_bed_type') == 'fs-11':
             #boot_args = "app=hw_hsu_test retimer=0,1 --disable_dispatch_loop_switch --dpc-uart --dpc-server --csr-replay --all_100g"
-            boot_args = "app=hw_hsu_test retimer=0,1 --dpc-uart --dpc-server --csr-replay --all_100g"
+            boot_args = "app=hw_hsu_test retimer=0,1 --dpc-uart --dpc-server --csr-replay --all_100g --rxlog"
             # fs = Fs.get(disable_f1_index=1)
             topology_helper = TopologyHelper()
             topology_helper.set_dut_parameters(dut_index=0,
