c4da20c92d0fa523b617297215ad3f5cec0a0190
Ash41291 <ashwin.s@fungible.com>
2019-05-17 13:38:34 -0700

    fixed output unit for F1

--- fun_test/web/fun_test/maintenance.py
+++ fun_test/web/fun_test/maintenance.py
@@ -18,3 +18,9 @@ if __name__ == "__main__":
             chart.save()
             print "chart name is: {}".format(chart.chart_name)
             print "peer chart ids: {}".format(json.loads(chart.peer_ids))
+        if chart.leaf:
+            data_sets = json.loads(chart.data_sets)
+            for data_set in data_sets:
+                data_set["output"]["unit"] = chart.visualization_unit
+            chart.data_sets = json.dumps(data_sets)
+            chart.save()
