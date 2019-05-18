6de5f839e8f255959d90cb5de09b0aa5f0fd5805
John Abraham <john.abraham@fungible.com>
2019-05-18 13:01:02 -0700

    WIP

--- fun_test/web/fun_test/tests/test_triaging.py
+++ fun_test/web/fun_test/tests/test_triaging.py
@@ -1,3 +1,4 @@
+from fun_global import RESULTS
 import web.fun_test.django_interactive
 from web.fun_test.triaging_global import TriagingStates, TriageTrialStates, TriagingTypes
 from web.fun_test.metrics_models import TriagingResult
@@ -92,6 +93,21 @@ class TriageStateMachine:
         self.triage_id = triage.triage_id
         self.set_all_commits()
 
+    def error(self, message):
+        s = "{}: {}".format(self._get_triage_string(), str(message))
+        logger.exception(s)
+        triage = self.get_triage()
+        triage.status = TriagingStates.ERROR
+        triage.save()
+
+    def debug(self, message):
+        s = "{}: {}".format(self._get_triage_string(), str(message))
+        logger.debug(s)
+
+    def _get_triage_string(self):
+        t = self.get_triage()
+        return "T: {} Set: {}".format(self.triage_id, t.current_trial_set_id)
+
     def get_triage(self):
         return Triage3.objects.get(triage_id=self.triage_id)
 
@@ -109,6 +125,8 @@ class TriageStateMachine:
         logger.debug("This triage has {} commits".format(len(all_commits)))
         logger.debug("First commit: {}".format(str(self.all_commits[0])))
         logger.debug("Second commit: {}".format(str(self.all_commits[-1])))
+        self.all_shas = [x.sha for x in self.all_commits]
+
 
     def get_trial_tag(self, base_tag, fun_os_sha):
         t = self.get_triage()
@@ -116,7 +134,7 @@ class TriageStateMachine:
 
     def start_trial(self, fun_os_sha):
         t = self.get_triage()
-
+        started = False
         if not Triage3Trial.objects.filter(triage_id=t.triage_id, fun_os_sha=fun_os_sha).exists():
             trial = Triage3Trial(fun_os_sha=fun_os_sha,
                                  triage_id=self.triage_id,
@@ -128,20 +146,22 @@ class TriageStateMachine:
             trial.tag = trial_tag
             trial.save()
             logger.debug("Started trial for {}".format(fun_os_sha))
+            started = True
         else:
+            trial = Triage3Trial.objects.get(triage_id=t.triage_id, fun_os_sha=fun_os_sha)
+            trial.trial_set_id = t.current_trial_set_id
+            trial.save()
             logger.debug("Skipping trial for {} as it was already complete".format(fun_os_sha))
+        return started
 
     def start_trial_set(self, from_fun_os_sha, to_fun_os_sha):
         # create trial sets for boundary shas and in-between shas
         # get potential list of shas to try
         logger.debug("Starting Trial set, from: {}, to: {}".format(from_fun_os_sha, to_fun_os_sha))
 
-
-        all_shas = [x.sha for x in self.all_commits]
-
-        for sha in all_shas:
+        for sha in self.all_shas:
             print sha
-        commits_subset = all_shas[all_shas.index(from_fun_os_sha): all_shas.index(to_fun_os_sha) + 1]
+        commits_subset = self.all_shas[self.all_shas.index(from_fun_os_sha): self.all_shas.index(to_fun_os_sha) + 1]
         first_commit = commits_subset[0]
         last_commit = commits_subset[-1]
 
@@ -150,23 +170,26 @@ class TriageStateMachine:
         t.current_trial_to_sha = last_commit
         t.save()
 
-        num_trials = 0
-        max_trials = 8
+
         increment = len(commits_subset)/self.STEP
+        if t.triage_type == TriagingTypes.PASS_OR_FAIL:
+            increment = len(commits_subset)/2
+
         if not increment:
             increment = 1
+        num_trials = 0
+        max_trials = 8
         for commit_index in range(0, len(commits_subset), increment):
             this_commit = commits_subset[commit_index]
             logger.debug("Candidate: {}".format(str(this_commit)))
-            self.start_trial(fun_os_sha=this_commit)
-            num_trials += 1
+            started = self.start_trial(fun_os_sha=this_commit)
+            if started:
+                num_trials += 1
             if num_trials > max_trials:
                 raise Exception("Too many trials")
 
-        l = []
-        l.append(from_fun_os_sha)
-        l.append(to_fun_os_sha)
-        pass
+        if not num_trials:
+            pass
 
     def is_current_trial_set_complete(self):
         trial_count = Triage3Trial.objects.filter(triage_id=triage_id, trial_set_id=t.current_trial_set_id, status__gt=TriageTrialStates.COMPLETED)
@@ -185,11 +208,65 @@ class TriageStateMachine:
                 time.sleep(1)
         else:
             logger.debug("No trials to process")
+        self.next()
+
+    def next(self):
+        """
+        Check if we need a new trial
+        :return:
+        """
+        t = self.get_triage()
+        if t.triage_type == TriagingTypes.PASS_OR_FAIL:
+            completed_trials = Triage3Trial.objects.filter(triage_id=t.triage_id, trial_set_id=t.current_trial_set_id, status=TriageTrialStates.COMPLETED).order_by('-submission_date_time')
+            trials_in_current_set = Triage3Trial.objects.filter(triage_id=t.triage_id, trial_set_id=t.current_trial_set_id)
+
+            self.debug("Num trials in set: {}, completed count: {}".format(trials_in_current_set.count(),
+                                                                           completed_trials.count()))
+
+            last_pass_sha = None
+            last_fail_sha = None
+            if completed_trials.count() == trials_in_current_set.count():
+                self.debug("Trials in set have completed ")
+
+                for completed_trial in completed_trials:
+                    pass  # Look for last pass
+                    trial_result = completed_trial.result
+                    if completed_trial.fun_os_sha == t.to_fun_os_sha:
+                        if trial_result == RESULTS["PASSED"]:
+                            self.error("The latest commit: {} actually PASSED. Please re-submit triage".format(t.to_fun_os_sha))
+
+                    elif completed_trial.fun_os_sha == t.from_fun_os_sha:
+                        if trial_result == RESULTS["FAILED"]:
+                            self.error("The earliest commit: {} actually FAILED. Please re-submit triage".format(t.from_fun_os_sha))
+                    else:
+                        if trial_result == RESULTS["PASSED"]:
+                            last_pass_sha = completed_trial.fun_os_sha
+                        elif trial_result == RESULTS["FAILED"]:
+                            last_fail_sha = completed_trial.fun_os_sha
+                        if last_pass_sha and last_fail_sha:
+                            break
+            if not last_pass_sha:
+                self.error("last pass sha not available")
+            if not last_fail_sha:
+                self.error("last fail sha not available")
+
+            last_pass_index = self.all_shas.index(last_pass_sha)
+            last_fail_index = self.all_shas.index(last_fail_sha)
+            if last_fail_index < last_pass_index:
+                self.error("last fail index: {} is lesser than last pass index: {}".format(last_fail_index, last_pass_index))
+            if (last_fail_index - last_pass_index) <= 1:
+                return self.complete()
+            else:
+                self.start_trial_set(from_fun_os_sha=last_pass_sha, to_fun_os_sha=last_fail_sha)
+
+
+    def complete(self):
+        t = self.get_triage()
+        t.status = TriagingStates.COMPLETED
+        t.save()
 
     def process(self):
-        triage_id = self.triage_id
         t = self.get_triage()
-
         status = t.status
 
         if status == TriagingStates.INIT:
@@ -285,9 +362,10 @@ class TrialStateMachine:
         elif status == TriageTrialStates.PREPARING_RESULTS:
             lsf_server = LsfStatusServer()  # TODO
             job_info = lsf_server.get_last_job(tag=trial.tag)
-            if "output_text" in job_info:
-                lines = job_info["output_text"].split("\n")
-                if triage.triage_type == TriagingTypes.REGEX_MATCH:
+            if triage.triage_type == TriagingTypes.REGEX_MATCH:
+                if "output_text" in job_info:
+                    lines = job_info["output_text"].split("\n")
+
                     trial.regex_match = ""
                     for line in lines:
                         m = re.search(triage.regex_match_string, line)
@@ -295,8 +373,40 @@ class TrialStateMachine:
                             trial.regex_match = m.group(0)
                     trial.status = TriageTrialStates.COMPLETED
                     trial.save()
-
+            elif triage.triage_type == TriagingTypes.PASS_OR_FAIL:
+                code, message = self.validate_lsf_job(trial=trial)
+                if code == 0:
+                    trial.result = RESULTS["PASSED"]
+                    trial.status = TriageTrialStates.COMPLETED
+                    trial.save()
+                elif code == 1:
+                    trial.result = RESULTS["FAILED"]
+                    trial.status = TriageTrialStates.COMPLETED
+                    trial.save()
+                elif code == -1:
+                    trial.result = RESULTS["UNKNOWN"]
+                    trial.status = TriageTrialStates.ERROR
+                    trial.save()
         return status
+
+    def validate_lsf_job(self, trial):
+        code = 0
+        message = ""
+        lsf_server = LsfStatusServer()
+        job_info = lsf_server.get_last_job(tag=trial.tag)
+        if not job_info:
+            code = -1
+            message = "No job info"
+        else:
+            if "return_code" not in job_info:
+                code = -1
+                message = "return_code not in job_info"
+            else:
+                if not job_info["return_code"]:
+                    code = 1
+                else:
+                    code = 0
+        return code, message
 
 if __name__ == "__main2__":
     triage_id = 123
