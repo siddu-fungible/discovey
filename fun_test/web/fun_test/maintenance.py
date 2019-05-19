03e790817d99d8d45aa133d73a13dacc0b819887
Ash41291 <ashwin.s@fungible.com>
2019-05-19 13:20:43 -0700

    added 128 qdepth nvmetcp charts

--- fun_test/web/fun_test/maintenance.py
+++ fun_test/web/fun_test/maintenance.py
@@ -121,7 +121,7 @@ if __name__ == "__main_raw_nvmetcp__":
         else:
             fio_job_names = fio_read_job_names
             operation = "read"
-        for  fio_job_name in fio_job_names:
+        for fio_job_name in fio_job_names:
             if "1_1" in fio_job_name:
                 name = "qd1"
             elif "8_1" in fio_job_name:
@@ -138,7 +138,8 @@ if __name__ == "__main_raw_nvmetcp__":
             one_data_set["inputs"]["input_operation"] = operation
             one_data_set["inputs"]["input_platform"] = FunPlatform.F1
             one_data_set["inputs"]["input_fio_job_name"] = fio_job_name
-            one_data_set["output"] = {"name": output_name, 'min': 0, "max": -1, "expected": -1, "reference": -1, "unit": iops_chart.visualization_unit}
+            one_data_set["output"] = {"name": output_name, 'min': 0, "max": -1, "expected": -1, "reference": -1,
+                                      "unit": iops_chart.visualization_unit}
             data_sets.append(one_data_set)
         iops_chart.data_sets = json.dumps(data_sets)
         iops_chart.save()
@@ -192,7 +193,7 @@ if __name__ == "__main_raw_nvmetcp__":
                         platform=FunPlatform.F1).save()
     print "added new qdepth charts for raw block nvmetcp"
 
-if __name__ == "__main__":
+if __name__ == "__main_jenkins__":
     model_name = "JenkinsJobIdMap"
     app_config = apps.get_app_config(app_label=MAIN_WEB_APP)
     metric_model = app_config.get_metric_models()[model_name]
@@ -205,7 +206,7 @@ if __name__ == "__main__":
             entry.save()
             print "string date {}, date object {}".format(entry.completion_date, entry.build_date)
 
-if __name__ == "__main__":
+if __name__ == "__main_converted_old_storge_versions__":
     model_name = "BltVolumePerformance"
     app_config = apps.get_app_config(app_label=MAIN_WEB_APP)
     metric_model = app_config.get_metric_models()[model_name]
@@ -228,3 +229,199 @@ if __name__ == "__main__":
                                    build_properties="", lsf_job_id="",
                                    sdk_version=entry.input_version,
                                    build_date=build_date)
+
+if __name__ == "__main__":
+    internal_iops_chart_names = ["rand_read_qd_nvmetcp_output_iops", "read_qd_nvmetcp_output_iops"]
+    # internal_latency_chart_names = ["read_qd1_nvmetcp_output_latency", "read_qd8_nvmetcp_output_latency",
+    #                              "read_qd16_nvmetcp_output_latency", "read_qd32_nvmetcp_output_latency", "read_qd64_nvmetcp_output_latency",
+    #                         "rand_read_qd1_nvmetcp_output_latency", "rand_read_qd8_nvmetcp_output_latency", "rand_read_qd16_nvmetcp_output_latency",
+    #                         "rand_read_qd32_nvmetcp_output_latency", "rand_read_qd64_nvmetcp_output_latency"]
+    copy_read = "read_qd1_nvmetcp_output_latency"
+    copy_rand_read = "rand_read_qd1_nvmetcp_output_latency"
+    fio_rand_read_job_names = [
+        "fio_tcp_randread_blt_1_1_scaling", "fio_tcp_randread_blt_16_1_scaling",
+        "fio_tcp_randread_blt_16_2_scaling", "fio_tcp_randread_blt_16_4_scaling", "fio_tcp_randread_blt_16_8_scaling"]
+    fio_read_job_names = ["fio_tcp_read_blt_1_1_scaling",
+                          "fio_tcp_read_blt_16_1_scaling",
+                          "fio_tcp_read_blt_16_2_scaling",
+                          "fio_tcp_read_blt_16_4_scaling", "fio_tcp_read_blt_16_8_scaling"]
+    for internal_iops_chart_name in internal_iops_chart_names:
+        output_name = "output_read_iops"
+        iops_chart = MetricChart.objects.get(internal_chart_name=internal_iops_chart_name)
+        data_sets = []
+        if "rand_read" in internal_iops_chart_name:
+            fio_job_names = fio_rand_read_job_names
+            operation = "randread"
+        else:
+            fio_job_names = fio_read_job_names
+            operation = "read"
+        for fio_job_name in fio_job_names:
+            if "1_1" in fio_job_name:
+                name = "qd1"
+            elif "16_1" in fio_job_name:
+                name = "qd16"
+            elif "16_2" in fio_job_name:
+                name = "qd32"
+            elif "16_4" in fio_job_name:
+                name = "qd64"
+            else:
+                name = "qd128"
+
+            one_data_set = {}
+            one_data_set["name"] = name
+            one_data_set["inputs"] = {}
+            one_data_set["inputs"]["input_operation"] = operation
+            one_data_set["inputs"]["input_platform"] = FunPlatform.F1
+            one_data_set["inputs"]["input_fio_job_name"] = fio_job_name
+            one_data_set["output"] = {"name": output_name, 'min': 0, "max": -1, "expected": -1, "reference": -1,
+                                      "unit": iops_chart.visualization_unit}
+            data_sets.append(one_data_set)
+        iops_chart.data_sets = json.dumps(data_sets)
+        iops_chart.save()
+
+    chart_name = "Latency"
+    fio_job_names = fio_read_job_names + fio_rand_read_job_names
+    for fio_job_name in fio_job_names:
+        if "1_1" in fio_job_name:
+            name = "qd1"
+        elif "16_1" in fio_job_name:
+            name = "qd16"
+        elif "16_2" in fio_job_name:
+            name = "qd32"
+        elif "16_4" in fio_job_name:
+            name = "qd64"
+        else:
+            name = "qd128"
+
+        if "randread" in fio_job_name:
+            internal_chart_name = "rand_read_" + name + "_nvmetcp_output_latency"
+            copy = copy_rand_read
+        else:
+            internal_chart_name = "read_" + name + "_nvmetcp_output_latency"
+            copy = copy_read
+        try:
+            latency_chart = MetricChart.objects.get(internal_chart_name=internal_chart_name)
+            data_sets = json.loads(latency_chart.data_sets)
+            for data_set in data_sets:
+                data_set["inputs"]["input_fio_job_name"] = fio_job_name
+            latency_chart.data_sets = json.dumps(data_sets)
+            latency_chart.save()
+        except:
+            chart = MetricChart.objects.get(internal_chart_name=copy)
+            data_sets = json.loads(chart.data_sets)
+            for data_set in data_sets:
+                data_set["inputs"]["input_fio_job_name"] = fio_job_name
+            metric_id = LastMetricId.get_next_id()
+            MetricChart(chart_name=chart_name,
+                        metric_id=metric_id,
+                        internal_chart_name=internal_chart_name,
+                        data_sets=json.dumps(data_sets),
+                        leaf=True,
+                        description=chart.description,
+                        owner_info=chart.owner_info,
+                        source=chart.source,
+                        positive=chart.positive,
+                        y1_axis_title=chart.y1_axis_title,
+                        visualization_unit=chart.y1_axis_title,
+                        metric_model_name=chart.metric_model_name,
+                        base_line_date=chart.base_line_date,
+                        work_in_progress=False,
+                        platform=FunPlatform.F1).save()
+    print "added 128 qdepth charts for raw block nvmetcp"
+
+if __name__ == "__main__":
+    fio_job_names = ["fio_tcp_randwrite_blt_1_1_scaling",
+                     "fio_tcp_randwrite_blt_8_1_scaling",
+                     "fio_tcp_randwrite_blt_16_1_scaling",
+                     "fio_tcp_randwrite_blt_16_2_scaling"]
+    internal_iops_chart_name = "rand_write_qd_nvmetcp_output_iops"
+    copy = "rand_read_qd_nvmetcp_output_iops"
+    chart = MetricChart.objects.get(internal_chart_name=copy)
+    chart_name = "IOPS"
+    data_sets = []
+    for fio_job_name in fio_job_names:
+        if "1_1" in fio_job_name:
+            name = "qd1"
+        elif "8_1" in fio_job_name:
+            name = "qd8"
+        elif "16_1" in fio_job_name:
+            name = "qd16"
+        else:
+            name = "qd32"
+        one_data_set = {}
+        one_data_set["name"] = name
+        one_data_set["inputs"] = {}
+        one_data_set["inputs"]["input_operation"] = "randwrite"
+        one_data_set["inputs"]["input_platform"] = FunPlatform.F1
+        one_data_set["inputs"]["input_fio_job_name"] = fio_job_name
+        one_data_set["output"] = {"name": "output_write_iops", 'min': 0, "max": -1, "expected": -1, "reference": -1,
+                                  "unit": chart.visualization_unit}
+        data_sets.append(one_data_set)
+
+    metric_id = LastMetricId.get_next_id()
+    MetricChart(chart_name=chart_name,
+                metric_id=metric_id,
+                internal_chart_name=internal_iops_chart_name,
+                data_sets=json.dumps(data_sets),
+                leaf=True,
+                description=chart.description,
+                owner_info=chart.owner_info,
+                source=chart.source,
+                positive=chart.positive,
+                y1_axis_title=chart.y1_axis_title,
+                visualization_unit=chart.y1_axis_title,
+                metric_model_name=chart.metric_model_name,
+                base_line_date=chart.base_line_date,
+                work_in_progress=False,
+                platform=FunPlatform.F1).save()
+
+    internal_chart_names = ["rand_write_", "_nvmetcp_output_latency"]
+    chart_name = "Latency"
+    copy = "rand_read_qd1_nvmetcp_output_latency"
+    chart = MetricChart.objects.get(internal_chart_name=copy)
+    output_names = ["output_write_avg_latency", "output_write_99_latency", "output_write_99_99_latency"]
+    for fio_job_name in fio_job_names:
+        if "1_1" in fio_job_name:
+            name = "qd1"
+        elif "8_1" in fio_job_name:
+            name = "qd8"
+        elif "16_1" in fio_job_name:
+            name = "qd16"
+        else:
+            name = "qd32"
+        internal_chart_name = internal_chart_names[0] + name + internal_chart_names[1]
+        data_sets = []
+        for output_name in output_names:
+            if "_avg_" in output_name:
+                name = "avg"
+            elif "_99_99_" in output_name:
+                name = "99.99%"
+            else:
+                name = "99%"
+            one_data_set = {}
+            one_data_set["name"] = name
+            one_data_set["inputs"] = {}
+            one_data_set["inputs"]["input_operation"] = "randwrite"
+            one_data_set["inputs"]["input_platform"] = FunPlatform.F1
+            one_data_set["inputs"]["input_fio_job_name"] = fio_job_name
+            one_data_set["output"] = {"name": output_name, 'min': 0, "max": -1, "expected": -1, "reference": -1,
+                                      "unit": chart.visualization_unit}
+            data_sets.append(one_data_set)
+        metric_id = LastMetricId.get_next_id()
+        MetricChart(chart_name=chart_name,
+                    metric_id=metric_id,
+                    internal_chart_name=internal_chart_name,
+                    data_sets=json.dumps(data_sets),
+                    leaf=True,
+                    description=chart.description,
+                    owner_info=chart.owner_info,
+                    source=chart.source,
+                    positive=chart.positive,
+                    y1_axis_title=chart.y1_axis_title,
+                    visualization_unit=chart.y1_axis_title,
+                    metric_model_name=chart.metric_model_name,
+                    base_line_date=chart.base_line_date,
+                    work_in_progress=False,
+                    platform=FunPlatform.F1).save()
+    print "added random write raw block nvmetcp"
+
