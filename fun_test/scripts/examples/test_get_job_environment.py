71a463249af79b483bc5a94065c997e8bfaab7f2
Ash41291 <ashwin.s@fungible.com>
2019-05-24 13:32:21 -0700

    added temp suite to test update and get job environment

---
+++ fun_test/scripts/examples/test_get_job_environment.py
@@ -0,0 +1,5 @@
+from lib.system.fun_test import *
+
+if __name__ == "__main__":
+    result = fun_test.get_job_environment_variable("test")
+    print "result got from temp1.py is {}".format(result)
---
+++ fun_test/scripts/examples/test_update_job_environment.py
@@ -0,0 +1,5 @@
+from lib.system.fun_test import *
+
+if __name__ == "__main__":
+    fun_test.update_job_environment_variable("test", 100)
+    print "updated test with values as 100"
