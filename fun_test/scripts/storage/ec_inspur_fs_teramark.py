34d206ef1f261bffa641aebf0f86189e8d1f6391
Ravi Hulle <ravi.hulle@fungible.com>
2019-05-25 17:39:54 +0530

    * Reordered the test case execution in script     * commented failed test cases so it does not impact next script execution

--- fun_test/scripts/storage/ec_inspur_fs_teramark.py
+++ fun_test/scripts/storage/ec_inspur_fs_teramark.py
@@ -826,8 +826,8 @@ class RandReadWrite8kBlocksLatencyTest(ECVolumeLevelTestcase):
 if __name__ == "__main__":
     ecscript = ECVolumeLevelScript()
     ecscript.add_test_case(RandReadWrite8kBlocks())
-    ecscript.add_test_case(MixedRandReadWriteIOPS())
-    ecscript.add_test_case(SequentialReadWrite1024kBlocks())
+    # ecscript.add_test_case(MixedRandReadWriteIOPS())
+    # ecscript.add_test_case(SequentialReadWrite1024kBlocks())
     # ecscript.add_test_case(OLTPModelReadWriteIOPS())
     # ecscript.add_test_case(OLAPModelReadWriteIOPS())
     # ecscript.add_test_case(RandReadWrite8kBlocksLatencyTest())
