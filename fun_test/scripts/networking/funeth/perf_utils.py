ca973392ada877653fa51c08d5413089f5edd0eb
George Liang <george.liang@fungible.com>
2019-05-27 18:57:35 -0700

    Increase timeout for flow list

--- fun_test/scripts/networking/funeth/perf_utils.py
+++ fun_test/scripts/networking/funeth/perf_utils.py
@@ -136,6 +136,8 @@ def collect_host_stats(funeth_obj, version, when='before', duration=0):
 
 def collect_dpc_stats(network_controller_objs, fpg_interfaces, version, when='before'):
 
+    tc_id = fun_test.current_test_case_id
+
     # peek resource/pc/[1], and peek resource/pc/[1]
     #for nc_obj in network_controller_objs:
     #    for pc_id in (1, 2):
@@ -154,11 +156,11 @@ def collect_dpc_stats(network_controller_objs, fpg_interfaces, version, when='be
     #checkpoint = "Get Flow list {} test".format(when)
     for nc_obj in network_controller_objs:
         fun_test.log_module_filter("random_module")
-        output = nc_obj.get_flow_list().get('data')
-        fun_test.sleep("Waiting for flow list cmd dump to complete", seconds=2)
+        output = nc_obj.get_flow_list(timeout=180).get('data')
+        fun_test.sleep("Waiting for flow list cmd dump to complete", seconds=5)
         fun_test.log_module_filter_disable()
-        flowlist_temp_filename = '{}_F1_{}_flowlist_{}.txt'.format(str(version), network_controller_objs.index(nc_obj),
-                                                                   when)
+        flowlist_temp_filename = '{}_{}_F1_{}_flowlist_{}.txt'.format(str(version), tc_id,
+                                                                      network_controller_objs.index(nc_obj), when)
         file_path = fun_test.get_test_case_artifact_file_name(flowlist_temp_filename)
         with open(file_path, 'w') as f:
             json.dump(output, f, indent=4, separators=(',', ': '), sort_keys=True)
