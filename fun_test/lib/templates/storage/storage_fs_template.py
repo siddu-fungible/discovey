8f8a33bf52e7bc9a464e02d1150f6242b4fce66b
Alagarswamy Devaraj <alagarswamy.devaraj@fungible.com>
2019-05-31 22:45:03 +0530

    * Implemented stop_container method to stop the docker containter spun by this object

--- fun_test/lib/templates/storage/storage_fs_template.py
+++ fun_test/lib/templates/storage/storage_fs_template.py
@@ -436,6 +436,27 @@ class StorageFsTemplate(object):
             result['status'] = True
         return result
 
+    def stop_container(self, *container_name):
+        result = True
+        cmd = "docker stop"
+
+        # If no container name is passed preparing the list with the container names already available in this object
+        if not container_name:
+            container_name = sorted(self.container_info)
+
+        for name in container_name:
+            cmd += " " + name
+
+        # Stopping the container(s)
+        stopped_container = self.come_obj.command(cmd, timeout=self.DEFAULT_TIMEOUT).split("\n")
+        stopped_container = [name.strip("\r") for name in stopped_container]
+
+        # Checking whether the container is not stopped or not. If not stopped the
+        for name in container_name:
+            if name not in stopped_container:
+                result = False
+        return result
+
     """
     def clear_containers(self):
         # Stop Container F1_0
