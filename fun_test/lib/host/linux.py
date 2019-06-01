4e74af34b77935414cb4b1bf3b02cda565ed8a15
John Abraham <john.abraham@fungible.com>
2019-05-31 22:51:07 -0700

    begin to lower ping counts

--- fun_test/lib/host/linux.py
+++ fun_test/lib/host/linux.py
@@ -1974,7 +1974,7 @@ class Linux(object, ToDictMixin):
         ping_result = False
         while not host_is_up and not max_reboot_timer.is_expired():
             if service_host and not ping_result:
-                ping_result = service_host.ping(dst=self.host_ip, count=20)
+                ping_result = service_host.ping(dst=self.host_ip, count=10)
                 if ping_result:
                     max_reboot_timer = FunTimer(max_time=30)
             if ping_result or not service_host:
