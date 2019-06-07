d61afe31f3fd0e2611e96698600d6dfd8099ed13
Yajat Singh <yajat.singh@fungible.com>
2019-06-07 16:51:04 -0700

    Add check for lspci -d 1dad:

--- fun_test/scripts/networking/funcp/helper.py
+++ fun_test/scripts/networking/funcp/helper.py
@@ -22,7 +22,7 @@ def verify_host_pcie_link(hostname, username="localadmin", password="Precious1*"
             else:
                 fun_test.log("Cannot ping host")
                 fun_test.sleep(seconds=10, message="waiting for host")
-        funeth_op=""
+        funeth_op = ""
         funeth_op = linux_obj.command(command="lsmod | grep funeth")
         if "funeth" not in funeth_op:
             linux_obj.reboot()
@@ -44,15 +44,40 @@ def verify_host_pcie_link(hostname, username="localadmin", password="Precious1*"
             if count == 5:
                 fun_test.test_assert(expression=False, message="Cant reach server %s" % hostname)
 
-    lspci_out = linux_obj.sudo_command(command="sudo lspci -d 1dad: -vv | grep LnkSta")
     result = "1"
-    if mode not in lspci_out:
-        if "LnkSta" not in lspci_out:
-            fun_test.critical("PCIE link did not come up")
-            result = "0"
+    reboot_count = 1
+    while True:
+        lspci_out = linux_obj.sudo_command(command="sudo lspci -d 1dad: -vv | grep LnkSta")
+        if mode not in lspci_out:
+            if "LnkSta" not in lspci_out:
+                fun_test.critical("PCIE link did not come up")
+                result = "0"
+                if reboot_count < 2:
+                    reboot_count += 1
+                    power_cycle_host(hostname)
+                    fun_test.sleep(message="Waiting for host after power cycle", seconds=90)
+                    retries = 0
+                    while not linux_obj.check_ssh():
+                        fun_test.sleep(message="Waiting for host after power cycle", seconds=15)
+                        retries += 1
+                        if retries > 10:
+                            break
+                    continue
+                else:
+                    result = "0"
+                    break
+            else:
+                fun_test.critical("PCIE link did not come up in %s mode" % mode)
+                output = linux_obj.command('lspci -d 1dad:')
+                critical_log(re.search(r'Ethernet controller: (?:Device 1dad:00f1|Fungible Device 00f1)', output),
+                             message="Check ethernet device")
+                result = "2"
+                break
         else:
-            fun_test.critical("PCIE link did not come up in %s mode" % mode)
-            result = "2"
+            output = linux_obj.command('lspci -d 1dad:')
+            critical_log(re.search(r'Ethernet controller: (?:Device 1dad:00f1|Fungible Device 00f1)', output),
+                         message="Check ethernet device")
+            break
     return result
 
 
@@ -75,13 +100,16 @@ def rmmod_funeth_host(hostname, username="localadmin", password="Precious1*"):
             fun_test.log("Cannot ping host")
             fun_test.sleep(seconds=15, message="waiting for host")
     funeth_op = ""
-    funeth_op = linux_obj.command(command="lsmod | grep funeth")
-    if "funeth" not in funeth_op:
-        return True
-    else:
-        funeth_rm = linux_obj.sudo_command("rmmod funeth")
-        critical_log(expression="ERROR" not in funeth_rm, message="Funeth removed succesfully")
-        return True
+    try:
+        funeth_op = linux_obj.command(command="lsmod | grep funeth")
+        if "funeth" not in funeth_op:
+            return True
+        else:
+            funeth_rm = linux_obj.sudo_command("rmmod funeth")
+            critical_log(expression="ERROR" not in funeth_rm, message="Funeth removed succesfully")
+            return True
+    except:
+        return False
 
 
 def power_cycle_host(hostname):
@@ -99,6 +127,7 @@ def test_host_pings(host, ips):
     fun_test.log("================")
     linux_obj = Linux(host_ip=host, ssh_username="localadmin", ssh_password="Precious1*")
     for hosts in ips:
+        linux_obj.command(command="ifconfig -a")
         result = linux_obj.ping(dst=hosts)
         if result:
             fun_test.log("%s can reach %s" % (host, hosts))
@@ -119,7 +148,7 @@ def setup_hu_host(funeth_obj, update_driver=True):
         if update_src_result:
             funsdk_commit, funsdk_bld, driver_commit, driver_bld = update_src_result
             critical_log(update_src_result, 'Update funeth driver source code.')
-        critical_log(funeth_obj.build(parallel=True), 'Build funeth driver.')
+        fun_test.test_assert(funeth_obj.build(parallel=True), 'Build funeth driver.')
     critical_log(funeth_obj.load(sriov=4), 'Load funeth driver.')
     for hu in funeth_obj.hu_hosts:
         linux_obj = funeth_obj.linux_obj_dict[hu]
@@ -149,7 +178,6 @@ def get_ethtool_on_hu_host(funeth_obj):
 
 def critical_log(expression, message):
     if expression:
-
         fun_test.test_assert(expression=expression, message=message)
 
     if not expression:
