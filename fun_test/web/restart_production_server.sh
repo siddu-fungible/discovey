aefa627b36bdb00fdf911368075535c9eb2bcc52
QA Automation <qa-admin@fungible.com>
2019-06-11 16:43:12 -0700

    add migrations

--- fun_test/web/restart_production_server.sh
+++ fun_test/web/restart_production_server.sh
@@ -1,6 +1,7 @@
 kill `cat /tmp/fun_test_web_server.pid`
 kill -9 `cat /tmp/fun_test_web_server.pid`
 rm -rf /tmp/fun_test_web_server.pid
+python web/manage.py bump_version
 nohup python web/start_production_server.py $1 &> server.out &
 sleep 3
 ps -ef | grep start_production_server
