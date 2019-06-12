4e75fa82e8ee4be432d62b168047c4e9d598eaf0
durgaraghu1 <durga.raghu@fungible.com>
2019-06-12 14:00:15 -0700

    nvme connect/disconnect test

--- fun_test/scripts/storage/nvme-test.py
+++ fun_test/scripts/storage/nvme-test.py
@@ -106,33 +106,6 @@ class BLTVolumePerformanceScript(FunTestScript):
         fun_test.shared_variables["storage_controller"] = self.storage_controller
 
     def cleanup(self):
-        '''
-        try:
-            self.blt_details = fun_test.shared_variables["blt_details"]
-            self.end_host_inst = fun_test.shared_variables["end_host_inst"]
-            command_result = self.end_host_inst.sudo_command(
-                "nvme disconnect -n nqn.2017-05.com.fungible:nss-uuid1 -d nvme0n1")
-            fun_test.log(command_result)
-
-            thin_uuid = fun_test.shared_variables["thin_uuid"]
-            blt_count = fun_test.shared_variables["blt_count"]
-            for x in range(1, blt_count + 1, 1):
-                curr_uuid = thin_uuid[x-1]
-                command_result = self.storage_controller.volume_detach_remote(
-                    ns_id=x, uuid=curr_uuid, remote_ip=tb_config['tg_info'][0]['remote_ip'])
-                if not command_result["status"]:
-                    fun_test.test_assert(command_result["status"], "Detach of BLT {} with uuid {}".
-                                         format(x, curr_uuid))
-                command_result = self.storage_controller.delete_volume(type=self.blt_details["type"],
-                                                                       name="thin_block" + str(x),
-                                                                       uuid=curr_uuid,
-                                                                       command_duration=30)
-                if not command_result["status"]:
-                    fun_test.test_assert(command_result["status"], "Delete of BLT {} with uuid {}".
-                                         format(x, curr_uuid))
-        except:
-            fun_test.log("Clean-up of volume failed.")
-        '''
         fun_test.log("FS cleanup")
         fun_test.shared_variables["fs"].cleanup()
 
@@ -334,191 +307,53 @@ class BLTVolumePerformanceTestcase(FunTestCase):
                 self.end_host.sudo_command("arp -s {} 00:de:ad:be:ef:00".format(iface_gw))
 
             fun_test.sleep("x86 Config done", seconds=10)
-            if hasattr(self, "nvme_io_q"):
-                command_result = self.end_host.sudo_command(
-                    "nvme connect -t {} -a {} -s {} -n {} -i {}".
-                    format(unicode.lower(nvme_transport),
+
+            num_nvme = self.num_nvme
+            print "nvme connect and disconnect test"
+            fun_test.log("nvme connect and disconnect test")
+
+            for i in range(num_nvme):
+               command_result = self.end_host.sudo_command(
+                   "nvme connect -t {} -a {} -s {} -n {}".
+                       format(unicode.lower(nvme_transport),
                            tb_config['dut_info'][0]['f1_ip'],
                            tb_config['dut_info'][0]['tcp_port'],
-                           self.nqn,
-                           self.nvme_io_q))
-                fun_test.log(command_result)
-            else:
-                command_result = self.end_host.sudo_command(
-                    "nvme connect -t {} -a {} -s {} -n {}".
-                        format(unicode.lower(nvme_transport),
-                               tb_config['dut_info'][0]['f1_ip'],
-                               tb_config['dut_info'][0]['tcp_port'],
-                               self.nqn))
-                fun_test.log(command_result)
+                           self.nqn))
+               fun_test.log(command_result)
 
-            # Checking that the above created BLT volume is visible to the end host
-            fun_test.sleep("Sleeping for couple of seconds for the volume to accessible to the host", 5)
-            self.volume_name = self.nvme_device.replace("/dev/", "") + "n" + str(self.blt_details["ns_id"])
-            lsblk_output = self.end_host.lsblk()
-            fun_test.test_assert(self.volume_name in lsblk_output, "{} device available".format(self.volume_name))
-            fun_test.test_assert_expected(expected="disk", actual=lsblk_output[self.volume_name]["type"],
-                                          message="{} device type check".format(self.volume_name))
+               fun_test.sleep("Sleeping for couple of seconds for the volume to accessible to the host", 5)
+               self.volume_name = self.nvme_device.replace("/dev/", "") + "n" + str(self.blt_details["ns_id"])
+               lsblk_output = self.end_host.lsblk()
+               fun_test.test_assert(self.volume_name in lsblk_output, "{} device available".format(self.volume_name))
+               fun_test.test_assert_expected(expected="disk", actual=lsblk_output[self.volume_name]["type"],
+                                             message="{} device type check".format(self.volume_name))
 
-            self.end_host.sudo_command("dmesg | grep -i nvme")
+               self.end_host.sudo_command("dmesg | grep -i nvme")
 
-            # Pre-conditioning the volume (one time task)
-            '''
-            if self.warm_up_traffic:
-                fun_test.log("Initial Write IO to volume, this might take long time depending on fio --size provided")
-                fio_output = self.end_host.pcie_fio(filename=self.nvme_block_device, **self.warm_up_fio_cmd_args)
-                fun_test.test_assert(fio_output, "Pre-populating the volume")
-                fun_test.log("FIO Command Output:\n{}".format(fio_output))
-                fun_test.sleep("Sleeping for {} seconds between iterations".format(self.iter_interval),
-                               self.iter_interval)
-            '''                   
+               time.sleep(5)
+               command_result1 = self.end_host.sudo_command(
+                   "nvme disconnect -n {} -d nvme0n1".
+                      format(unicode.lower(nvme_transport),
+                             self.nqn))
+
+               fun_test.log(command_result1)
+
+               fun_test.sleep("Sleeping for couple of seconds foddr the volume to accessible to the host", 5)
+               self.volume_name = self.nvme_device.replace("/dev/", "") + "n" + str(self.blt_details["ns_id"])
+               lsblk_output = self.end_host.lsblk()
+
+               self.end_host.sudo_command("dmesg | grep -i nvme")
+               time.sleep(5)
+
             fun_test.log("Done with setup")
             fun_test.shared_variables["blt"]["setup_created"] = True
 
     def run(self):
         pass
-        '''
-        testcase = self.__class__.__name__
-        test_method = testcase[3:]
 
-        row_data_dict = {}
-
-        table_data_headers = ["Num Jobs", "IO Depth", "Size", "Operation", "Write IOPS", "Read IOPS",
-                              "Write Throughput in MB/s", "Read Throughput in MB/s", "Write Latency in uSecs",
-                              "Write Latency 90 Percentile in uSecs", "Write Latency 95 Percentile in uSecs",
-                              "Write Latency 99 Percentile in uSecs", "Write Latency 99.99 Percentile in uSecs",
-                              "Read Latency in uSecs", "Read Latency 90 Percentile in uSecs",
-                              "Read Latency 95 Percentile in uSecs", "Read Latency 99 Percentile in uSecs",
-                              "Read Latency 99.99 Percentile in uSecs", "fio_job_name"]
-        table_data_cols = ["num_jobs", "iodepth", "size", "mode", "writeiops", "readiops", "writebw", "readbw",
-                           "writeclatency", "writelatency90", "writelatency95", "writelatency99", "writelatency9999",
-                           "readclatency", "readlatency90", "readlatency95", "readlatency99", "readlatency9999",
-                           "fio_job_name"]
-        table_data_rows = []
-        # Going to run the FIO test for the block size and iodepth combo listed in fio_jobs_iodepth in both write only
-        # & read only modes
-        fio_result = {}
-        fio_output = {}
-        internal_result = {}
-
-        for combo in self.fio_jobs_iodepth:
-            fio_result[combo] = {}
-            fio_output[combo] = {}
-            internal_result[combo] = {}
-
-            for mode in self.fio_modes:
-
-                tmp = combo.split(',')
-                fio_numjobs = tmp[0].strip('() ')
-                fio_iodepth = tmp[1].strip('() ')
-                fio_block_size = self.fio_bs
-                fio_result[combo][mode] = True
-                internal_result[combo][mode] = True
-                row_data_dict["mode"] = mode
-                row_data_dict["iodepth"] = fio_iodepth
-                row_data_dict["num_jobs"] = fio_numjobs
-                row_data_dict["block_size"] = fio_block_size
-                file_size_in_gb = self.blt_details["capacity"] / 1073741824
-                row_data_dict["size"] = str(file_size_in_gb) + "GB"
-
-                fun_test.log("Running FIO {} test for blocksize : {} using num_jobs: {}, IO depth: {}".
-                             format(mode, self.fio_bs, fio_numjobs, fio_iodepth))
-
-                if int(fio_numjobs) == 1:
-                    cpus_allowed = "8"
-                elif int(fio_numjobs) == 4:
-                    cpus_allowed = "8,9,10,11"
-                elif int(fio_numjobs) >= 8:
-                    cpus_allowed = "8,9,10,11,12,13,14,15,24,25,26,27,28,29,30,31"
-
-                # Flush cache before read test
-                self.end_host.sudo_command("sync")
-                self.end_host.sudo_command("echo 3 > /proc/sys/vm/drop_caches")
-
-                # Check EQM stats before test
-                # self.eqm_stats_before = {}
-                # self.eqm_stats_before = self.storage_controller.peek(props_tree="stats/eqm")
-                # eqm_result = False
-
-                fun_test.log("Running FIO...")
-                fio_job_name = "fio_tcp_" + mode + "_" + "blt" + "_" + fio_numjobs + "_" + fio_iodepth + "_" + self.fio_job_name[mode]
-                # Executing the FIO command for the current mode, parsing its out and saving it as dictionary
-                fio_output[combo][mode] = {}
-                fio_output[combo][mode] = self.end_host.pcie_fio(filename=self.nvme_block_device,
-                                                                 numjobs=fio_numjobs,
-                                                                 rw=mode,
-                                                                 bs=fio_block_size,
-                                                                 iodepth=fio_iodepth,
-                                                                 name=fio_job_name,
-                                                                 cpus_allowed=cpus_allowed,
-                                                                 **self.fio_cmd_args)
-                fun_test.log("FIO Command Output:")
-                fun_test.log(fio_output[combo][mode])
-                fun_test.test_assert(fio_output[combo][mode], "Fio {} test for numjobs {} & iodepth {}".
-                                     format(mode, fio_numjobs, fio_iodepth))
-
-                self.eqm_stats_after = {}
-                self.eqm_stats_after = self.storage_controller.peek(props_tree="stats/eqm")
-
-                for field, value in self.eqm_stats_before["data"].items():
-                    current_value = self.eqm_stats_after["data"][field]
-                    if (value != current_value) and (field != "incoming BN msg valid"):
-                        eqm_result = True
-                        # fun_test.test_assert_expected(value, current_value, "EQM {} stat mismatch".format(field))
-                        stat_delta = current_value - value
-                        fun_test.critical("There is a mismatch in {} stat, delta {}".
-                                          format(field, stat_delta))
-                
-                # Boosting the fio output with the testbed performance multiplier
-                multiplier = tb_config["dut_info"][0]["perf_multiplier"]
-                for op, stats in fio_output[combo][mode].items():
-                    for field, value in stats.items():
-                        if field == "iops":
-                            fio_output[combo][mode][op][field] = int(round(value * multiplier))
-                        if field == "bw":
-                            # Converting the KBps to MBps
-                            fio_output[combo][mode][op][field] = int(round(value * multiplier / 1000))
-                        if field == "latency":
-                            fio_output[combo][mode][op][field] = int(round(value / multiplier))
-                fun_test.log("FIO Command Output after multiplication:")
-                fun_test.log(fio_output[combo][mode])
-
-                fun_test.sleep("Sleeping for {} seconds between iterations".format(self.iter_interval),
-                               self.iter_interval)
-
-                # Comparing the FIO results with the expected value for the current block size and IO depth combo
-                for op, stats in self.expected_fio_result[combo][mode].items():
-                    for field, value in stats.items():
-                        actual = fio_output[combo][mode][op][field]
-                        row_data_dict[op + field] = actual
-
-                row_data_dict["fio_job_name"] = fio_job_name
-
-            # Building the table row for this variation for both the script table and performance dashboard
-            row_data_list = []
-            for i in table_data_cols:
-                if i not in row_data_dict:
-                    row_data_list.append(-1)
-                else:
-                    row_data_list.append(row_data_dict[i])
-            table_data_rows.append(row_data_list)
-            post_results("BLT_IO_Scaling", test_method, *row_data_list)
-
-        table_name = "BLT " + unicode.upper(mode) + " Results Summary"
-        table_data = {"headers": table_data_headers, "rows": table_data_rows}
-        fun_test.add_table(panel_header="{}".format(table_name), table_name=self.summary, table_data=table_data)
-        # Posting the final status of the test result
-        test_result = True
-        fun_test.log(fio_result)
-        fun_test.log(internal_result)
-        for combo in self.fio_jobs_iodepth:
-            for mode in self.fio_modes:
-                if not fio_result[combo][mode] or not internal_result[combo][mode]:
-                    test_result = False
-        fun_test.log("Test Result: {}".format(test_result))
-        '''
     def cleanup(self):
-        pass
+        fun_test.log("FS cleanup")
+        fun_test.shared_variables["fs"].cleanup()
 
 
 class BLTFioSeqRead(BLTVolumePerformanceTestcase):
@@ -533,21 +368,6 @@ class BLTFioSeqRead(BLTVolumePerformanceTestcase):
         4. Run the FIO Seq Read test(without verify) from the 
          host with numjobs & IOdepth : (1,1),(8,1),(16,1),(16,2),(16,4) and check the performance. 
         ''')
-
-
-class BLTFioRandRead(BLTVolumePerformanceTestcase):
-
-    def describe(self):
-        self.set_test_details(id=2,
-                              summary="Random Read performance on BLT volume with different IO Depth",
-                              steps='''
-        1. Create a BLT on FS attached with SSD.
-        2. Export (Attach) this BLT to the external host connected via the network interface. 
-        3. Pre-condition the volume with write test using fio.
-        4. Run the FIO Rand Read test(without verify) from the 
-         host with numjobs & IOdepth : (1,1),(8,1),(16,1),(16,2),(16,4) and check the performance. 
-        ''')
-
 
 if __name__ == "__main__":
 
