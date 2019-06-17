a3a4a2561ab75a97fdf897057128fbe0445a2fc9
Ash41291 <ashwin.s@fungible.com>
2019-06-17 13:08:29 -0700

    added charts

--- fun_test/web/fun_test/maintenance.py
+++ fun_test/web/fun_test/maintenance.py
@@ -76,7 +76,7 @@ if __name__ == "__main_0%_compression__":
             chart.save()
     print "added 0 compression dataset for durable volume EC + compression"
 
-if __name__ == "__main__":
+if __name__ == "__main_inspur_8k_comp__":
     internal_chart_names = ["inspur_8132_8k_rand_rw_comp_output_latency", "inspur_8132_8k_rand_rw_comp_output_iops"]
     model_name = "BltVolumePerformance"
     base_line_date = datetime(year=2019, month=6, day=10, minute=0, hour=0, second=0)
@@ -136,3 +136,52 @@ if __name__ == "__main__":
                     work_in_progress=False,
                     platform=FunPlatform.F1).save()
     print "added charts for inspur random read write compression"
+
+if __name__ == "__main__":
+    random_read_qdepths = ["qd1", "qd128", "qd256"]
+    random_write_qdepths = ["qd1", "qd64", "qd128"]
+    model_name = "BltVolumePerformance"
+    internal_chart_names = ["rand_read_qd1_nvmetcp_output_latency",
+                                       "rand_read_qd128_nvmetcp_output_latency",
+                                        "rand_read_qd256_nvmetcp_output_latency",
+                                        "rand_write_qd1_nvmetcp_output_latency",
+                                         "rand_write_qd64_nvmetcp_output_latency", "rand_write_qd128_nvmetcp_output_latency"]
+    fio_job_names = ["fio_tcp_randread_blt_1_1_scaling"]
+    fio_job_names = ["fio_tcp_", "_blt_"]
+    fio_job_names_randread = ["fio_tcp_randread_blt_1_1_scaling", "fio_tcp_randread_blt_16_8_scaling",
+                              "fio_tcp_randread_blt_32_4_nvols", "fio_tcp_randread_blt_32_8_nvols"]
+    fio_job_names_randwrite = ["fio_tcp_randwrite_blt_1_1_scaling", "fio_tcp_randwrite_blt_16_4_scaling",
+                               "fio_tcp_randwrite_blt_32_2_nvols", "fio_tcp_randwrite_blt_32_4_nvols"]
+
+    for internal_chart_name in internal_chart_names:
+        if "rand_read" in internal_chart_name:
+            # fio_job_name = fio_job_names[0] + "randread" + fio_job_names[1] +
+            operation = "randread"
+            fio_job_names = fio_job_names_randread
+        else:
+            operation = "randwrite"
+            fio_job_names = fio_job_names_randwrite
+        try:
+            chart = MetricChart.objects.get(internal_chart_name=internal_chart_name)
+            if chart:
+                data_sets = json.loads(chart.data_sets)
+                for data_set in data_sets:
+                    data_set["name"] = data_set["name"] + "(1 vol)"
+                chart.data_sets = json.dumps(data_sets)
+                chart.save()
+        except ObjectDoesNotExist:
+            print "working"
+
+if __name__ == "__main__":
+  internal_chart_name = ""
+        chart = MetricChart.objects.get(internal_chart_name=internal_chart_name)
+        data_sets = json.loads(chart.data_sets)
+        for data_set in data_sets:
+            data_set["name"] = data_set["name"] + "(1 vol)"
+        chart.data_sets
+
+
+
+
+
+
