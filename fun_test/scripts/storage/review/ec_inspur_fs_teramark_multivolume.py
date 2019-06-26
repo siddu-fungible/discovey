b73884d711cf399192d2f7a3a46789c4e54756f1
Ravi Hulle <ravi.hulle@fungible.com>
2019-06-26 22:27:20 +0530

    * bug fix

--- fun_test/scripts/storage/review/ec_inspur_fs_teramark_multivolume.py
+++ fun_test/scripts/storage/review/ec_inspur_fs_teramark_multivolume.py
@@ -134,11 +134,11 @@ class ECVolumeLevelScript(FunTestScript):
         # Pulling reserved DUTs and Hosts and test bed specific configuration if script is submitted with testbed-type
         # suite-based
         elif self.testbed_type == "suite-based":
+            self.topology_helper = TopologyHelper()
             self.available_dut_indexes = self.topology_helper.get_available_duts().keys()
             self.required_hosts = self.topology_helper.get_available_hosts()
             self.testbed_config = self.topology_helper.spec
             self.total_available_duts = len(self.available_dut_indexes)
-            self.topology_helper = TopologyHelper()
 
         fun_test.test_assert(expression=self.num_duts <= self.total_available_duts,
                              message="Testbed has enough DUTs")
