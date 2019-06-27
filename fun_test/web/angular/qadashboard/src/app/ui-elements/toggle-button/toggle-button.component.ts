e4fb72b2ac170262dcecdd20cc16d704f16c7dd6
John Abraham <john.abraham@fungible.com>
2019-06-26 17:45:04 -0700

    add disable toggle button

---
+++ fun_test/web/angular/qadashboard/src/app/ui-elements/toggle-button/toggle-button.component.ts
@@ -0,0 +1,21 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-toggle-button',
+  templateUrl: './toggle-button.component.html',
+  styleUrls: ['./toggle-button.component.css']
+})
+export class ToggleButtonComponent implements OnInit {
+  @Input() checked: boolean = false;
+  @Output() changed: EventEmitter<boolean> = new EventEmitter();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onChange() {
+    this.checked = !this.checked;
+    this.changed.emit(this.checked);
+  }
+
+}
