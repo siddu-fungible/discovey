63f0648e8def0ba1ea86d6ae3a429ea52c7e1468
John Abraham <john.abraham@fungible.com>
2019-07-03 13:02:41 -0700

    remove demo1

--- fun_test/web/angular/demo1/src/app/pipes/fun-table-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FunTableFilterPipe } from './fun-table-filter.pipe';
-
-describe('FunTableFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FunTableFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
