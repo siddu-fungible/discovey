c2f26333b5a43da53a585fac976b14c57d018529
Mohit Saxena <mohit.saxena@fungible.com>
2019-07-03 12:23:03 +0530

    commented 16 TCP flow cases

--- fun_test/scripts/networking/tcp/performance.py
+++ fun_test/scripts/networking/tcp/performance.py
@@ -575,6 +575,6 @@ if __name__ == '__main__':
     ts.add_test_case(TcpPerformance1Conn())
     ts.add_test_case(TcpPerformance4Conn())
     ts.add_test_case(TcpPerformance8Conn())
-    ts.add_test_case(TcpPerformance16Conn2Host())
+    #ts.add_test_case(TcpPerformance16Conn2Host())
 
     ts.run()
