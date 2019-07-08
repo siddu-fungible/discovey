19a32d3d546e5fa43b87cee6b3cc6dd708fcc149
Ash41291 <ashwin.s@fungible.com>
2019-07-08 15:02:06 -0700

    added s1 teramarks jenkins schedule

---
+++ fun_test/scripts/system/s1_teramarks.py
@@ -0,0 +1,155 @@
+from lib.system.fun_test import *
+from lib.utilities.jenkins_manager import JenkinsManager
+from lib.host.lsf_status_server import LsfStatusServer
+from fun_global import FunPlatform
+
+
+class MyScript(FunTestScript):
+    def describe(self):
+        self.set_test_details(steps="""
+
+        1. Step 1
+        2. Step 2
+        3. Step 3""")
+
+    def setup(self):
+        fun_test.log("Script-level setup")
+        self.lsf_status_server = LsfStatusServer()
+        fun_test.shared_variables["lsf_status_server"] = self.lsf_status_server
+
+    def cleanup(self):
+        fun_test.log("Script-level cleanup")
+
+
+class PalladiumTC(FunTestCase):
+    boot_args = ""
+    tags = ""
+    note = ""
+    fun_os_make_flags = ""
+    hw_model = "S1_Compute"
+    max_duration = 900
+    disable_assertions = "true"
+    hw_version = "rel_06152019"
+    run_target = "palladium"
+
+    def describe(self):
+        self.set_test_details(id=0,
+                              summary="",
+                              steps="""
+        1. Steps 1
+        2. Steps 2
+        3. Steps 3
+                              """)
+
+    def setup(self):
+        fun_test.log("Testcase setup")
+
+    def cleanup(self):
+        fun_test.log("Testcase cleanup")
+
+    def run(self):
+        fun_test.add_checkpoint("Starting the jenkins build")
+        jenkins_manager = JenkinsManager()
+        params = {"BOOTARGS": self.boot_args,
+                  "MAX_DURATION": self.max_duration,
+                  "RUN_MODE": "Batch",
+                  "TAGS": self.tags,
+                  "NOTE": self.note,
+                  "DISABLE_ASSERTIONS": self.disable_assertions,
+                  "FUNOS_MAKEFLAGS": self.fun_os_make_flags,
+                  "HW_VERSION": self.hw_version,
+                  "RUN_TARGET": self.run_target,
+                  "HW_MODEL": self.hw_model}
+
+        build_result = jenkins_manager.build(params=params, extra_emails=[
+            "team-regression@fungible.com"])
+        fun_test.test_assert(build_result, "Build completed normally")
+
+
+class CryptoTeramarkTc(PalladiumTC):
+    boot_args = "app=crypto_test,crypto_tunnel_perf,crypto_api_perf,crypto_raw_speed --test-exit-fast --serial"
+    tags = "qa_s1_crypto_teramark"
+    note = "Crypto teramark app on S1"
+    fun_os_make_flags = "NDEBUG=1"
+
+    def describe(self):
+        self.set_test_details(id=1,
+                              summary="Schedule crypto s1 teramark app on Jenkins",
+                              steps="""
+        1. Steps 1
+        2. Steps 2
+        3. Steps 3
+                              """)
+
+
+class PkeTeramarkTc(PalladiumTC):
+    boot_args = "app=pke_rsa_crt_dec_no_pad_soak,pke_rsa_crt_dec_no_pad_4096_soak,pke_ecdh_soak_256,pke_ecdh_soak_25519,pke_x25519_2k_tls_soak,pke_p256_2k_tls_soak --serial"
+    tags = "qa_s1_pke_teramark"
+    hw_model = "S1_CUT"
+    note = "PKE teramark app on S1"
+
+    def describe(self):
+        self.set_test_details(id=2,
+                              summary="Schedule PKE teramark app on Jenkins",
+                              steps="""
+            1. Steps 1
+            2. Steps 2
+            3. Steps 3
+                                  """)
+
+
+class EcTeramarkTc(PalladiumTC):
+    boot_args = "app=qa_ec_stress min_ndata=8 max_ndata=8 min_nparity=4 max_nparity=4 min_stridelen=4096 max_stridelen=4096 syslog=2 --seq_fail --enable_multi_pcs num_pcs=8"
+    tags = "qa_s1_ec_teramark"
+    note = "EC teramark app on S1"
+    max_duration = 1800
+
+    def describe(self):
+        self.set_test_details(id=3,
+                              summary="Schedule EC teramark app on Jenkins",
+                              steps="""
+            1. Steps 1
+            2. Steps 2
+            3. Steps 3
+                                  """)
+
+
+class DfaTeramarkTc(PalladiumTC):
+    boot_args = "app=dfa_perf_bootstrap rbm-size=1m dfa_perf.pc_mask=255 dfa_perf.nflows=3072 dfa_perf.niterations=1024 syslog=2"
+    tags = "qa_s1_dfa_teramark"
+    note = "DFA teramark app on S1"
+    fun_os_make_flags = "PM_TESTS=1"
+
+    def describe(self):
+        self.set_test_details(id=3,
+                              summary="Schedule DFA teramark app on Jenkins",
+                              steps="""
+            1. Steps 1
+            2. Steps 2
+            3. Steps 3
+                                  """)
+
+
+class NfaTeramarkTc(PalladiumTC):
+    boot_args = "app=nfa_perf_bootstrap rbm-size=1m nfa_perf.pc_mask=255 nfa_perf.nflows=3584 nfa_perf.niterations=1024 syslog=2"
+    tags = "qa_s1_nfa_teramark"
+    note = "NFA teramark app on S1"
+    fun_os_make_flags = "PM_TESTS=1"
+
+    def describe(self):
+        self.set_test_details(id=3,
+                              summary="Schedule NFA teramark app on Jenkins",
+                              steps="""
+            1. Steps 1
+            2. Steps 2
+            3. Steps 3
+                                  """)
+
+if __name__ == "__main__":
+    myscript = MyScript()
+    myscript.add_test_case(CryptoTeramarkTc())
+    myscript.add_test_case(PkeTeramarkTc())
+    myscript.add_test_case(EcTeramarkTc())
+    myscript.add_test_case(DfaTeramarkTc())
+    myscript.add_test_case(NfaTeramarkTc())
+    myscript.run()
