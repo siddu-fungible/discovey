e86bfd5cfbf6843234e7f0aecaa562bcadefdc46
Rohan Nair <rohan.nair@fungible.com>
2019-07-08 17:29:40 -0700

    removed whitespace

--- fun_test/web/angular/qadashboard/src/app/regression/regression-summary-widget/regression-summary-widget.component.ts
+++ fun_test/web/angular/qadashboard/src/app/regression/regression-summary-widget/regression-summary-widget.component.ts
@@ -3,7 +3,8 @@ import {ApiService} from "../../services/api/api.service";
 import {LoggerService} from "../../services/logger/logger.service";
 import {from, Observable, of} from "rxjs";
 import {mergeMap, switchMap} from "rxjs/operators";
-import { CommonService } from "../../services/common/common.service";
+import {CommonService} from "../../services/common/common.service";
+
 @Component({
   selector: 'app-regression-summary-widget',
   templateUrl: './regression-summary-widget.component.html',
@@ -11,7 +12,10 @@ import { CommonService } from "../../services/common/common.service";
 })
 export class RegressionSummaryWidgetComponent implements OnInit {
 
-  initialFilterData = [{info: "Networking overall", payload: {module: "networking"}}, {info: "Storage overall", payload: {module: "storage"}}];
+  initialFilterData = [{info: "Networking overall", payload: {module: "networking"}}, {
+    info: "Storage overall",
+    payload: {module: "storage"}
+  }];
 
   y1Values: any = [];
   payload: any = {};
@@ -27,21 +31,21 @@ export class RegressionSummaryWidgetComponent implements OnInit {
 
   initializeY1Values() {
     this.y1Values = [{
-        name: 'Passed',
-        data: Array(this.x1Values.length).fill(0),
-        color: 'green'
+      name: 'Passed',
+      data: Array(this.x1Values.length).fill(0),
+      color: 'green'
     }
-    ,  {
+      , {
         name: 'Failed',
         data: Array(this.x1Values.length).fill(0),
         color: 'red'
 
-    }, {
+      }, {
         name: 'Not-run',
         data: Array(this.x1Values.length).fill(0),
         color: 'grey'
 
-    },
+      },
       {
         name: 'In-progress',
         data: Array(this.x1Values.length).fill(0),
@@ -58,18 +62,22 @@ export class RegressionSummaryWidgetComponent implements OnInit {
     new Observable(observer => {
       observer.next(true);
       observer.complete();
-      return () => {}
+      return () => {
+      }
     }).pipe(switchMap(response => {
       let numbers = [];
-      this.initialFilterData.map(filter => {numbers.push(numbers.length)});
+      this.initialFilterData.map(filter => {
+        numbers.push(numbers.length)
+      });
       return from(numbers).pipe(
-          mergeMap(filterIndex => this.fetchTestCaseExecutions(filterIndex)));
-    })).subscribe(response => {}, error => {
+        mergeMap(filterIndex => this.fetchTestCaseExecutions(filterIndex)));
+    })).subscribe(response => {
+    }, error => {
       this.logger.error('Failed to fetch test case executions');
     });
   }
 
-  fetchTestCaseExecutions(index: number): any{
+  fetchTestCaseExecutions(index: number): any {
     console.log('testing');
     let numPassed = 0;
     let numFailed = 0;
@@ -77,20 +85,17 @@ export class RegressionSummaryWidgetComponent implements OnInit {
     let numInProgress = 0;
     let today = new Date();
     let payload = this.initialFilterData[index].payload;
-      return this.apiService.post("/regression/get_test_case_executions_by_time" + "?days_in_past=1", payload).pipe(switchMap((response) => {
+    return this.apiService.post("/regression/get_test_case_executions_by_time" + "?days_in_past=1", payload).pipe(switchMap((response) => {
       for (let i in response.data) {
         let historyTime = new Date(this.commonService.convertToLocalTimezone(response.data[i].started_time));
-        if (this.commonService.isSameDay(historyTime, today) && !response.data[i].is_re_run){
+        if (this.commonService.isSameDay(historyTime, today) && !response.data[i].is_re_run) {
           if (response.data[i].result == 'FAILED') {
-          ++numFailed;
-          }
-          else if (response.data[i].result == 'PASSED'){
+            ++numFailed;
+          } else if (response.data[i].result == 'PASSED') {
             ++numPassed
-          }
-          else if (response.data[i].result == 'NOT_RUN'){
+          } else if (response.data[i].result == 'NOT_RUN') {
             ++numNotRun;
-          }
-          else if (response.data[i].result == 'IN_PROGRESS'){
+          } else if (response.data[i].result == 'IN_PROGRESS') {
             ++numInProgress;
           }
         }
@@ -101,11 +106,11 @@ export class RegressionSummaryWidgetComponent implements OnInit {
   }
 
   populateResults(index, numPassed, numFailed, numNotRun, numInProgress) {
-      this.y1Values[0].data[index] = numPassed; //make these local vars, pass to populateResults
-      this.y1Values[1].data[index] = numFailed;
-      this.y1Values[2].data[index] = numNotRun;
-      this.y1Values[3].data[index] = numInProgress;
-      this.y1Values = [...this.y1Values];
+    this.y1Values[0].data[index] = numPassed; //make these local vars, pass to populateResults
+    this.y1Values[1].data[index] = numFailed;
+    this.y1Values[2].data[index] = numNotRun;
+    this.y1Values[3].data[index] = numInProgress;
+    this.y1Values = [...this.y1Values];
   }
 }
 
