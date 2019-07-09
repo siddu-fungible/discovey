4faf6cd0cf97f9e562ff39bae9e269cf394b9612
Rohan Nair <rohan.nair@fungible.com>
2019-07-09 11:34:00 -0700

    declare performance widget component

--- fun_test/web/angular/qadashboard/src/app/app.module.ts
+++ fun_test/web/angular/qadashboard/src/app/app.module.ts
@@ -70,6 +70,8 @@ import {ToggleButtonComponent} from './ui-elements/toggle-button/toggle-button.c
 import {RegressionSummaryWidgetComponent} from './regression/regression-summary-widget/regression-summary-widget.component';
 import {NgxJsonViewerModule} from "ngx-json-viewer";
 import {AnnouncementComponent} from './announcement/announcement.component';
+import { PerformanceSummaryWidgetComponent } from './performance/performance-summary-widget/performance-summary-widget.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -122,6 +124,7 @@ import {AnnouncementComponent} from './announcement/announcement.component';
     ChartComponent,
     ToggleButtonComponent,
     RegressionSummaryWidgetComponent,
+    PerformanceSummaryWidgetComponent,
     AnnouncementComponent
   ],
   imports: [
