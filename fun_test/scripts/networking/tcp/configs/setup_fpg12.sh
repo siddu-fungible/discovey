e71cf3a01e00efeb58403de69484d08c6559bd5a
Mohit Saxena <mohit.saxena@fungible.com>
2019-07-26 13:59:21 +0530

    added command to load mellanox drivers

--- fun_test/scripts/networking/tcp/configs/setup_fpg12.sh
+++ fun_test/scripts/networking/tcp/configs/setup_fpg12.sh
@@ -8,4 +8,5 @@ sudo ifconfig fpg12 up
 sudo route add -net 29.1.1.0/24 gw 23.1.1.2
 sudo route add -host 20.1.1.1 gw 23.1.1.2
 sudo arp -s 23.1.1.2 00:de:ad:be:ef:00
+sudo python /local-home/localadmin/mks/mlx_load.py
 #sudo /usr/sbin/set_irq_affinity_bynode.sh 1 fpg12
