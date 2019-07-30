40abe465d299a1f57bab8bd845e75490ba05788c
Alagarswamy Devaraj <alagarswamy.devaraj@fungible.com>
2019-07-30 19:37:31 +0530

    * Minor bug fix to obtain the correct available dut list while running the script locally

--- fun_test/scripts/storage/pocs/inspur/ec_inspur_fs_teramark_multi_f1_multi_host_old.py
+++ fun_test/scripts/storage/pocs/inspur/ec_inspur_fs_teramark_multi_f1_multi_host_old.py
@@ -2,7 +2,9 @@ from lib.system.fun_test import *
 from lib.system import utils
 from web.fun_test.analytics_models_helper import BltVolumePerformanceHelper, get_data_collection_time
 from lib.fun.fs import Fs
+import re
 from lib.topology.topology_helper import TopologyHelper
+from lib.host.storage_controller import StorageController
 from lib.templates.storage.storage_fs_template import *
 from scripts.storage.storage_helper import *
 from collections import OrderedDict
@@ -86,78 +88,44 @@ class ECVolumeLevelScript(FunTestScript):
 
         # Using Parameters passed during execution, this will override global and config parameters
         job_inputs = fun_test.get_job_inputs()
+        fun_test.log("Provided job inputs: {}".format(job_inputs))
         if not job_inputs:
             job_inputs = {}
-        fun_test.log("Provided job inputs: {}".format(job_inputs))
         if "dut_start_index" in job_inputs:
             self.dut_start_index = job_inputs["dut_start_index"]
         if "host_start_index" in job_inputs:
             self.host_start_index = job_inputs["host_start_index"]
-        if "num_hosts" in job_inputs:
-            self.num_hosts = job_inputs["num_hosts"]
         if "update_workspace" in job_inputs:
             self.update_workspace = job_inputs["update_workspace"]
         if "update_deploy_script" in job_inputs:
             self.update_deploy_script = job_inputs["update_deploy_script"]
-        if "disable_wu_watchdog" in job_inputs:
-            self.disable_wu_watchdog = job_inputs["disable_wu_watchdog"]
-        else:
-            self.disable_wu_watchdog = True
+
+        fun_test.log("Global Config: {}".format(self.__dict__))
+
+        self.testbed_type = fun_test.get_job_environment_variable("test_bed_type")
+        self.testbed_config = fun_test.get_asset_manager().get_test_bed_spec(self.testbed_type)
+        fun_test.log("{} Testbed Config: {}".format(self.testbed_type, self.testbed_config))
+        self.total_avaialble_duts = len(self.testbed_config["dut_info"])
+        fun_test.log("Total Avaialble Duts: {}".format(self.total_avaialble_duts))
+
+        self.num_duts = (self.num_f1s / self.num_f1_per_fs)
+        fun_test.log("Num DUTs for current test: {}".format(self.num_duts))
+        fun_test.test_assert(expression=self.num_duts <= self.total_avaialble_duts, message="Testbed has enough DUTs")
+
+        # Skipping DUTs not required for this test
+        self.skip_dut_list = []
+        for index in xrange(0, self.dut_start_index):
+            self.skip_dut_list.append(index)
+        for index in xrange(self.dut_start_index+self.num_duts, self.total_avaialble_duts):
+            self.skip_dut_list.append(index)
+        fun_test.debug("DUTs that will be skipped: {}".format(self.skip_dut_list))
 
         # Deploying of DUTs
-        self.num_duts = int(round(float(self.num_f1s) / self.num_f1_per_fs))
-        fun_test.log("Num DUTs for current test: {}".format(self.num_duts))
-
-        # Pulling test bed specific configuration if script is not submitted with testbed-type suite-based
-        self.testbed_type = fun_test.get_job_environment_variable("test_bed_type")
-        if self.testbed_type != "suite-based":
-            self.testbed_config = fun_test.get_asset_manager().get_test_bed_spec(self.testbed_type)
-            fun_test.log("{} Testbed Config: {}".format(self.testbed_type, self.testbed_config))
-            self.fs_hosts_map = utils.parse_file_to_json(SCRIPTS_DIR + "/storage/inspur_fs_hosts_mapping.json")
-            self.available_hosts = self.fs_hosts_map[self.testbed_type]["host_info"]
-            self.full_dut_indexes = [int(i) for i in sorted(self.testbed_config["dut_info"].keys())]
-            # Skipping DUTs not required for this test
-            self.skip_dut_list = []
-            for index in xrange(0, self.dut_start_index):
-                self.skip_dut_list.append(index)
-            for index in xrange(self.dut_start_index + self.num_duts, len(self.full_dut_indexes)):
-                self.skip_dut_list.append(index)
-            fun_test.log("DUTs that will be skipped: {}".format(self.skip_dut_list))
-            self.available_dut_indexes = list(set(self.full_dut_indexes) - set(self.skip_dut_list))
-            self.available_dut_indexes = [int(i) for i in self.available_dut_indexes]
-            self.total_available_duts = len(self.available_dut_indexes)
-            fun_test.log("Total Available Duts: {}".format(self.total_available_duts))
-            self.topology_helper = TopologyHelper(spec=self.fs_hosts_map[self.testbed_type])
-            # Making topology helper to skip DUTs in this list to initialise
-            self.topology_helper.disable_duts(self.skip_dut_list)
-        # Pulling reserved DUTs and Hosts and test bed specific configuration if script is submitted with testbed-type
-        # suite-based
-        elif self.testbed_type == "suite-based":
-            self.topology_helper = TopologyHelper()
-            self.available_dut_indexes = self.topology_helper.get_available_duts().keys()
-            required_hosts_tmp = OrderedDict(self.topology_helper.get_available_hosts())
-            self.required_hosts = OrderedDict()
-            for index, host_name in enumerate(required_hosts_tmp):
-                if index < self.num_hosts:
-                    self.required_hosts[host_name] = required_hosts_tmp[host_name]
-                else:
-                    break
-            self.testbed_config = self.topology_helper.spec
-            self.total_available_duts = len(self.available_dut_indexes)
-
-        fun_test.test_assert(expression=self.num_duts <= self.total_available_duts,
-                             message="Testbed has enough DUTs")
-
-        for i in range(len(self.bootargs)):
-            self.bootargs[i] += " --mgmt"
-            if self.disable_wu_watchdog:
-                self.bootargs[i] += " --disable-wu-watchdog"
-
-        for dut_index in self.available_dut_indexes:
-            self.topology_helper.set_dut_parameters(dut_index=dut_index,
-                                                    f1_parameters={0: {"boot_args": self.bootargs[0]},
-                                                                   1: {"boot_args": self.bootargs[1]}})
-        self.topology = self.topology_helper.deploy()
+        topology_helper = TopologyHelper()
+        topology_helper.disable_duts(self.skip_dut_list)
+        topology_helper.set_dut_parameters(f1_parameters={0: {"boot_args": self.bootargs[0]},
+                                                          1: {"boot_args": self.bootargs[1]}})
+        self.topology = topology_helper.deploy()
         fun_test.test_assert(self.topology, "Topology deployed")
 
         # Datetime required for daily Dashboard data filter
@@ -165,42 +133,35 @@ class ECVolumeLevelScript(FunTestScript):
         fun_test.log("Data collection time: {}".format(self.db_log_time))
 
         # Retrieving all Hosts list and filtering required hosts and forming required object lists out of it
-        if self.testbed_type != "suite-based":
-            hosts = self.topology.get_hosts()
-            fun_test.log("Available hosts are: {}".format(hosts))
-            required_host_index = []
-            self.required_hosts = OrderedDict()
-            for i in xrange(self.host_start_index, self.host_start_index + self.num_hosts):
-                required_host_index.append(i)
-            fun_test.debug("Host index required for scripts: {}".format(required_host_index))
-            for j, host_name in enumerate(sorted(hosts)):
-                if j in required_host_index:
-                    self.required_hosts[host_name] = hosts[host_name]
+        hosts = self.topology.get_hosts()
+        fun_test.log("Available hosts are: {}".format(hosts))
+        required_host_index = []
+        required_hosts = OrderedDict()
+        for i in xrange(self.host_start_index, self.host_start_index + self.num_hosts):
+            required_host_index.append(i)
+        fun_test.debug("Host index required for scripts: {}".format(required_host_index))
+        for j, host_name in enumerate(sorted(hosts)):
+            if j in required_host_index:
+                required_hosts[host_name] = hosts[host_name]
+        fun_test.log("Hosts that will be used for current test: {}".format(required_hosts.keys()))
 
-        self.host_info = {}
         self.hosts_test_interfaces = {}
         self.host_handles = {}
         self.host_ips = []
         self.host_numa_cpus = {}
-        self.total_numa_cpus = {}
-        for host_name, host_obj in self.required_hosts.items():
-            if host_name not in self.host_info:
-                self.host_info[host_name] = {}
-                self.host_info[host_name]["ip"] = []
+        for host_name, host_obj in required_hosts.items():
             # Retrieving host ips
+            # test_interfaces = host.get_test_interfaces()
             if host_name not in self.hosts_test_interfaces:
                 self.hosts_test_interfaces[host_name] = []
             test_interface = host_obj.get_test_interface(index=0)
             self.hosts_test_interfaces[host_name].append(test_interface)
-            self.host_info[host_name]["test_interface"] = test_interface
             host_ip = self.hosts_test_interfaces[host_name][-1].ip.split('/')[0]
             self.host_ips.append(host_ip)
-            self.host_info[host_name]["ip"].append(host_ip)
             fun_test.log("Host-IP: {}".format(host_ip))
             # Retrieving host handles
             host_instance = host_obj.get_instance()
             self.host_handles[host_ip] = host_instance
-            self.host_info[host_name]["handle"] = host_instance
 
         # Rebooting all the hosts in non-blocking mode before the test and getting NUMA cpus
         for key in self.host_handles:
@@ -222,9 +183,10 @@ class ECVolumeLevelScript(FunTestScript):
         self.sc_obj = []
         self.f1_ips = []
         self.gateway_ips = []
-        for curr_index, dut_index in enumerate(self.available_dut_indexes):
-            self.fs_obj.append(self.topology.get_dut_instance(index=dut_index))
-            self.fs_spec.append(self.topology.get_dut(index=dut_index))
+        for i in xrange(self.dut_start_index, self.dut_start_index + self.num_duts):
+            curr_index = i - self.dut_start_index
+            self.fs_obj.append(self.topology.get_dut_instance(index=i))
+            self.fs_spec.append(self.topology.get_dut(index=i))
             self.come_obj.append(self.fs_obj[curr_index].get_come())
             self.f1_obj[curr_index] = []
             for j in xrange(self.num_f1_per_fs):
