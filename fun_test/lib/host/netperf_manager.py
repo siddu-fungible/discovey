6a4b20787df6ace9ac98b1d7707aabe32281baf3
George Liang <george.liang@fungible.com>
2019-08-07 14:38:22 -0700

    Do perf at test case level

--- fun_test/lib/host/netperf_manager.py
+++ fun_test/lib/host/netperf_manager.py
@@ -249,6 +249,7 @@ class NetperfManager:
                 cpu_list_server = sorted(arg_dict.get('cpu_list_server'))[::-1]  # reversed order
                 cpu_list_client = sorted(arg_dict.get('cpu_list_client'))[::-1]  # reversed order
                 fixed_netperf_port = arg_dict.get('fixed_netperf_port', False)
+                csi_perf_obj = arg_dicts.get('csi_perf_obj', None)
 
                 if test == 2:
                     num_processes = 1
@@ -288,9 +289,15 @@ class NetperfManager:
                 break
 
             if test == 3:  # +1 for latency under load
+                # Get perf for throughput test, no need to latency only test
+                if csi_perf_obj:
+                    csi_perf_obj.start(f1_index=0)
                 mp_task_obj.run(max_parallel_processes=(num_processes+1)*len(direction_list))
+                if csi_perf_obj:
+                    csi_perf_obj.stop(f1_index=0)
             else:
                 mp_task_obj.run(max_parallel_processes=num_processes*len(direction_list))
+
             rdict = {}
             for direction in direction_list:
                 if direction not in result:
