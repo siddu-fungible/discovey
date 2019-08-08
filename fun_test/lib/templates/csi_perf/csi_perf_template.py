dc750b19f9d3e7d7a022459eaf5c5684feeb810f
John Abraham <john.abraham@fungible.com>
2019-08-07 17:54:27 -0700

    support for messages

--- fun_test/lib/templates/csi_perf/csi_perf_template.py
+++ fun_test/lib/templates/csi_perf/csi_perf_template.py
@@ -10,7 +10,8 @@ import os
 # move funos-binary
 
 
-BASE_JOB_DIRECTORY = "/tmp/trace_jobs"
+TRACE_JOBS_DIRECTORY = "trace_jobs"
+BASE_JOB_DIRECTORY = "/home/localadmin/{}".format(TRACE_JOBS_DIRECTORY)
 TOOLS_DIRECTORY = "/home/localadmin"
 PERF_LISTENER = "perf_listener.py"
 PERF_LISTENER_PATH = TOOLS_DIRECTORY + "/" + PERF_LISTENER
@@ -42,7 +43,9 @@ class CsiPerfTemplate():
         self.fs = fs
         self.instance = 0
         self.tools_directory = TOOLS_DIRECTORY
+        self.base_job_directory = TOOLS_DIRECTORY + "/trace_jobs"
         self.setup_docker = setup_docker
+        self.prepare_complete = False
 
     def ensure_docker_images_exist(self):
         docker_images_output = self.perf_host.sudo_command("docker images")
@@ -63,12 +66,12 @@ class CsiPerfTemplate():
         """
         pass
         self.perf_host = self.get_perf_host()
-        files = self.perf_host.list_files(BASE_JOB_DIRECTORY + "/s_*")
+        files = self.perf_host.list_files(self.base_job_directory + "/s_*")
         if len(files) >= num_allowed_trace_job_directories:
             # remove the oldest directory
             fun_test.log("Removing oldest directory: {}".format(files[0]["filename"]))
             self.perf_host.remove_directory(files[0]["filename"])
-            files = self.perf_host.list_files(BASE_JOB_DIRECTORY + "/s_*")
+            files = self.perf_host.list_files(self.base_job_directory + "/s_*")
         return len(files) < num_allowed_trace_job_directories
 
     def get_perf_host(self):
@@ -83,18 +86,19 @@ class CsiPerfTemplate():
         if "csi_perf" in self.perf_host.extra_attributes:
             if "tools_directory" in self.perf_host.extra_attributes["csi_perf"]:
                 self.tools_directory = self.perf_host.extra_attributes["csi_perf"]["tools_directory"]
+                self.base_job_directory = self.tools_directory + "/" + TRACE_JOBS_DIRECTORY
 
         if self.setup_docker:
             fun_test.simple_assert(self.do_setup_docker(), "Docker setup")
         fun_test.simple_assert(self.position_files(), "Position tools")
-        fun_test.simple_assert(self.ensure_space_is_available(), "Ensure space exists on. Please clear trace files/directories from {}. max dirs: {}".format(BASE_JOB_DIRECTORY, MAX_TRACE_JOB_DIRECTORIES))
+        fun_test.simple_assert(self.ensure_space_is_available(), "Ensure space exists on. Please clear trace files/directories from {}. max dirs: {}".format(self.base_job_directory, MAX_TRACE_JOB_DIRECTORIES))
         fun_test.simple_assert(self.ensure_tools_exists(), "Ensure necessary tools exists")
         fun_test.simple_assert(self.ensure_docker_images_exist(), "Ensure docker images exist")
 
         s = "s_{}_{}_f1_{}".format(fun_test.get_suite_execution_id(), fun_test.get_test_case_execution_id(), f1_index)
         if prefix:
             s += "_{}".format(prefix)
-        s = "{}/{}".format(BASE_JOB_DIRECTORY, s)
+        s = "{}/{}".format(self.base_job_directory, s)
         if self.perf_host.check_file_directory_exists(s):
             if remove_old_job_directory:
                 self.perf_host.remove_directory_contents(s)
@@ -104,7 +108,7 @@ class CsiPerfTemplate():
         tftp_image_path = self.fs.get_tftp_image_path()
         self.move_f1_binary(tftp_image_path=tftp_image_path)
         fun_test.add_checkpoint("Perf host: {} Job directory: {}".format(self.perf_host, self.job_directory))
-
+        self.prepare_complete = True
         return True
 
     def _prepare_directory_structure(self, job_directory):
@@ -112,6 +116,7 @@ class CsiPerfTemplate():
         self.perf_host.command("mkdir -p {}/odp/trace_dumps".format(job_directory))
 
     def start(self, f1_index=0):
+        fun_test.simple_assert(self.prepare_complete, "Please call prepare() before calling start")
         fun_test.add_checkpoint("CSI perf before start")
         self.instance += 1
         # ensure perf listener is not running
@@ -139,6 +144,10 @@ class CsiPerfTemplate():
         uart_log_path = self.fs.get_uart_log_file(f1_index=f1_index, post_fix=self.instance)
         self.move_uart_log(uart_log_path=uart_log_path, f1_index=f1_index)
         fun_test.add_checkpoint("CSI perf after stop")
+        fun_test.report_message("CSI perf traces are at trace job directory: {}".format(self.base_job_directory))
+        fun_test.report_message("CSI perf host: {} username: {} password: {}".format(self.perf_host.host_ip,
+                                                                                     self.perf_host.ssh_username,
+                                                                                     self.perf_host.ssh_password))
 
     def move_trace_files(self, source_directory, job_directory):
         trace_files = self.perf_host.list_files("{}/trace_cluster*".format(source_directory))
