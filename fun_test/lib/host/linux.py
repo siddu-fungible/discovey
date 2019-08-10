9ac203dac7546dda43c5aee5e45595096c47e038
John Abraham <john.abraham@fungible.com>
2019-08-10 10:19:55 -0700

    send an empty command after kill

--- fun_test/lib/host/linux.py
+++ fun_test/lib/host/linux.py
@@ -803,6 +803,8 @@ class Linux(object, ToDictMixin):
                 m = reg.search(line)
                 if m:
                     files.append({"info": m.group(1), "filename": m.group(2)})
+            if "No such" in line:
+                break
         return files
 
     @fun_test.safe
