d513d72a90d20d11db717ffb5cb2af5546f2a4bd
Rohan Nair <rohan.nair@fungible.com>
2019-08-15 11:53:53 -0700

    adding files

--- fun_test/web/angular/qadashboard/src/app/test/test.component.ts
+++ fun_test/web/angular/qadashboard/src/app/test/test.component.ts
@@ -4,7 +4,10 @@ import {LoggerService} from "../services/logger/logger.service";
 import {CommonService} from "../services/common/common.service";
 import {RegressionService} from "../regression/regression.service";
 import {FormControl, FormGroup} from "@angular/forms";
-import { FormBuilder, Validators} from "@angular/forms";
+import {FormBuilder, Validators} from "@angular/forms";
+import {NgMultiSelectDropDownModule} from 'ng-multiselect-dropdown';
+import {UserService} from "../services/user/user.service";
+
 
 @Component({
   selector: 'app-test',
@@ -27,24 +30,57 @@ export class TestComponent implements OnInit {
   })
 
 
-  constructor(private apiService: ApiService, private logger: LoggerService,
+  constructor(private apiService: ApiService, private logger: LoggerService, private userService: UserService,
               private renderer: Renderer2, private commonService: CommonService, private regressionService: RegressionService,
               private fb: FormBuilder) {
   }
 
 
+  dropdownList = [];
+  selectedItems = [];
+  dropdownSettings = {};
+  userMap: any = null;
+  submitter_emails : any = [];
+  fetched : boolean = false;
+
+
+
   ngOnInit() {
-
-  }
-
-  loadApiData() {
-    this.registrationForm.setValue({
-      userName: 'Bob',
-      password: 'fun123',
-      confirmPassword: 'fun123'
+    this.userService.getUserMap().subscribe((response) => {
+      this.userMap = response;
+      for (let user of Object.keys(this.userMap)){
+        this.submitter_emails.push(user);
+      }
+      this.fetched = true;
+    }, error => {
+      this.logger.error("Unable to fetch usermap");
     });
+    this.dropdownList = [
+      {item_id: 1, item_text: 'Mumbai'},
+      {item_id: 2, item_text: 'Bangaluru'},
+      {item_id: 3, item_text: 'Pune'},
+      {item_id: 4, item_text: 'Navsari'},
+      {item_id: 5, item_text: 'New Delhi'}
+    ];
+
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
   }
 
+  onItemSelect(item: any) {
+    console.log(item);
+  }
+
+  onSelectAll(items: any) {
+    console.log(items);
+  }
 
 }
 
