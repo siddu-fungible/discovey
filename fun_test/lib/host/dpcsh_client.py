2b1107bbed046bde8cde23a7c72aca8df19985a7
John Abraham <john.abraham@fungible.com>
2019-08-19 13:08:02 -0700

    trying to track down an overlap bug

--- fun_test/lib/host/dpcsh_client.py
+++ fun_test/lib/host/dpcsh_client.py
@@ -60,6 +60,9 @@ class DpcshClient(object):
                     break
             else:
                 output += buffer
+        if fun_test.get_job_environment_variable("dry_run"):
+            if "\n" in output:
+                fun_test.log("slash n in output. Output: S: {} xxx E".format(output))
         return output
 
     def _connect(self):
