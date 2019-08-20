390a205c8886ae52c2dbbd9f28d573274f53c173
DivyaKrishnankutty <divya.krishnankutty@fungible.com>
2019-08-20 15:26:39 +0530

    adding storage sanity to alibaba fs_power_cycle_test.py

--- fun_test/scripts/networking/funcp/fs_power_cycle_test.py
+++ fun_test/scripts/networking/funcp/fs_power_cycle_test.py
@@ -10,6 +10,8 @@ from lib.host.storage_controller import StorageController
 from lib.system import utils
 from lib.topology.topology_helper import TopologyHelper
 import json
+from scripts.networking.funcp.ali_bmv_storage_sanity import *
+
 
 
 class SetupBringup(FunTestScript):
@@ -153,8 +155,89 @@ class TestHostPCIeLanes(FunTestCase):
         pass
 
 
+class LocalSSDTest(StorageConfiguration):
+    def describe(self):
+        self.set_test_details(id=3,
+                              summary="Run fio traffic on locally attached SSD",
+                              steps="""
+                                      1. Create BLT volume
+                                      2. Create PCI controller
+                                      3. Attach volume to the controller
+                                      4. Check if nvme device is present on host
+                                      5. Run fio write
+                                      6. Run fio read
+                                      """)
+
+    def setup(self):
+        config = "LocalSSDTest"
+        super(LocalSSDTest, self).setup(config)
+
+    def run(self):
+        self.vol_type = "PCI"
+        self.ctrl_type = "PCI"
+        self.vol_attach_type = "local"
+        self.storage_controller = StorageController(target_ip=self.come_ip, target_port=self.dpc_p0)
+        self.dpu = 0
+        super(LocalSSDTest, self).run()
+        server = self.storage_config['server']
+        i = 0
+        for host in server:
+            self.host = Linux(host_ip=server[i], ssh_username=self.uname, ssh_password=self.pwd)
+            self.host.command("sudo nvme list")
+            device = self.host.command("sudo nvme list | grep nvme | sed -n 1p | awk {'print $1'}").strip()
+            fun_test.test_assert(device, message="nvme device visible on host")
+            i +=1
+
+        self.storage_controller = StorageController(target_ip=self.come_ip, target_port=self.dpc_p1)
+        self.dpu = 1
+        super(LocalSSDTest, self).run()
+        server = self.storage_config['server_f1']
+        i = 0
+        for host in server:
+            self.host = Linux(host_ip=server[i], ssh_username=self.uname, ssh_password=self.pwd)
+            self.host.command("sudo nvme list")
+            device = self.host.command("sudo nvme list | grep nvme | sed -n 1p | awk {'print $1'}").strip()
+            fun_test.test_assert(device, message="nvme device visible on host")
+            i += 1
+
+        def runfio(arg1, device):
+            for rw_mode in self.mode:
+                fio_result = arg1.pcie_fio(filename=device, rw=rw_mode,
+                                           numjobs=self.num_jobs,
+                                           iodepth=self.iodepth,
+                                           name="fio_" + str(rw_mode),
+                                           runtime=360,
+                                           prio=0,
+                                           direct=1,
+                                           timeout=400)
+                arg1.disconnect()
+
+        threads_list = []
+        hosts = self.storage_config['io_servers']
+        print(hosts)
+        for servers in hosts:
+            self.host = Linux(host_ip=servers, ssh_username=self.uname, ssh_password=self.pwd)
+            device = self.host.command("sudo nvme list -o normal | awk -F ' ' '{print $1}' | grep -i nvme0").\
+                replace("\r", '')
+            device_list = device.replace("\n", ":").rstrip(":")
+            thread_id = fun_test.execute_thread_after(time_in_seconds=2, func=runfio,
+                                                      arg1=self.host, device=device_list)
+            fun_test.sleep("Threadzz started", 2)
+            threads_list.append(thread_id)
+
+        fun_test.sleep("Sleeping between thread join...", seconds=10)
+        for thread_id in threads_list:
+            fun_test.join_thread(fun_test_thread_id=thread_id, sleep_time=1)
+
+        # self.storage_controller.disconnect()
+
+    def cleanup(self):
+        pass
+
+
 if __name__ == '__main__':
     ts = SetupBringup()
     ts.add_test_case(BootF1())
     ts.add_test_case(TestHostPCIeLanes())
+    ts.add_test_case(LocalSSDTest())
     ts.run()
