299f9f68631aec31b54f73538b524b81dfe6d467
John Abraham <john.abraham@fungible.com>
2019-08-28 09:24:07 -0700

    add suite view

--- fun_test/web/angular/qadashboard/src/app/regression/regression.service.ts
+++ fun_test/web/angular/qadashboard/src/app/regression/regression.service.ts
@@ -165,7 +165,7 @@ getPrettyLocalizeTime(t) {
     }))
   }
 
-  tags(): Observable<string[]> {
+  tags() {
     return this.apiService.get('/regression/tags').pipe(switchMap(response => {
       let data = JSON.parse(response.data);
       let i = 1;
@@ -175,7 +175,7 @@ getPrettyLocalizeTime(t) {
       }
       return of(parsedTags);
     }), catchError(error => {
-      throw([error]);
+      throw(error);
     }))
   }
 
