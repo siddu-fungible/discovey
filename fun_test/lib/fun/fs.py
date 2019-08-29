1178f4abee857c858ea43921b607c8e30748dd16
John Abraham <john.abraham@fungible.com>
2019-08-29 13:37:05 -0700

    override autoboot

--- fun_test/lib/fun/fs.py
+++ fun_test/lib/fun/fs.py
@@ -1147,8 +1147,14 @@ class Fs(object, ToDictMixin):
                     if f1_index in self.f1_parameters:
                         if "boot_args" in self.f1_parameters[f1_index]:
                             boot_args = self.f1_parameters[f1_index]["boot_args"]
-                # if self.validate_u_boot_version:
-                #    fun_test.test_assert(self.bmc.validate_u_boot_version(f1_index=f1_index, minimum_date=self.MIN_U_BOOT_DATE), "Validate preamble")
+                fun_test.test_assert(self.get_bmc().setup_serial_proxy_connection(f1_index=f1_index, auto_boot=self.auto_boot),
+                                     "Setup nc serial proxy connection")
+                fs.get_fpga().reset_f1(f1_index=f1_index)
+
+                if self.validate_u_boot_version:
+                    fun_test.test_assert(self.bmc.validate_u_boot_version(f1_index=f1_index, minimum_date=self.MIN_U_BOOT_DATE), "Validate preamble")
+                preamble = self.get_bmc().get_preamble(f1_index=f1_index)
+
                 fun_test.test_assert(expression=self.bmc.u_boot_load_image(index=f1_index, tftp_image_path=self.tftp_image_path, boot_args=boot_args, gateway_ip=self.gateway_ip),
                                      message="U-Bootup f1: {} complete".format(f1_index),
                                      context=self.context)
