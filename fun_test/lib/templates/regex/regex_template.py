d7c8472298596fca9a19fadc12e8fa5b9ed5e673
p-indrani <indrani.p@fungible.com>
2019-09-10 13:59:00 +0530

    ..

--- fun_test/lib/templates/regex/regex_template.py
+++ fun_test/lib/templates/regex/regex_template.py
@@ -124,9 +124,6 @@ class RegexTemplate(Linux):
                 elif "target" in arg:
                     if arg[1] == "s1":
                         cmd = cmd + "--" + arg[0] + "=" + arg[1] + " "
-                elif "s" in arg:
-                    self.logger.log("setting result buffer size to 1500")
-                    cmd=cmd+"-"+ arg[0]+ " "+arg[1]+ " "
                 else:
                     cmd = cmd + "-" + arg[0] + " " + arg[1] + " "
             print ("graph path is ",graph_path)
