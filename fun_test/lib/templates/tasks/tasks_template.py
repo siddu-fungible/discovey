3169be26aecf0f481b1349ae7b845b24e0dc6f30
John Abraham <john.abraham@fungible.com>
2019-09-11 14:18:11 -0700

    improve ssh maintenance

--- fun_test/lib/templates/tasks/tasks_template.py
+++ fun_test/lib/templates/tasks/tasks_template.py
@@ -9,10 +9,28 @@ class TaskTemplate:
         output = subprocess.call(command, shell=shell)
         return output
 
-    def popen(self, command, args):
-        p = subprocess.Popen([command] + args, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    def popen(self, command, args, shell=None):
+        p = subprocess.Popen([command] + args, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=shell)
         output, err = p.communicate()
         return_code = p.returncode
+        return return_code, output, err
+
+    def piped_commands(self, commands):
+        previous_proc = None
+        for index, command in enumerate(commands):
+            stdout = subprocess.PIPE
+            stderr = subprocess.PIPE
+            stdin = subprocess.PIPE
+            if previous_proc:
+                stdin = previous_proc.stdout
+            proc = subprocess.Popen(command.split(), stdin=stdin, stdout=stdout, stderr=stderr)
+            previous_proc = proc
+
+        output = err = None
+        return_code = -1
+        if previous_proc:
+            output, err = previous_proc.communicate()
+            return_code = previous_proc.returncode
         return return_code, output, err
 
 if __name__ == "__main__":
