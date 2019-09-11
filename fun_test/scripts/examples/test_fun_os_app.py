718168fca38893f21788b2405ac9918d519e19e0
John Abraham <john.abraham@fungible.com>
2019-09-10 18:49:46 -0700

    append dpc-server to bootargs

--- fun_test/scripts/examples/test_fun_os_app.py
+++ fun_test/scripts/examples/test_fun_os_app.py
@@ -33,6 +33,8 @@ class MyScript(FunTestScript):
                                 boot_args += " --dpc-server"
             topology_helper = TopologyHelper()
             topology_helper.set_dut_parameters(dut_index=0, custom_boot_args=boot_args)
+            fun_test.shared_variables["topology_obj_helper"] = topology_obj_helper
+
 
     def cleanup(self):
         if "topology" in fun_test.shared_variables:
