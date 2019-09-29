005783241a3730548d18afed7b9aa8687aa1ef8a
John Abraham <john.abraham@fungible.com>
2019-09-28 23:21:32 -0700

    fix pool member type options

--- fun_test/web/fun_test/management/add_default_pool_member.py
+++ fun_test/web/fun_test/management/add_default_pool_member.py
@@ -8,11 +8,16 @@ for suite in all_suites:
     if suite.custom_test_bed_spec:
         if "asset_request" in suite.custom_test_bed_spec:
             asset_request = suite.custom_test_bed_spec["asset_request"]
-            print asset_request
 
             if "DUT" in asset_request:
                 dut = asset_request["DUT"]
                 if "num" in dut:
+                    print asset_request
+                    dut["pool_member_type_options"] = {1: {"num": dut["num"]}}
+                    del dut["num"]
+                    suite.save()
+                    """
                     if "pool_member_type" not in dut:
                         dut["pool_member_type"] = 0
                         suite.save()
+                    """
