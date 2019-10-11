3270f224ff6de246965a7e498316b6b7729cec57
Ash41291 <ashwin.s@fungible.com>
2019-10-11 12:32:08 -0700

    changed hw version for s1_compute

--- fun_test/scripts/system/s1_teramarks.py
+++ fun_test/scripts/system/s1_teramarks.py
@@ -29,7 +29,7 @@ class PalladiumTc(FunTestCase):
     hw_model = "S1_Compute"
     max_duration = 900
     release_build = "true"
-    hw_version = "rel_09152019"
+    hw_version = "rel_10012019"
     run_target = "protium_s"
     extra_emails = []
 
