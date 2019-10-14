95dd16fbccbefe208a85fe8e118ac50bdeb6404b
Ranganath Gowda <ranga.gowda@fungible.com>
2019-10-15 01:35:23 +0530

    added watt unit

--- fun_test/fun_global.py
+++ fun_test/fun_global.py
@@ -131,6 +131,10 @@ class PerfUnit:
     UNIT_KCPS = "Kcps"
     UNIT_GCPS = "Gcps"
 
+    UNIT_WATT = "W"
+    UNIT_KILOWATT = "kW"
+    UNIT_MILLIWATT = "mW"
+
 class FunPlatform:
     F1 = "F1"
     S1 = "S1"
