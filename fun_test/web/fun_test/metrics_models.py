40954151b6c05a0c5858c6cb83d39e90e400e1cb
Ash41291 <ashwin.s@fungible.com>
2019-10-29 13:57:30 -0700

    created a model for funondemand build time

--- fun_test/web/fun_test/metrics_models.py
+++ fun_test/web/fun_test/metrics_models.py
@@ -3088,3 +3088,25 @@ class RdsClientPerformance(FunModel):
         for key, value in self.__dict__.iteritems():
             s += "{}:{} ".format(key, value)
         return s
+
+
+class FunOnDemandBuildTimePerformance(FunModel):
+    interpolation_allowed = models.BooleanField(default=False)
+    interpolated = models.BooleanField(default=False)
+    status = models.CharField(max_length=30, verbose_name="Status", default=RESULTS["PASSED"])
+    input_date_time = models.DateTimeField(verbose_name="Date", default=datetime.now)
+    input_platform = models.TextField(default=FunPlatform.F1)
+    input_version = models.CharField(verbose_name="Version", max_length=50, default="")
+
+    input_job_name = models.TextField(default="")
+    input_boot_args = models.TextField(verbose_name="Boot args", default="")
+    input_build_properties = JSONField(default={}, null=True)
+    output_build_time = models.FloatField(verbose_name="Build Time", default=-1)
+
+    output_build_time_unit = models.TextField(default=PerfUnit.UNIT_SECS)
+
+    def __str__(self):
+        s = ""
+        for key, value in self.__dict__.iteritems():
+            s += "{}:{} ".format(key, value)
+        return s
