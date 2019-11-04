64f3aba0bc87866d5a7fe79e86e1fa435f45499c
manuks88 <manu.ks@fungible.com>
2019-11-04 19:44:18 +0530

    If test_bed is Fs45, restart docker service

--- fun_test/scripts/networking/funcp/rdma_perf.py
+++ fun_test/scripts/networking/funcp/rdma_perf.py
@@ -183,6 +183,8 @@ class BringupSetup(FunTestCase):
             come_obj.sudo_command("iptables -F")
             come_obj.sudo_command("ip6tables -F")
             come_obj.sudo_command("dmesg -c > /dev/null")
+            if "fs-45" in fs_name:
+                come_obj.command("/home/fun/mks/restart_docker_service.sh")
 
             fun_test.log("Getting host details")
             host_dict = {"f1_0": [], "f1_1": []}
