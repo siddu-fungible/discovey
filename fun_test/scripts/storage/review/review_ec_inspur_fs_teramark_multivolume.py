211dbdb1bc7025ec10339bd84a1ef7d71d90f54d
Alagarswamy Devaraj <alagarswamy.devaraj@fungible.com>
2019-11-28 16:26:54 -0800

    * Code re-org

--- fun_test/scripts/storage/review/review_ec_inspur_fs_teramark_multivolume.py
+++ fun_test/scripts/storage/review/review_ec_inspur_fs_teramark_multivolume.py
@@ -501,15 +501,15 @@ class ECVolumeLevelScript(FunTestScript):
                         fun_test.simple_assert(disalbe_init_fs1600(self.come_obj[0]), "init-fs1600 service is disabled")
                         init_fs1600_service_status = False
 
+                    # Stopping containers and unloading the drivers
+                    self.come_obj[0].command("sudo /opt/fungible/cclinux/cclinux_service.sh --stop")
+
                     # kill run_sc health_check and all containers
                     health_check_pid = self.come_obj[0].get_process_id_by_pattern("system_health_check.py")
                     if health_check_pid:
                         self.come_obj[0].kill_process(process_id=health_check_pid)
                     else:
                         fun_test.critical("system_health_check.py script is not running")
-
-                    # Stopping containers and unloading the drivers
-                    self.come_obj[0].command("sudo /opt/fungible/cclinux/cclinux_service.sh --stop")
 
                     # Bring-up the containers
                     for index in xrange(self.num_duts):
