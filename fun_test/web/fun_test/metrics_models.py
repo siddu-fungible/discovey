b49640fad6ed8a4cf42cd86e38be747afff98957
Ranganath Gowda <ranga.gowda@fungible.com>
2019-12-02 02:22:03 +0530

    rdma charts for 2 hosts and 4 hosts

--- fun_test/web/fun_test/metrics_models.py
+++ fun_test/web/fun_test/metrics_models.py
@@ -2736,6 +2736,7 @@ class AlibabaRdmaPerformance(models.Model):
     input_qp = models.IntegerField(verbose_name="QP", default=-1)
     input_fcp = models.BooleanField(default=False)
     input_mtu = models.IntegerField(verbose_name="MTU", default=-1)
+    input_hosts = models.IntegerField(verbose_name="HOSTS", default=-1)
     
     output_read_avg_latency = models.FloatField(verbose_name="read average latency (usec)", default=-1)
     output_write_avg_latency = models.FloatField(verbose_name="write average latency (usec)", default=-1)
