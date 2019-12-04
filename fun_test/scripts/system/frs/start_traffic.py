57a3b46ee5d19c42a1eef3e846f04d822187af4f
Ranganath Gowda <ranga.gowda@fungible.com>
2019-12-04 18:24:35 +0530

    removed comments

--- fun_test/scripts/system/frs/start_traffic.py
+++ fun_test/scripts/system/frs/start_traffic.py
@@ -482,8 +482,8 @@ class FrsTestCase(FunTestCase):
 
     def run(self):
         self.initial_stats()
-        # self.start_collecting_stats(heading="Before starting traffic")
-        # self.stop_collection_of_stats_for_count(count=1)
+        self.start_collecting_stats(heading="Before starting traffic")
+        self.stop_collection_of_stats_for_count(count=1)
         self.run_the_traffic()
         self.start_collecting_stats(heading="During traffic")
         fun_test.sleep("For the apps to stop", seconds=self.duration + 20)
