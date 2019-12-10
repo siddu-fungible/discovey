e9e9be9746e0ae0309860e1e9997803c573e9c37
John Abraham <john.abraham@fungible.com>
2019-12-10 13:25:04 -0800

    test bmc

--- fun_test/scripts/examples/test_bmc.py
+++ fun_test/scripts/examples/test_bmc.py
@@ -18,9 +18,15 @@ b = B(**blob)
 am = fun_test.get_asset_manager()
 all_fs_spec = am.get_all_fs_spec()
 
+results = []
+
 for fs_spec in all_fs_spec:
     bmc_spec = fs_spec.get("bmc")
-    bmc = Bmc(**bmc_spec)
+    bmc = Bmc(host_ip=bmc_spec["mgmt_ip"], ssh_username=bmc_spec["mgmt_ssh_username"], ssh_password=bmc_spec["mgmt_ssh_password"])
     output = bmc.command("dmesg | grep JGR")
     is_rev_2 = "REV2" in output
-    fun_test.log("FS: {} Rev2: ".format(fs_spec["name"], is_rev_2))
+    results.append((fs_spec["name"]), is_rev_2)
+
+
+for result in results:
+    fun_test.log("FS: {}, Is Rev2: {}".format(result[0], result[1]))
