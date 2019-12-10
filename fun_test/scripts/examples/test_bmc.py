c47de1dc9ecc2376c064b79d355594f234c88295
John Abraham <john.abraham@fungible.com>
2019-12-10 14:03:56 -0800

    add gpio check

--- fun_test/scripts/examples/test_bmc.py
+++ fun_test/scripts/examples/test_bmc.py
@@ -25,14 +25,24 @@ for fs_spec in all_fs_spec:
     bmc_spec = fs_spec.get("bmc")
     bmc = Bmc(host_ip=bmc_spec["mgmt_ip"], ssh_username=bmc_spec["mgmt_ssh_username"], ssh_password=bmc_spec["mgmt_ssh_password"])
     try:
-        output = bmc.command("dmesg | grep JGR")
+        bmc.command("cd /mnt/sdmmc0p1/scripts")
+
+        #output = bmc.command("dmesg | grep JGR")
+        output = bmc.command('gpiotool 8 --get-data | grep High >/dev/null 2>&1 && echo FS1600_REV2 || echo FS1600_REV1')
         is_rev_2 = "REV2" in output
-        results.append((fs_spec["name"], is_rev_2))
+        rev = "Unknown"
+        if "REV1.1" in output:
+            rev = "1.1"
+        elif "REV1" in output:
+            rev = "1"
+        elif "REV2" in output:
+            rev = "2"
+        results.append((fs_spec["name"], rev))
     except:
         errored_outs.append((fs_spec["name"]))
 
 for result in results:
-    fun_test.log("FS: {}, Is Rev2: {}".format(result[0], result[1]))
+    fun_test.log("FS: {}, Rev: {}".format(result[0], result[1]))
 
 fun_test.log("Following FS errored out")
 for errored_out in errored_outs:
