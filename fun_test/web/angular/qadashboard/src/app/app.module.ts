518041c8fc93c91b5505e9f71ea434dc3e7b7f0b
John Abraham <john.abraham@fungible.com>
2019-12-18 18:20:12 -0800

    add releases link

--- fun_test/web/angular/qadashboard/src/app/app.module.ts
+++ fun_test/web/angular/qadashboard/src/app/app.module.ts
@@ -107,6 +107,7 @@ import { ReleaseDetailComponent } from './regression/release-detail/release-deta
 import { FunStatsComponent } from './statistics/fun-stats/fun-stats.component';
 import { FunStatsTableComponent } from './statistics/fun-stats-table/fun-stats-table.component';
 import { ReleaseSummaryWidgetComponent } from './regression/release-summary-widget/release-summary-widget.component';
+import { ReleaseSummaryComponent } from './regression/release-summary/release-summary.component';
 
 
 @NgModule({
@@ -195,7 +196,8 @@ import { ReleaseSummaryWidgetComponent } from './regression/release-summary-widg
     ReleaseDetailComponent,
     FunStatsComponent,
     FunStatsTableComponent,
-    ReleaseSummaryWidgetComponent
+    ReleaseSummaryWidgetComponent,
+    ReleaseSummaryComponent
   ],
   imports: [
     BsDropdownModule,
