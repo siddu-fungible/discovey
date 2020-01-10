72bca9e1fb0412817bacf9d3b25d3d2e44d9143f
John Abraham <john.abraham@fungible.com>
2020-01-10 13:23:40 -0800

    health monitor disable with -9

--- fun_test/lib/fun/fs.py
+++ fun_test/lib/fun/fs.py
@@ -1325,6 +1325,8 @@ class ComE(Linux):
         health_monitor_processes = self.get_process_id_by_pattern(self.HEALTH_MONITOR, multiple=True)
         for health_monitor_process in health_monitor_processes:
             self.kill_process(process_id=health_monitor_process, signal=9)
+        health_monitor_processes = self.get_process_id_by_pattern(self.HEALTH_MONITOR, multiple=True)
+
 
     def pre_reboot_cleanup(self, skip_cc_cleanup=False, for_bundle_installation=True):
         fun_test.log("Cleaning up storage controller containers", context=self.context)
