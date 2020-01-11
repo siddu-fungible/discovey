01c968e0c206a41516498fe44fb68da59e379a32
John Abraham <john.abraham@fungible.com>
2020-01-11 12:34:19 -0800

    check expected containers

--- fun_test/lib/fun/fs.py
+++ fun_test/lib/fun/fs.py
@@ -953,8 +953,8 @@ class BootupWorker(Thread):
 
             if self.fs.get_revision() in ["2"] and self.fs.bundle_compatible:
                 come = fs.get_come()
-                if not self.fs.health():
-                    come.fs_reset(fast=True)
+                if not self.fs.health() or not come.ensure_expected_containers_running():
+                    come.fs_reset()
                     fs.come = None
                     fs.bmc = None
                 come = fs.get_come()
