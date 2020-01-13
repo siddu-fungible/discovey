8fd892934938fcd6348f0dae8cd3cc47a2af0422
John Abraham <john.abraham@fungible.com>
2020-01-13 14:58:26 -0800

    test rail set result

--- fun_test/lib/utilities/test_rail.py
+++ fun_test/lib/utilities/test_rail.py
@@ -6,15 +6,27 @@ import json
 
 
 class TestRailApi:
+    class Result:
+        PASSED = 1
+        BLOCKED = 2
+        UNTESTED = 3
+        RETEST = 4
+        FAILED = 5
+
     base_url = TESTRAIL_URL
 
-    def __init__(self, project_name):
+    def __init__(self, project_name, run_name):
         self.http_basic_auth = requests.auth.HTTPBasicAuth(username="john.abraham@fungible.com", password="fun123")
         project = self.get_project_by_name(name=project_name)
         self.project_id = project["id"]
+        self.run_name = run_name
 
     def get(self, url):
         response = requests.get(url, auth=self.http_basic_auth, headers={"Content-type": "application/json"})
+        return response.json()
+
+    def post(self, url, payload):
+        response = requests.post(url, payload, auth=self.http_basic_auth, headers={"Content-type": "application/json"})
         return response.json()
 
     def get_projects(self):
@@ -45,6 +57,58 @@ class TestRailApi:
             all_cases.extend(cases)
         return all_cases
 
+    def get_runs(self):
+        url = self.base_url + "?/api/v2/get_runs/{}".format(self.project_id)
+        return self.get(url)
+
+    def get_run(self, run_name):
+        result = None
+        runs = self.get_runs()
+        for run in runs:
+            if run_name == run["name"]:
+                result = run
+                break
+        return result
+
+    def get_run_id(self, run_name):
+        run = self.get_run(run_name=run_name)
+        result = None
+        if run:
+            result = run["id"]
+        return result
+
+    def get_tests(self, run_name):
+        run_id = self.get_run_id(run_name=run_name)
+        result = None
+        if run_id:
+            url = self.base_url + "?/api/v2/get_tests/{}".format(run_id)
+            result = self.get(url=url)
+        return result
+
+    def get_test_by_case_id(self, case_id):
+        tests = self.get_tests(run_name=self.run_name)
+        result = None
+        for test in tests:
+            if test["case_id"] == int(case_id.lstrip("C")):
+                result = test
+                break
+        return result
+
+    def set_result(self, case_id, result_to_set):
+        test = self.get_test_by_case_id(case_id=case_id)
+        if test:
+            test_id = test["id"]
+            url = self.base_url + "?/api/v2/add_result/{}".format(test_id)
+            payload = {"status_id": result_to_set}
+            response = self.post(url, json.dumps(payload))
+            print json.dumps(response, indent=4)
+
 if __name__ == "__main__":
-    tr = TestRailApi(project_name="Experimental")
-    print json.dumps(tr.get_cases(), indent=4)
+    tr = TestRailApi(project_name="Experimental", run_name="AARun")
+    # print json.dumps(tr.get_cases(), indent=4)
+    # print json.dumps(tr.get_runs(), indent=4)
+    # print json.dumps(tr.get_run(run_name="AARun"), indent=4)
+    # print json.dumps(tr.get_tests(run_name="AARun"), indent=4)
+    # print json.dumps(tr.get_test_by_case_id(case_id="C19879"), indent=4)
+
+    tr.set_result(case_id="C19879", result_to_set=TestRailApi.Result.FAILED)
