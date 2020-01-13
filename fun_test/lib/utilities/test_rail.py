25c9a3b2ebc745914ac35fcf1270b44659ff9ec2
John Abraham <john.abraham@fungible.com>
2020-01-13 13:33:54 -0800

    test rail utility

--- fun_test/lib/utilities/test_rail.py
+++ fun_test/lib/utilities/test_rail.py
@@ -1,17 +1,17 @@
+from lib.system.fun_test import fun_test
 from fun_settings import TESTRAIL_URL
 import requests
 import requests.auth
 import json
 
+
 class TestRailApi:
     base_url = TESTRAIL_URL
 
-    def __init__(self):
+    def __init__(self, project_name):
         self.http_basic_auth = requests.auth.HTTPBasicAuth(username="john.abraham@fungible.com", password="fun123")
-
-    def something(self):
-        pass
-
+        project = self.get_project_by_name(name=project_name)
+        self.project_id = project["id"]
 
     def get(self, url):
         response = requests.get(url, auth=self.http_basic_auth, headers={"Content-type": "application/json"})
@@ -22,6 +22,29 @@ class TestRailApi:
         projects = self.get(url=url)
         return projects
 
+    def get_project_by_name(self, name):
+        projects = self.get_projects()
+        result = None
+        for project in projects:
+            if name == project["name"]:
+                result = project
+                break
+        return result
+
+    def get_suites(self):
+        url = self.base_url + "?/api/v2/get_suites/{}".format(self.project_id)
+        return self.get(url=url)
+
+    def get_cases(self):
+        suites = self.get_suites()
+        suite_ids = [x["id"] for x in suites]
+        all_cases = []
+        for suite_id in suite_ids:
+            url = self.base_url + "?/api/v2/get_cases/{}&suite_id={}".format(self.project_id, suite_id)
+            cases = self.get(url=url)
+            all_cases.extend(cases)
+        return all_cases
 
 if __name__ == "__main__":
-    print json.dumps(TestRailApi().get_projects(), indent=4)
+    tr = TestRailApi(project_name="Experimental")
+    print json.dumps(tr.get_cases(), indent=4)
