3011a1041134fec24c09a24bdb9049e2a20a36c8
John Abraham <john.abraham@fungible.com>
2020-01-14 12:34:15 -0800

    add user

--- fun_test/web/fun_test/management/add_user.py
+++ fun_test/web/fun_test/management/add_user.py
@@ -1,7 +1,7 @@
 from web.fun_test.django_interactive import *
 from django.contrib.auth.models import User as AuthUser
 
-
+"""
 users_info = [("paritosh.joshi@fungible.com", "Paritosh", "Joshi"),
               ("arpit.arora@fungible.com", "Arpit", "Arora"),
               ("ashwini.nanjappa@fungible.com", "Ashwini", "Nanjappa"),
@@ -12,6 +12,189 @@ users_info = [("paritosh.joshi@fungible.com", "Paritosh", "Joshi"),
               ("kapil.suri@fungible.com", "Kapil", "Suri"),
               ("karthik.siddavaram@fungible.com", "Karthik", "Siddavaram")]
 
+"""
+users_info = [
+    [
+        "akshay.deshpande@fungible.com",
+        "Akshay",
+        "Deshpande"
+    ],
+    [
+        "ame.wongsa@fungible.com",
+        "Ame",
+        "Wongsa"
+    ],
+    [
+        "amy.wu@fungible.com",
+        "Amy",
+        "Wu"
+    ],
+    [
+        "bibek.regmi@fungible.com",
+        "Bibek",
+        "Regmi"
+    ],
+    [
+        "boris.altshul@fungible.com",
+        "Boris",
+        "Altshul"
+    ],
+    [
+        "chen.wan@fungible.com",
+        "Chen",
+        "Wan"
+    ],
+    [
+        "chester.knapp@fungible.com",
+        "Chester",
+        "Knapp"
+    ],
+    [
+        "chris.sherman@fungible.com",
+        "Chris",
+        "Sherman"
+    ],
+    [
+        "christina.janczak@fungible.com",
+        "Christina",
+        "Janczak"
+    ],
+    [
+        "denis.martens@fungible.com",
+        "Denis",
+        "Martens"
+    ],
+    [
+        "dimitrian.tkachenko@fungible.com",
+        "Dimitrian",
+        "Tkachenko"
+    ],
+    [
+        "eric.wuerschmidt@fungible.com",
+        "Eric",
+        "Wuerschmidt"
+    ],
+    [
+        "evgen.masiakin@fungible.com",
+        "Evgen",
+        "Masiakin"
+    ],
+    [
+        "getnat.ejigu@fungible.com",
+        "Getnat",
+        "Ejigu"
+    ],
+    [
+        "herman.wu@fungible.com",
+        "Herman",
+        "Wu"
+    ],
+    [
+        "in.hosok@fungible.com",
+        "In",
+        "HoSok"
+    ],
+    [
+        "ivy.chen@fungible.com",
+        "Ivy",
+        "Chen"
+    ],
+    [
+        "jaimin.shah@fungible.com",
+        "Jaimin",
+        "Shah"
+    ],
+    [
+        "jason.vanvalkenburgh@fungible.com",
+        "Jason",
+        "Van Valkenburgh"
+    ],
+    [
+        "kosha.parekh@fungible.com",
+        "Kosha",
+        "Parekh"
+    ],
+    [
+        "kostiantyn.kuzin@fungible.com",
+        "Kostiantyn",
+        "Kuzin"
+    ],
+    [
+        "lakshmi.nannapaneni@fungible.com",
+        "Lakshmi",
+        "Nannapaneni"
+    ],
+    [
+        "mayank.khanna@fungible.com",
+        "Mayank",
+        "Khanna"
+    ],
+    [
+        "michael.nachmias@fungible.com",
+        "Michael",
+        "Nachmias"
+    ],
+    [
+        "paolo.garcia@fungible.com",
+        "Paolo",
+        "Garcia"
+    ],
+    [
+        "pradeep.tomar@fungible.com",
+        "Pradeep",
+        "Tomar"
+    ],
+    [
+        "rachel.kotraba@fungible.com",
+        "Rachel",
+        "Kotraba"
+    ],
+    [
+        "roderick.kidd@fungible.com",
+        "Roderick",
+        "Kidd"
+    ],
+    [
+        "ryan.norwood@fungible.com",
+        "Ryan",
+        "Norwood"
+    ],
+    [
+        "saikiran.mankena@fungible.com",
+        "Saikiran",
+        "Mankena"
+    ],
+    [
+        "serban.jora@fungible.com",
+        "Serban",
+        "Jora"
+    ],
+    [
+        "shankha.banerjee@fungible.com",
+        "Shankha",
+        "Banerjee"
+    ],
+    [
+        "shelby.sweeney@fungible.com",
+        "Shelby",
+        "Sweeney"
+    ],
+    [
+        "tudor.popescu@fungible.com",
+        "Tudor",
+        "Popescu"
+    ],
+    [
+        "vedavyas.duggirala@fungible.com",
+        "Vedavyas",
+        "Duggirala"
+    ],
+    [
+        "venkat.jaligama@fungible.com",
+        "Venkat",
+        "Jaligama"
+    ]
+]
 
 for user_info in users_info:
 
