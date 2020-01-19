05e3cd88deee623797e4ad703e5db58c232d6781
Yajat Singh <yajat.singh@fungible.com>
2020-01-19 15:20:34 +0530

    Fungible Swagger API

---
+++ fun_test/lib/host/swagger_client/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.storage_api import StorageApi
+from swagger_client.api.storagepools_api import StoragepoolsApi
+from swagger_client.api.storagevolumes_api import StoragevolumesApi
+from swagger_client.api.system_api import SystemApi
+from swagger_client.api.topology_api import TopologyApi
+from swagger_client.api.topologydrive_api import TopologydriveApi
+from swagger_client.api.topologynode_api import TopologynodeApi
