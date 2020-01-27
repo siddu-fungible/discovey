8eb42f222fa5fae861fdfd7174281a278fde8774
Yajat Singh <yajat.singh@fungible.com>
2020-01-28 01:48:21 +0530

    New location for Swagger_client

---
+++ fun_test/stash/swagger_client/api/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.controller_api import ControllerApi
+from swagger_client.api.network_api import NetworkApi
+from swagger_client.api.storage_api import StorageApi
+from swagger_client.api.storagepools_api import StoragepoolsApi
+from swagger_client.api.storagevolumes_api import StoragevolumesApi
+from swagger_client.api.topology_api import TopologyApi
+from swagger_client.api.topologydrive_api import TopologydriveApi
+from swagger_client.api.topologynode_api import TopologynodeApi
