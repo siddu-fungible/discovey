7b2adfb7c78d347dac3bebd196754f71a178d208
Yajat Singh <yajat.singh@fungible.com>
2020-01-28 01:46:54 +0530

    Add new attach volume logic, and add traffic

--- fun_test/scripts/storage/review/sample_storage_api.py
+++ fun_test/scripts/storage/review/sample_storage_api.py
@@ -1,10 +1,10 @@
 from lib.system.fun_test import *
 from lib.system import utils
 from lib.host.linux import Linux
-from fungible_client.models.body_volume_intent_create import BodyVolumeIntentCreate
+from swagger_client.models.body_volume_intent_create import BodyVolumeIntentCreate
 from lib.topology.topology_helper import TopologyHelper
-from lib.templates.storage.review.storage_template import BltVolumeTemplate
-from fungible_client.models.volume_types import VolumeTypes
+from lib.templates.storage.review.storage_template import BltVolumeOperationsTemplate
+from swagger_client.models.volume_types import VolumeTypes
 
 
 class BringupSetup(FunTestScript):
@@ -19,13 +19,9 @@ class BringupSetup(FunTestScript):
 
     def setup(self):
         already_deployed = True
-
         topology_helper = TopologyHelper()
-
         self.topology = topology_helper.deploy(already_deployed=already_deployed)
-
         fun_test.test_assert(self.topology, "Topology deployed")
-
         fun_test.shared_variables["topology"] = self.topology
 
     def cleanup(self):
@@ -55,18 +51,15 @@ class RunStorageApiCommands(FunTestCase):
         capacity = 160027797094
         compression_effort = False
         encrypt = False
-        attach_volume_map = {}
-        # ask only for fs objects | fs_obj and host_obj variables consistency
         body_volume_intent_create = BodyVolumeIntentCreate(name=name, vol_type=vol_type, capacity=capacity,
                                                            compression_effort=compression_effort,
                                                            encrypt=encrypt, data_protection={})
-        # raise exception for deploy
-        self.storage_controller_template = BltVolumeTemplate(topology=self.topology)
+        self.storage_controller_template = BltVolumeOperationsTemplate(topology=self.topology)
         self.storage_controller_template.initialize()
 
         fs_obj_list = []
-        for index in self.topology.get_duts().keys():
-            fs_obj = self.topology.get_dut_instance(index=index)
+        for dut_index in self.topology.get_duts().keys():
+            fs_obj = self.topology.get_dut_instance(index=dut_index)
             fs_obj_list.append(fs_obj)
 
         vol_uuid_dict = self.storage_controller_template.create_volume(fs_obj_list=fs_obj_list,
@@ -76,19 +69,23 @@ class RunStorageApiCommands(FunTestCase):
         for fs_obj in vol_uuid_dict:
             for host_id in hosts:
                 host_obj = hosts[host_id]
-                attach_vol_res = self.storage_controller_template.attach_volume(host_obj=host_obj, fs_obj=fs_obj,
-                                                                                volume_uuid=vol_uuid_dict[fs_obj])
-
-                self.storage_controller_template.nvme_connect_from_host(host_obj=host_obj,
-                                                                        subsys_nqn=attach_vol_res.subsys_nqn,
-                                                                        host_nqn=attach_vol_res.host_nqn,
-                                                                        dataplane_ip=attach_vol_res.ip)
+                attach_vol_result = self.storage_controller_template.attach_volume(host_obj=host_obj, fs_obj=fs_obj,
+                                                                                   volume_uuid=vol_uuid_dict[fs_obj],
+                                                                                   raw_api_call=True)
+                fun_test.test_assert(expression=attach_vol_result, message="Attach Volume")
 
     def run(self):
-        pass
+        hosts = self.topology.get_hosts()
+        for host_id in hosts:
+            host_obj = hosts[host_id]
+            nvme_device_name = self.storage_controller_template.get_host_nvme_device(host_obj=host_obj)
+            traffic_result = self.storage_controller_template.traffic_from_host(host_obj=host_obj,
+                                                                                nvme_device_name=nvme_device_name)
+            fun_test.test_assert(expression=traffic_result, message="FIO traffic result")
 
     def cleanup(self):
-        self.storage_controller_template.cleanup()
+        # self.storage_controller_template.cleanup()
+        pass
 
 
 if __name__ == "__main__":
