2d0315904e785e738c78114d0a776dadf399a33e
Ravi Hulle <ravi.hulle@fungible.com>
2020-01-28 12:56:42 +0530

    * Ensuring both DPU IDs are fetch by get_dpu_ids() API call * Cosmetic: Added log for fetched DPU IDs * Changed default value for num_f1_per_fs to 2 in all scripts

--- fun_test/scripts/storage/storage_helper.py
+++ fun_test/scripts/storage/storage_helper.py
@@ -322,10 +322,11 @@ def single_fs_setup(obj):
         # Getting all the DUTs of the setup
         dpu_id_ready_timer = FunTimer(max_time=9 * 60)
         nodes = obj.sc_api.get_dpu_ids()
-        while not nodes and not dpu_id_ready_timer.is_expired():
+        while not nodes and (len(nodes) < obj.num_f1_per_fs) and not dpu_id_ready_timer.is_expired():
             nodes = obj.sc_api.get_dpu_ids()
             fun_test.sleep("Checking DPU IDs", seconds=20)
             fun_test.log("Remaining time: {}".format(dpu_id_ready_timer.remaining_time()))
+        fun_test.log("DPU nodes: {}".format(nodes))
         fun_test.test_assert(nodes, "Bundle Image boot: Getting UUIDs of all DUTs in the setup")
         for node_index, node in enumerate(nodes):
             if node_index >= obj.num_f1_per_fs:
