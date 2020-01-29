df430e0eec323bf65a6bfc137bf3a48d298dc3e7
Yajat Singh <yajat.singh@fungible.com>
2020-01-29 16:50:34 +0530

    Fix enable storage api function location

--- fun_test/scripts/storage/review/sample_storage_api.py
+++ fun_test/scripts/storage/review/sample_storage_api.py
@@ -1,11 +1,11 @@
 from lib.system.fun_test import *
+fun_test.enable_storage_api()
 from lib.system import utils
 from lib.host.linux import Linux
 from swagger_client.models.body_volume_intent_create import BodyVolumeIntentCreate
 from lib.topology.topology_helper import TopologyHelper
 from lib.templates.storage.review.storage_operations_template import BltVolumeOperationsTemplate
 from swagger_client.models.volume_types import VolumeTypes
-fun_test.enable_storage_api()
 
 
 class BringupSetup(FunTestScript):
