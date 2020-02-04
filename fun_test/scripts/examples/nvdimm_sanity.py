0eee52f219dafd85d16439f0013813dedcc1570a
Murtaza Arsiwala <murtaza.arsiwala@fungible.com>
2020-02-04 11:34:19 -0800

    Adding nvdimm demo bootargs to be able to run from the regression server

--- fun_test/scripts/examples/nvdimm_sanity.py
+++ fun_test/scripts/examples/nvdimm_sanity.py
@@ -41,7 +41,7 @@ class NvdimmTest1(FunTestCase):
         # fun_test.build_parameters["bundle_image_parameters"] = {"release_train": "rel_1_0a_aa", "build_number": -1}
         topology_helper = TopologyHelper()
         #topology_helper.set_dut_parameters(dut_index=0, custom_boot_args="app=load_mods --dpc-uart --dpc-server --csr-replay --all_100g")
-        topology_helper.set_dut_parameters(dut_index=0, custom_boot_args="app=load_mods,hw_hsu_test sku=SKU_FS1600R2_0 --serial --dpc-server --dpc-uart --mgmt --all_100g --fec syslog=6 nvdimm_arm_backup=1")
+        topology_helper.set_dut_parameters(dut_index=0, custom_boot_args="app=nvdimm_demo nvdimm_arm_backup=1 demo-mode=backup syslog=6 --test-exit-fast")
         topology = topology_helper.deploy()
         fun_test.test_assert(topology, "Topology deployed")
         fs = topology.get_dut_instance(index=0)
@@ -54,20 +54,23 @@ class NvdimmTest1(FunTestCase):
         #fun_test.shared_variables["fs"] = fs
 
 
-        #Get BMC handle and assert pin 208 which should assert the HW_INT_FAIL to MMIO
-        fun_test.log("get bmc handle ...")
-        bmc_handle = fs.get_bmc()
-        fun_test.log("low pulse on bmc pin 208 ...")
-        output = bmc_handle.command('gpiotool --set-data-low 208; sleep 1; gpiotool --set-data-high 208') #F1_0, for F1_1, use pin 209
-        fun_test.log("sleep for 300 seconds ...")
-        fun_test.sleep("Temporary Wait for backup complete", seconds=300) #TODO: Need to figure out how to poll on backup complete
+      #  #Get BMC handle and assert pin 208 which should assert the HW_INT_FAIL to MMIO
+      #  fun_test.log("get bmc handle ...")
+      #  bmc_handle = fs.get_bmc()
+      #  fun_test.log("low pulse on bmc pin 208 ...")
+      #  output = bmc_handle.command('gpiotool --set-data-low 208; sleep 1; gpiotool --set-data-high 208') #F1_0, for F1_1, use pin 209
+      #  fun_test.log("sleep for 300 seconds ...")
+      #  fun_test.sleep("Temporary Wait for backup complete", seconds=300) #TODO: Need to figure out how to poll on backup complete
 
-        fun_test.log("Rebooting FS ...")
-        topology_helper = TopologyHelper()
-        #topology_helper.set_dut_parameters(dut_index=0, custom_boot_args="app=load_mods --dpc-uart --dpc-server --csr-replay --all_100g")
-        topology_helper.set_dut_parameters(dut_index=0, custom_boot_args="app=load_mods,hw_hsu_test sku=SKU_FS1600R2_0 --serial --dpc-server --dpc-uart --mgmt --all_100g --fec syslog=6 nvdimm_arm_backup=1 nvdimm_restore=1")
-        topology = topology_helper.deploy()
+      #  fun_test.log("Rebooting FS ...")
+      #  topology_helper = TopologyHelper()
+      #  #topology_helper.set_dut_parameters(dut_index=0, custom_boot_args="app=load_mods --dpc-uart --dpc-server --csr-replay --all_100g")
+      #  topology_helper.set_dut_parameters(dut_index=0, custom_boot_args="app=load_mods,hw_hsu_test sku=SKU_FS1600R2_0 --serial --dpc-server --dpc-uart --mgmt --all_100g --fec syslog=6 nvdimm_arm_backup=1 nvdimm_restore=1")
+      #  topology = topology_helper.deploy()
 
+        ##John's recommendation w.r.t. Reboot -
+        #fs.reset()
+        #fs.re_initialize()
         fun_test.log("Test Done !!!")
 
         topology.cleanup()
