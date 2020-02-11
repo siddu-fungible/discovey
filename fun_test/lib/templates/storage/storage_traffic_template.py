61b310f649b21664b983ab4fad117f6491972bfa
Yajat Singh <yajat.singh@fungible.com>
2020-02-11 23:37:02 +0530

    Rename functions and remove assertions

--- fun_test/lib/templates/storage/storage_traffic_template.py
+++ fun_test/lib/templates/storage/storage_traffic_template.py
@@ -9,21 +9,25 @@ class StorageTrafficTemplate:
         self.storage_operations_template = storage_operations_template
         self.topology = self.storage_operations_template.topology
 
-    def basic_rw_traffic(self):
-        hosts = self.topology.get_available_host_instances()
-        for host_obj in hosts:
+    def basic_rw_traffic(self, host_obj):
+        host_obj_list = []
+        if not isinstance(host_obj, list):
+            host_obj_list.append(host_obj)
+        else:
+            host_obj_list = host_obj
+
+        for host_obj in host_obj_list:
             nvme_device_name = self.storage_operations_template.get_host_nvme_device(host_obj=host_obj)
-            traffic_result = self.traffic_from_host(host_obj=host_obj, filename="/dev/" + nvme_device_name)
+            traffic_result = self.fio_basic(host_obj=host_obj, filename="/dev/" + nvme_device_name)
             fun_test.test_assert(expression=traffic_result,
                                  message="Host : {} FIO traffic result".format(host_obj.name))
             fun_test.log(traffic_result)
 
-    def fio_integrity_check(self, host_linux_handle, filename, job_name="Fungible_nvmeof", numjobs=1, iodepth=1,
-                            runtime=600, bs="4k", ioengine="libaio", direct=1,
-                            time_based=False, norandommap=True, verify="md5", verify_fatal=1,
-                            offset="0kb", verify_state_save=1, verify_dump=1,
-                            verify_state_load=1, verify_integrity=False):
-
+    def fio_with_integrity_check(self, host_linux_handle, filename, job_name="Fungible_nvmeof", numjobs=1, iodepth=1,
+                                 runtime=600, bs="4k", ioengine="libaio", direct=1, time_based=False, norandommap=True,
+                                 verify="md5", verify_fatal=1,offset="0kb", verify_state_save=1, verify_dump=1,
+                                 verify_state_load=1, verify_integrity=False):
+        result = True
         if not verify_integrity:
             host_linux_handle.command("cd /tmp; rm -fr test_fio_with_integrity;"
                                       "mkdir test_fio_with_integrity; cd test_fio_with_integrity")
@@ -32,19 +36,20 @@ class StorageTrafficTemplate:
                                                timeout=1500, fill_device=1, do_verify=0,
                                                verify=verify, verify_fatal=verify_fatal, offset=offset,
                                                verify_state_save=verify_state_save, verify_dump=verify_dump)
-            fun_test.test_assert(expression=fio_result, message="Write FIO test")
+            result &= fio_result
 
-        host_linux_handle.command("cd ~/test_fio_with_integrity; ls")
+        host_linux_handle.command("cd /tmp/test_fio_with_integrity; ls")
         fio_result = host_linux_handle.fio(name=job_name, numjobs=numjobs, iodepth=iodepth, bs=bs, rw="read",
                                            filename=filename, ioengine=ioengine, direct=direct,
                                            timeout=1500, offset=offset, fill_device=1,
                                            verify=verify, do_verify=1, verify_fatal=verify_fatal,
                                            verify_state_load=verify_state_load, verify_dump=verify_dump)
-        fun_test.test_assert(expression=fio_result, message="Read FIO result")
+        result &= fio_result
+        return result
 
-    def traffic_from_host(self, host_obj, filename, job_name="Fungible_nvmeof", numjobs=1, iodepth=1,
-                          rw="readwrite", runtime=60, bs="4k", ioengine="libaio", direct=1,
-                          time_based=True, norandommap=True, verify=None, do_verify=None):
+    def fio_basic(self, host_obj, filename, job_name="Fungible_nvmeof", numjobs=1, iodepth=1, rw="readwrite",
+                  runtime=60, bs="4k", ioengine="libaio", direct=1, time_based=True, norandommap=True, verify=None,
+                  do_verify=None):
         fio_result = host_obj.fio(name=job_name, numjobs=numjobs, iodepth=iodepth, bs=bs, rw=rw,
                                   filename=filename, runtime=runtime, ioengine=ioengine, direct=direct,
                                   timeout=runtime+15, time_based=time_based, norandommap=norandommap,
