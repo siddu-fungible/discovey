aea1f80df13b9a9b48a1525a94f82c1179a66290
Nimesh Chaudhary <43816296+nimeshchaudhary@users.noreply.github.com>
2020-02-22 00:50:22 +0530

    Updated to check persistence check

--- fun_test/scripts/storage/review/create_delete_raw_volumes_tid_c15214.py
+++ fun_test/scripts/storage/review/create_delete_raw_volumes_tid_c15214.py
@@ -26,50 +26,60 @@ class BringupSetup(FunTestScript):
         self.topology = topology_helper.deploy(already_deployed=already_deployed)
         fun_test.test_assert(self.topology, "Topology deployed")
         fun_test.shared_variables["topology"] = self.topology
+        fun_test.shared_variables["already_deployed"] = already_deployed
 
     def cleanup(self):
         self.topology.cleanup()
 
 class CreateDeleteVolume(FunTestCase):
     topology = None
+    already_deployed = True
     storage_controller_template = None
     body_volume_intent_create = None
     fs_obj_list = []
+    capacity = []
+    vol_uuid_list = []
+    no_of_volumes = 0
+    vol_type = VolumeTypes().LOCAL_THIN
+    compression_effort = False
+    encrypt = False
+    come_handle = None
 
     def describe(self):
         self.set_test_details(id=1,
                               summary="Create Volume and Delete Volume",
                               steps='''
                               1. Make sure API server is up and running
-                              2. Create a Volume using API Call
-                              3. Delete the Volume Using API call
-                              4. Clean up 
+                              2. Create a Volumes using API Call
+                              3. Check weather volumes are created or not
+                              4. Delete the Volumes Using API call
+                              5. Check weather volumes are deleted or not
+                              6. Clean up 
                               ''')
 
     def setup(self):
         self.topology = fun_test.shared_variables["topology"]
+        self.already_deployed = fun_test.shared_variables["already_deployed"]
         for dut_index in self.topology.get_available_duts().keys():
             fs_obj = self.topology.get_dut_instance(index=dut_index)
             self.fs_obj_list.append(fs_obj)
+            self.come_handle = fs_obj.get_come()
 
     def create_volumes(self):
-        capacity = []
         total_capacity = 0
         remaining_capacity = 0
         max_no_of_volumes = 0
-        no_of_volumes = 0
         min_volume_capacity = 1073741824
         max_volume_capacity = 0
-        vol_type = VolumeTypes().LOCAL_THIN
-        compression_effort = False
-        encrypt = False
         for dut_index in self.topology.get_available_duts().keys():
             fs_obj = self.topology.get_dut_instance(index=dut_index)
             storage_controller = fs_obj.get_storage_controller()
             raw_sc_api = StorageControllerApi(api_server_ip=storage_controller.target_ip)
             response = raw_sc_api.get_pools()
-            pool_uuid = str(response["data"].keys()[0])
-            total_capacity = total_capacity + response["data"][pool_uuid]["capacity"]
+            pool_uuid = str(response['data'].keys()[0])
+            loc_capacity = str(response['data'][pool_uuid]['capacity'])
+            total_capacity = total_capacity + int(loc_capacity)
+            print("total_capacity = ", total_capacity)
             max_volume_capacity = find_min_drive_capacity(storage_controller,30)
 
         max_no_of_volumes = total_capacity/min_volume_capacity
@@ -77,29 +87,33 @@ class CreateDeleteVolume(FunTestCase):
             random_capacity = random.randint(min_volume_capacity,max_volume_capacity)
         else:
             random_capacity = total_capacity
+        random_capacity = random_capacity - (random_capacity%4096)
         remaining_capacity = total_capacity - random_capacity
         for volume in range(max_no_of_volumes):
-            capacity.append(random_capacity)
-            no_of_volumes = no_of_volumes + 1
+            self.capacity.append(random_capacity)
+            self.no_of_volumes = self.no_of_volumes + 1
             if remaining_capacity == 0:
+                print("no_of_volumes = ", self.no_of_volumes)
                 break
             else:
                 if remaining_capacity-min_volume_capacity >= max_volume_capacity:
                     random_capacity = random.randint(min_volume_capacity,max_volume_capacity)
                 else:
                     random_capacity = remaining_capacity
+                random_capacity = random_capacity - (random_capacity % 4096)
                 remaining_capacity = remaining_capacity - random_capacity
 
         self.storage_controller_template = BltVolumeOperationsTemplate(topology=self.topology)
-        self.storage_controller_template.initialize(already_deployed=False)
-        for volume in range(no_of_volumes):
+        self.storage_controller_template.initialize(already_deployed=self.already_deployed)
+        for volume in range(self.no_of_volumes):
             name = "blt_vol_" + str(volume + 1)
-            self.body_volume_intent_create = BodyVolumeIntentCreate(name=name, vol_type=vol_type, capacity=capacity[volume],
-                                                                    compression_effort=compression_effort,
-                                                                    encrypt=encrypt, data_protection={})
-            vol_uuid_list = self.storage_controller_template.create_volume(fs_obj=self.fs_obj_list,
-                                                                           body_volume_intent_create=self.body_volume_intent_create)
-            fun_test.test_assert(expression=vol_uuid_list, message="Create Volume Successful")
+            self.body_volume_intent_create = BodyVolumeIntentCreate(name=name, vol_type=self.vol_type, capacity=self.capacity[volume],
+                                                                    compression_effort=self.compression_effort,
+                                                                    encrypt=self.encrypt, data_protection={})
+            vol_uuid = self.storage_controller_template.create_volume(fs_obj=self.fs_obj_list,
+                                                                      body_volume_intent_create=self.body_volume_intent_create)
+            fun_test.test_assert(expression=vol_uuid, message="Create Volume Successful")
+            self.vol_uuid_list.append(vol_uuid[0])
 
     def delete_volumes(self):
         for dut_index in self.topology.get_available_duts().keys():
@@ -112,12 +126,44 @@ class CreateDeleteVolume(FunTestCase):
                 delete_volume = storage_controller.storage_api.delete_volume(volume_uuid=volume)
                 fun_test.test_assert(expression=delete_volume.status, message="Delete Volume {}".format(volume))
 
+    def volumes_persistent_check(self):
+        for dut_index in self.topology.get_available_duts().keys():
+            fs_obj = self.topology.get_dut_instance(index=dut_index)
+            storage_controller = fs_obj.get_storage_controller()
+            raw_sc_api = StorageControllerApi(api_server_ip=storage_controller.target_ip)
+            for volume_num in range(self.no_of_volumes):
+                print("volume_num = ", volume_num)
+                print("volume_uuid = ", self.vol_uuid_list[volume_num])
+                print("come_handle = ", self.come_handle)
+                print("capacity = ", self.capacity[volume_num])
+                print("vol_type = ", self.vol_type)
+                print("encrypt = ", self.encrypt)
+                vol_db_status = raw_sc_api.is_raw_vol_in_db(vol_uuid=self.vol_uuid_list[volume_num],come_handle=self.come_handle,
+                                                            capacity=self.capacity[volume_num],stripe_count=0,
+                                                            vol_type=self.vol_type,encrypt=self.encrypt)
+                fun_test.test_assert(expression=vol_db_status["status"], message="Volume Persistent Check {}".format(vol_db_status))
+
+    def volumes_deletion_check(self):
+        for dut_index in self.topology.get_available_duts().keys():
+            fs_obj = self.topology.get_dut_instance(index=dut_index)
+            storage_controller = fs_obj.get_storage_controller()
+            raw_sc_api = StorageControllerApi(api_server_ip=storage_controller.target_ip)
+            for volume_uuid in self.vol_uuid_list:
+                print("volume_uuid = ", volume_uuid)
+                print("come_handle = ", self.come_handle)
+                vol_db_status = raw_sc_api.is_delete_in_db(come_handle=self.come_handle,vol_uuid=volume_uuid)
+                fun_test.test_assert( expression=vol_db_status["status"], message="Volume Deletion Check {}".format(vol_db_status))
+
     def run(self):
-        self.create_volumes()
         self.delete_volumes()
+        self.create_volumes()
+        self.volumes_persistent_check()
+        self.delete_volumes()
+        self.volumes_deletion_check()
 
     def cleanup(self):
-        self.storage_controller_template.cleanup()
+        pass
+        #self.storage_controller_template.cleanup()
 
 
 if __name__ == "__main__":
