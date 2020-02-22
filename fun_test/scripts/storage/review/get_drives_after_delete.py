914b535709ea25406808e95e5f07906c738602f9
Yajat Singh <yajat.singh@fungible.com>
2020-02-21 19:15:33 -0800

    extract drive uuids from topology to compare

--- fun_test/scripts/storage/review/get_drives_after_delete.py
+++ fun_test/scripts/storage/review/get_drives_after_delete.py
@@ -31,7 +31,7 @@ class BootupSetup(FunTestScript):
 class RunStorageApiCommands(FunTestCase):
     topology = None
     storage_controller_template = None
-    topology_result = None
+    drives = []
 
     def describe(self):
         self.set_test_details(id=1,
@@ -51,18 +51,32 @@ class RunStorageApiCommands(FunTestCase):
                        for dut_index in self.topology.get_available_duts().keys()]
         storage_controller = fs_obj_list[0].get_storage_controller()
         self.storage_controller_template.get_health(fs_obj=fs_obj_list[0])
-        self.topology_result = storage_controller.topology_api.get_hierarchical_topology()
-        fun_test.log(self.topology_result)
+        topology_result = storage_controller.topology_api.get_hierarchical_topology()
+        fun_test.log(topology_result)
+        for node in topology_result.data:
+            for dpu in topology_result.data[node].dpus:
+                for drive_info in dpu.drives:
+                    self.drives.append(drive_info.uuid)
+        self.drives.sort()
+        fun_test.log(self.drives)
         self.storage_controller_template.initialize(already_deployed=False)
 
     def run(self):
         fs_obj_list = [self.topology.get_dut_instance(index=dut_index)
                        for dut_index in self.topology.get_available_duts().keys()]
         storage_controller = fs_obj_list[0].get_storage_controller()
-        topology_result2 = storage_controller.topology_api.get_hierarchical_topology()
-        fun_test.log(topology_result2)
-        fun_test.test_assert(topology_result2 != self.topology_result,
-                             message="Comparing topology details after drive format")
+        topology_result = storage_controller.topology_api.get_hierarchical_topology()
+        fun_test.log(topology_result)
+        drives2 = []
+        for node in topology_result.data:
+            for dpu in topology_result.data[node].dpus:
+                for drive_info in dpu.drives:
+                    drives2.append(drive_info.uuid)
+        drives2.sort()
+        fun_test.log("Drives before format: {}".format(self.drives))
+        fun_test.log("Drives after format: {}".format(drives2))
+        fun_test.test_assert(self.drives != drives2,
+                             message="Comparing drive UUIDs after drive format")
 
     def cleanup(self):
         self.storage_controller_template.cleanup(fun_test.is_current_test_case_failed())
