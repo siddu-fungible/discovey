610ea3ab7b6702942a548b14a2643b1e57ca436b
JitendraLulla <jitendra.lulla@fungible.com>
2020-02-23 01:27:30 +0530

    wip

--- fun_test/scripts/storage/review/blt_stress_patterns.py
+++ fun_test/scripts/storage/review/blt_stress_patterns.py
@@ -22,6 +22,37 @@ from lib.system import utils
 '''
 
 
+def create_volume(fs_obj, body_volume_intent_create, name, sfx):
+    """
+    Create a volume for the fs_obj
+    :param fs_obj: Single fs object
+    :param body_volume_intent_create: object of class BodyVolumeIntentCreate with volume details
+    :return: single vol_uuid
+    """
+    body_volume_intent_create.name = name + str(sfx)
+    result = ""
+    storage_controller = fs_obj.get_storage_controller()
+    try:
+        create_vol_result = storage_controller.storage_api.create_volume(body_volume_intent_create)
+        vol_uuid = create_vol_result.data.uuid
+        result = vol_uuid
+    except Exception as e:
+        fun_test.critical("Exception when creating volume on fs %s: %s\n" % (fs_obj, e))
+    return result
+
+
+def detach(fs_obj, port):
+    storage_controller = fs_obj.get_storage_controller()
+    detach_vol_result = storage_controller.storage_api.delete_port(port_uuid=port)
+    return detach_vol_result
+
+
+def delete(fs_obj, uuid):
+    storage_controller = fs_obj.get_storage_controller()
+    delete_vol_result = storage_controller.storage_api.delete_volume(volume_uuid=uuid)
+    return delete_vol_result
+
+
 class BootupSetup(FunTestScript):
     topology = None
     testbed_type = fun_test.get_job_environment_variable("test_bed_type")
@@ -61,14 +92,14 @@ class BootupSetup(FunTestScript):
         self.topology.cleanup()
 
 
-class RunStorageApiCommands(FunTestCase):
+class CADtADt(FunTestCase):
     topology = None
     storage_controller_template = None
     attach_result = None
 
     def describe(self):
         self.set_test_details(id=1,
-                              summary="Create Volume and run IO from host",
+                              summary="CADtADt",
                               steps='''
                               1. Make sure API server is up and running
                               2. Create a Volume using API Call
@@ -77,22 +108,73 @@ class RunStorageApiCommands(FunTestCase):
                               ''')
 
     def setup(self):
+        testcase = self.__class__.__name__
+        testcase_file = fun_test.get_script_name_without_ext() + ".json"
+        fun_test.log("json file being used: {}".format(testcase_file))
+        testcase_dict = utils.parse_file_to_json(testcase_file)
+        parsing = True
 
+        if testcase not in testcase_dict or not testcase_dict[testcase]:
+            parsing = False
+            fun_test.critical("Input is not available for the current testcase {} in {} file".
+                              format(testcase, testcase_file))
+            fun_test.test_assert(parsing, "Parsing json file for this {} testcase".format(testcase))
+
+        for k, v in testcase_dict[testcase].iteritems():
+            setattr(self, k, v)
+
+        self.name = "blt_VOL"
+        self.vol_type = VolumeTypes().LOCAL_THIN
+        compression_effort = False
         self.topology = fun_test.shared_variables["topology"]
-        self.assign_job_sepcifics()
+
+        job_inputs = fun_test.get_job_inputs()
+        if not job_inputs:
+            job_inputs = {}
+
+        if "loop_count" in job_inputs:
+            self.loop_count = job_inputs["loop_count"]
+
+        if "pattern" in job_inputs:
+            self.pattern = job_inputs["pattern"]
+
+        if "capacity" in job_inputs:
+            self.capacity = job_inputs["capacity"]
+
+        if "encrypt" in job_inputs:
+            self.encrypt = job_inputs["encrypt"]
+
+        if "volume_count" in job_inputs:
+            self.volume_count = job_inputs["volume_count"]
+
+        if "dpu_count" in job_inputs:
+            self.dpu_count = job_inputs["dpu_count"]
+
+        if self.dpu_count == 2:
+            dpu_indexes = [0, 1]
+        else:
+            dpu_indexes = [0]
+
+        if self.encrypt == "Y":
+            encrypt = True
+        else:
+            encrypt = False
+
         self.already_deployed = fun_test.shared_variables["already_deployed"]
         self.format_drive = fun_test.shared_variables["format_drive"]
 
-        self.assign_volume_specifics()
+        print "capacity:", self.capacity, " loop:", self.loop_count, " pattern:", self.pattern
 
         body_volume_intent_create = BodyVolumeIntentCreate(name=self.name, vol_type=self.vol_type,
                                                            capacity=self.capacity,
-                                                           compression_effort=self.compression_effort,
-                                                           encrypt=self.encrypt, data_protection={})
+                                                           compression_effort=compression_effort,
+                                                           encrypt=encrypt, data_protection={})
         body_volume_intent_create.vol_type = self.vol_type
-        self.storage_controller_template = BltVolumeOperationsTemplate(topology=self.topology)
+        self.storage_controller_template = BltVolumeOperationsTemplate(topology=self.topology,
+                                                                       api_logging_level=logging.ERROR)
         self.storage_controller_template.initialize(already_deployed=self.already_deployed,
-                                                    format_drives=self.format_drive)
+                                                    format_drives=self.format_drive,
+                                                    dpu_indexes=dpu_indexes)
 
         fs_obj_list = []
         for dut_index in self.topology.get_available_duts().keys():
@@ -101,8 +183,9 @@ class RunStorageApiCommands(FunTestCase):
 
         sfx = 0
         fs_obj = fs_obj_list[0]
-        vol_uuid = self.\
-            create_volume(fs_obj=fs_obj, body_volume_intent_create=body_volume_intent_create, sfx=str(sfx))
+        vol_uuid = create_volume(fs_obj=fs_obj, body_volume_intent_create=body_volume_intent_create,
+                                 name=self.name, sfx=str(sfx))
+
         print "vol_uuid:", vol_uuid
         fun_test.test_assert(expression=vol_uuid, message="Create Volume Successful")
         hosts = self.topology.get_available_host_instances()
@@ -118,11 +201,11 @@ class RunStorageApiCommands(FunTestCase):
             # Lets wait for some time before we detach
             fun_test.sleep("Lets wait for some time before we detach..", seconds=5)
             port = attach_vol_result["data"]["uuid"]
-            detach_vol_result = self.detach(fs_obj, port)
+            detach_vol_result = detach(fs_obj, port)
             print "detach_vol_result:", detach_vol_result
-            #fun_test.test_assert(expression=detach_vol_result["status"], message="Detach Volume Successful")
+            fun_test.test_assert(expression=True, message="Detach Volume Successful")
 
-        delete_vol_result = self.delete(fs_obj, vol_uuid)
+        delete_vol_result = delete(fs_obj, vol_uuid)
         print "delete_vol_result:", delete_vol_result
         fun_test.test_assert(expression=delete_vol_result, message="Delete Volume Successful")
 
@@ -144,39 +227,29 @@ class RunStorageApiCommands(FunTestCase):
     def cleanup(self):
         self.storage_controller_template.cleanup(fun_test.is_current_test_case_failed())
 
-    def create_volume(self, fs_obj, body_volume_intent_create, sfx):
-        """
-        Create a volume for the fs_obj
-        :param fs_obj: Single fs object
-        :param body_volume_intent_create: object of class BodyVolumeIntentCreate with volume details
-        :return: single vol_uuid
-        """
-        body_volume_intent_create.name = body_volume_intent_create.name + str(sfx)
-        result = ""
-        storage_controller = fs_obj.get_storage_controller()
-        try:
-            create_vol_result = storage_controller.storage_api.create_volume(body_volume_intent_create)
-            vol_uuid = create_vol_result.data.uuid
-            result = vol_uuid
-        except Exception as e:
-            fun_test.critical("Exception when creating volume on fs %s: %s\n" % (fs_obj, e))
-        return result
 
-    def detach(self, fs_obj, port):
-        storage_controller = fs_obj.get_storage_controller()
-        detach_vol_result = storage_controller.storage_api.delete_port(port_uuid=port)
-        return detach_vol_result
+class CCDD(FunTestCase):
+    topology = None
+    storage_controller_template = None
+    attach_result = None
 
-    def delete(self, fs_obj, uuid):
-        storage_controller = fs_obj.get_storage_controller()
-        delete_vol_result = storage_controller.storage_api.delete_volume(volume_uuid=uuid)
-        return delete_vol_result
+    def describe(self):
+        self.set_test_details(id=1,
+                              summary="CCDD",
+                              steps='''
+                              1. Make sure API server is up and running
+                              2. Create a Volume using API Call
+                              3. Attach Volume to a remote host
+                              4. Run FIO from host
+                              '''
+                              )
 
-    def assign_volume_specifics(self):
+    def setup(self):
         testcase = self.__class__.__name__
         testcase_file = fun_test.get_script_name_without_ext() + ".json"
         fun_test.log("json file being used: {}".format(testcase_file))
         testcase_dict = utils.parse_file_to_json(testcase_file)
+        parsing = True
 
         if testcase not in testcase_dict or not testcase_dict[testcase]:
             parsing = False
@@ -189,11 +262,8 @@ class RunStorageApiCommands(FunTestCase):
 
         self.name = "blt_VOL"
         self.vol_type = VolumeTypes().LOCAL_THIN
-        self.capacity = 17179869184
-        self.compression_effort = False
-        self.encrypt = False
-
-    def assign_job_sepcifics(self):
+        compression_effort = False
+        self.topology = fun_test.shared_variables["topology"]
 
         job_inputs = fun_test.get_job_inputs()
         if not job_inputs:
@@ -201,39 +271,49 @@ class RunStorageApiCommands(FunTestCase):
 
         if "loop_count" in job_inputs:
             self.loop_count = job_inputs["loop_count"]
-        else:
-            self.loop_count = 2
 
         if "pattern" in job_inputs:
             self.pattern = job_inputs["pattern"]
+
+        if "capacity" in job_inputs:
+            self.capacity = job_inputs["capacity"]
+
+        if "encrypt" in job_inputs:
+            self.encrypt = job_inputs["encrypt"]
+
+        if "volume_count" in job_inputs:
+            self.volume_count = job_inputs["volume_count"]
+
+        if "dpu_count" in job_inputs:
+            self.dpu_count = job_inputs["dpu_count"]
+
+        if self.dpu_count == 2:
+            dpu_indexes = [0, 1]
         else:
-            self.pattern = "CADtADt"
+            dpu_indexes = [0]
 
+        if self.encrypt == "Y":
+            encrypt = True
+        else:
+            encrypt = False
 
-class CCDD(RunStorageApiCommands):
-    def describe(self):
-        self.set_test_details(id=1,
-                              summary="CCDD Ceate 1000, Delete 1000 in loop",
-                              steps='''
-                              ''')
+        #self.already_deployed = fun_test.shared_variables["already_deployed"]
+        #self.format_drive = fun_test.shared_variables["format_drive"]
+        self.already_deployed = True
+        self.format_drive = False
 
-    def setup(self):
-        # super(CADtADtWithEnc, self).setup()
-        self.topology = fun_test.shared_variables["topology"]
-        self.assign_job_sepcifics()
-        self.already_deployed = fun_test.shared_variables["already_deployed"]
-        self.format_drive = fun_test.shared_variables["format_drive"]
-
-        super(CCDD, self).assign_volume_specifics()
+        print "capacity:", self.capacity, " loop:", self.loop_count, " pattern:", self.pattern
 
         body_volume_intent_create = BodyVolumeIntentCreate(name=self.name, vol_type=self.vol_type,
                                                            capacity=self.capacity,
-                                                           compression_effort=self.compression_effort,
-                                                           encrypt=self.encrypt, data_protection={})
+                                                           compression_effort=compression_effort,
+                                                           encrypt=encrypt, data_protection={})
         body_volume_intent_create.vol_type = self.vol_type
-        self.storage_controller_template = BltVolumeOperationsTemplate(topology=self.topology)
+        self.storage_controller_template = BltVolumeOperationsTemplate(topology=self.topology,
+                                                                       api_logging_level=logging.ERROR)
         self.storage_controller_template.initialize(already_deployed=self.already_deployed,
-                                                    format_drives=self.format_drive)
+                                                    format_drives=self.format_drive,
+                                                    dpu_indexes=dpu_indexes)
         fs_obj_list = []
         for dut_index in self.topology.get_available_duts().keys():
             fs_obj = self.topology.get_dut_instance(index=dut_index)
@@ -243,14 +323,14 @@ class CCDD(RunStorageApiCommands):
         fs_obj = fs_obj_list[0]
         volumes = []
         for counter in range(self.loop_count):
-            vol_uuid = self. \
-                create_volume(fs_obj=fs_obj, body_volume_intent_create=body_volume_intent_create, sfx=str(counter))
+            vol_uuid = create_volume(fs_obj=fs_obj, body_volume_intent_create=body_volume_intent_create,
+                                     name=self.name, sfx=str(counter))
             print "vol_uuid:", vol_uuid
             fun_test.test_assert(expression=vol_uuid, message="Create Volume Successful")
             volumes.append(vol_uuid)
 
         for counter in range(len(volumes)):
-            delete_vol_result = self.delete(fs_obj, volumes[counter])
+            delete_vol_result = delete(fs_obj, volumes[counter])
             print "delete_vol_result:", delete_vol_result
             fun_test.test_assert(expression=delete_vol_result, message="Delete Volume Successful")
 
@@ -262,8 +342,44 @@ class CCDD(RunStorageApiCommands):
         super(CCDD, self).cleanup()
 
 
+class CADtADt1(CADtADt):
+    def describe(self):
+        self.set_test_details(id=1,
+                              summary="create. attach-detach in loop, delete",
+                              steps='''
+                              ''')
+
+    def setup(self):
+        super(CADtADt1, self).setup()
+
+    def run(self):
+        pass  # Do not run FIO.
+        #super(CADtADt1, self).run()
+
+    def cleanup(self):
+        super(CADtADt1, self).cleanup()
+
+
+class CCDD1(CCDD):
+    def describe(self):
+        self.set_test_details(id=2,
+                              summary="create in loop, detach in loop",
+                              steps='''
+                              ''')
+
+    def setup(self):
+        super(CCDD1, self).setup()
+
+    def run(self):
+        pass  # Do not run FIO.
+        #super(CCDD1, self).run()
+
+    def cleanup(self):
+        super(CCDD1, self).cleanup()
+
+
 if __name__ == "__main__":
     setup_bringup = BootupSetup()
-    # setup_bringup.add_test_case(RunStorageApiCommands())
-    setup_bringup.add_test_case(CCDD())
+    setup_bringup.add_test_case(CADtADt1())
+    setup_bringup.add_test_case(CCDD1())
     setup_bringup.run()
