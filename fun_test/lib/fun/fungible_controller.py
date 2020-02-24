55dc457a452dbd97ae41bc1ef80e6d16817bf1dc
John Abraham <john.abraham@fungible.com>
2020-02-24 14:05:35 -0800

    fun controller is_ready_for_deploy

--- fun_test/lib/fun/fungible_controller.py
+++ fun_test/lib/fun/fungible_controller.py
@@ -3,13 +3,18 @@ from lib.host.linux import Linux
 
 
 class FungibleController(Linux):
-    pass
+    def __init__(self, **kwargs):
+        super(FungibleController, self).__init__(**kwargs)
+        self.initialized = False
 
     def initialize(self, reset=False):
         fun_test.log("Fungible controller: {} initialize".format(self))
+        self.initialized = True
 
     def is_ready_for_deploy(self):
         result = False
+        if not self.initialized:
+            self.initialize()
         return result
 
     def health(self, only_reachability=False):
@@ -18,5 +23,7 @@ class FungibleController(Linux):
         return result
 
     def deploy(self, dut_instances, already_deployed=False):
+        if not self.initialized:
+            self.initialize()
         fs_objs = dut_instances
         return True
