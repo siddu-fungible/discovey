0a949ef2193fc904f20604ec42e59860835a44ac
John Abraham <john.abraham@fungible.com>
2020-03-02 09:02:29 -0800

    rename release

--- fun_test/web/fun_test/management/rename_release.py
+++ fun_test/web/fun_test/management/rename_release.py
@@ -4,5 +4,5 @@ from web.fun_test.models import ReleaseCatalogExecution
 
 rcs = ReleaseCatalogExecution.objects.filter(description="FS1600 Weekly Regression")
 for rc in rcs:
-    rc.description = "FS1600 Daily regression"
+    rc.description = "FS1600 regression"
     rc.save()
