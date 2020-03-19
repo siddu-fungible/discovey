c2312d3e4e1b6128c79c95d36c0e1b71a8477aef
siddu bagewadi <siddu.bagewadi@fungible.com>
2020-03-19 12:35:44 +0530

    delete/detach, nvme driver changes

--- discovery_controller_scale.py
+++ discovery_controller_scale.py
@@ -1,5 +1,7 @@
 from lib.system.fun_test import *
 from lib.system import utils
+from lib.templates.storage.storage_operations_template_old import StorageControllerOperationsTemplate, HostsState
+
 fun_test.enable_storage_api()
 from lib.system import utils
 from lib.host.linux import Linux
@@ -23,25 +25,14 @@ class BringupSetup(FunTestScript):
         2. Configure Linux Host instance and make it available for test case
         """)
 
-    def setup(self):
+    def setup(self, host_obj=None, host_info=None):
         already_deployed = True
         topology_helper = TopologyHelper()
         self.topology = topology_helper.deploy(already_deployed=already_deployed)
         fun_test.test_assert(self.topology, "Topology deployed")
         fun_test.shared_variables["topology"] = self.topology
 
-
         self.topology = fun_test.shared_variables["topology"]
-        name = "blt_vol6"
-        #already_deployed = True
-        count = 0
-        vol_type = VolumeTypes().LOCAL_THIN
-        capacity = 160027797094
-        compression_effort = False
-        encrypt = False
-        body_volume_intent_create = BodyVolumeIntentCreate(name=name, vol_type=vol_type, capacity=capacity,
-                                                           compression_effort=compression_effort,
-                                                           encrypt=encrypt, data_protection={})
         self.storage_controller_template = BltVolumeOperationsTemplate(topology=self.topology)
         self.storage_controller_template.initialize(already_deployed=already_deployed)
 
@@ -59,112 +50,39 @@ class BringupSetup(FunTestScript):
         print "Bond interface dict is " + str(self.bond_interface_dict)
         self.fs_obj = fs_obj_list[0]
 
-        if 0:
-            vol_uuid_list = self.storage_controller_template.create_volume(fs_obj=fs_obj_list,
-                                                                           body_volume_intent_create=body_volume_intent_create)
-            fun_test.test_assert(expression=vol_uuid_list, message="Create Volume Successful")
-            hosts = self.topology.get_available_host_instances()
-            for index, fs_obj in enumerate(fs_obj_list):
-                attach_vol_result = self.storage_controller_template.attach_volume(host_obj=hosts, fs_obj=fs_obj,
-                                                                                   volume_uuid=vol_uuid_list[index],
-                                                                                   validate_nvme_connect=True,
-                                                                                   raw_api_call=True)
-                fun_test.test_assert(expression=attach_vol_result, message="Attach Volume Successful")
-
-                print "Attach volume result is " + str(attach_vol_result)
-
-            self.f1_index = self.bond_interface_dict[str(attach_vol_result[0]["data"]["ip"])]
         fun_test.shared_variables["fs_obj_list"] = fs_obj_list
-        #fun_test.shared_variables["f1_index"] = self.f1_index
+        # fun_test.shared_variables["f1_index"] = self.f1_index
         fun_test.shared_variables["storage_controller_template"] = self.storage_controller_template
-        
-        #NVMe driver check code
-        command_result = self.end_host.command("lsmod | grep -w nvme")
-        if "nvme" in command_result:
-            fun_test.log("nvme driver is loaded")
-        else:
-            fun_test.log("Loading nvme")
-            self.end_host.modprobe("nvme")
-            self.end_host.modprobe("nvme_core")
-        command_result = self.end_host.lsmod("nvme_tcp")
-        if "nvme_tcp" in command_result:
-            fun_test.log("nvme_tcp driver is loaded")
-        else:
-            fun_test.log("Loading nvme_tcp")
-            self.end_host.modprobe("nvme_tcp")
-            self.end_host.modprobe("nvme_fabrics")
 
+        # NVMe driver check code
+        for host_obj in self.topology.get_available_host_instances():
+            host_handle = host_obj.get_instance()
+            command_result = host_handle.sudo_command("lsmod | grep -w nvme")
+            if "nvme" in command_result:
+                fun_test.log("nvme driver is loaded")
+            else:
+                fun_test.log("Loading nvme")
+                host_handle.sudo_command.modprobe("nvme")
+                host_handle.sudo_command.modprobe("nvme_core")
+            command_result = host_handle.sudo_command("lsmod |grep -w nvme_tcp")
+            if "nvme_tcp" in command_result:
+                fun_test.log("nvme_tcp driver is loaded")
+            else:
+                fun_test.log("Loading nvme_tcp")
+                host_handle.sudo_command.modprobe("nvme_tcp")
+                host_handle.sudo_command.modprobe("nvme_fabrics")
 
     def cleanup(self):
         self.topology.cleanup()
-        #pass
-        
-         
-
-class VerifyProtocol(FunTestCase):
-    topology = None
-    storage_controller_template = None
-
-    def describe(self):
-        self.set_test_details(id=1,
-                              summary="Run all nvme identify commands",
-                              steps='''
-                              1. Make sure API server is up and running
-                              2. Run all nvme identify commands
-                              ''')
-
-    def setup(self):
-        self.topology = fun_test.shared_variables["topology"]
-        self.fs_obj = fun_test.shared_variables["fs_obj"]
-        self.f1_index = fun_test.shared_variables["f1_index"]
-        self.storage_controller_template = fun_test.shared_variables["storage_controller_template"]
-    
-
-    def getCtrlId(self,lnx,device):
-        op = json.loads(lnx.sudo_command("nvme -id-ctrl {DEVICE} -o=json".format(DEVICE=device)))
-        print "Output : " + str(op)
-        return op["cntlid"]
-
-    def getNvmSetId(self,lnx,ns):
-        op = json.loads(lnx.sudo_command("nvme -id-ns {NS} -o=json".format(NS=ns)))
-        print "Output : " + str(op)
-        return op["nvmsetid"]
-
-
-    def run(self):
-        hosts = self.topology.get_available_hosts()
-        bmc_handle = self.fs_obj.get_bmc()
-        # Get uart log file
-        uart_log_file = bmc_handle.get_f1_uart_log_file_name(self.f1_index)
-        print "Uart log file is " + uart_log_file
-
-        # Read uart log file
-        log_file_contents = bmc_handle.command("tail -n 2 " + str(uart_log_file))
-        #print "Log file contents " + str(log_file_contents)
-
-        # Get last timestamp logged
-        last_line_in_log_file = log_file_contents.split("\n")[-1]
-        last_timestamp = last_line_in_log_file.split(' ')[0]
-        uart_last_timestamp = last_timestamp.replace("[", '')
-        print "Uart last time stamp is " + str(uart_last_timestamp)
-
-
-
-        for host_id in hosts:
-            host_obj = hosts[host_id]
-            host_handle = host_obj.get_instance()
-            nvme_device_name = self.storage_controller_template.get_host_nvme_device(host_obj=host_obj)[0]
-            nvme_device_ns = nvme_device_name
-            nvme_device = nvme_device_name[0:-2]
-            nsid = nvme_device_name[-1:]
-
-
-
-    def cleanup(self):
-        self.storage_controller_template.cleanup()
+        # pass
 
 
 class GenericCreateAttachDetachDelete(FunTestCase):
+
+    def __init__(self, **kwargs):
+        super(GenericCreateAttachDetachDelete, self).__init__(**kwargs)
+        self.api_logging_level = None
+        self.NVME_HOST_MODULES = ["nvme_core", "nvme", "nvme_fabrics", "nvme_tcp"]
 
     def setup(self):
 
@@ -207,8 +125,8 @@ class GenericCreateAttachDetachDelete(FunTestCase):
         self.vol_type = VolumeTypes().LOCAL_THIN
         self.hosts = self.topology.get_available_host_instances()
 
-
     def run(self):
+        global count
         self.blt_count = self.blt_per_host
         for count in range(self.test_iteration_count):
             self.vol_uuid_list = []
@@ -220,62 +138,93 @@ class GenericCreateAttachDetachDelete(FunTestCase):
                                                                    encrypt=self.encrypt, data_protection={})
                 vol_uuid = self.blt_template.create_volume(self.fs_obj_list, body_volume_intent_create)
                 fun_test.test_assert(expression=vol_uuid and vol_uuid[0], message="Create Volume{} Successful with "
-                                                                                  "uuid {}".format(i+1, vol_uuid[0]))
+                                                                                  "uuid {}".format(i + 1, vol_uuid[0]))
                 self.vol_uuid_list.append(vol_uuid[0])
 
             # Do a nvme connect-all from the host
             for index, fs_obj in enumerate(self.fs_obj_list):
-                attach_vol_result = self.blt_template.attach_volume(host_obj=self.hosts, fs_obj=fs_obj,
-                                                                                   volume_uuid=self.vol_uuid_list[index],
-                                                                                   validate_nvme_connect=True, discover=True,
-                                                                                    raw_api_call=True)
-                fun_test.test_assert(expression=attach_vol_result, message="Attach Volume Successful")
+                self.attach_vol_result = self.blt_template.attach_volume(host_obj=self.hosts, fs_obj=fs_obj,
+                                                                         volume_uuid=self.vol_uuid_list[index],
+                                                                         validate_nvme_connect=True, discover=True,
+                                                                         raw_api_call=True)
+                fun_test.test_assert(expression=self.attach_vol_result, message="Attach Volume Successful")
 
-             #detaching volume before deleting
-             print "Detaching a volume before delete"
-             storage_controller = self.fs_obj_list[0].get_storage_controller()
-             port = attach_vol_result[0]["data"]["uuid"]
-             detach_vol_result = storage_controller.storage_api.delete_port(port_uuid=port)
-            
-             print "detach_vol_result:", detach_vol_result
-             fun_test.test_assert(expression=detach_vol_result,
-                                  message="Detach Volume {} Successful".format(vol_uuid_list[i][j]))
-             print' Deleting a volume after detaching it'
-             storage_controller = self.fs_obj_list[0].get_storage_controller()
-             delete_vol_result = storage_controller.storage_api.delete_volume(volume_uuid=vol_uuid_list[i][j])
-             print "delete_vol_result", delete_vol_result
-             fun_test.test_assert(expression=delete_vol_result,
-                                  message="Delete Volume {} Successful".format(vol_uuid_list[i][j]))
-
-            time.sleep(120)
-            #self.cleanupio()
-            fun_test.test_assert(expression=True, message="Test completed {} Iteration".format(count + 1))
-
+        time.sleep(120)
+        self.cleanupio()
+        fun_test.test_assert(expression=True, message="Test completed {} Iteration".format(count + 1))
 
     def cleanupio(self):
+        self.hosts_state_object = HostsState()
+        for host_obj in self.topology.get_available_host_instances():
+            host_handle = host_obj.get_instance()
+            host_handle.sudo_command("killall fio")
+            fun_test.add_checkpoint(checkpoint="Kill any running FIO processes")
+            disconnected_nvme_devices = []
+            host_namespaces = self.hosts_state_object.get_host_nvme_namespaces(hostname=host_obj.name)
+            for nvme_namespace in self.hosts_state_object.get_host_nvme_namespaces(hostname=host_obj.name):
+                nvme_device = self._get_nvme_device_namespace(namespace=nvme_namespace)
+                if nvme_device and nvme_device not in disconnected_nvme_devices:
+                    host_handle.nvme_disconnect(device=nvme_device)
+                    fun_test.add_checkpoint(checkpoint="Disconnect NVMe device: {} from host {}".
+                                            format(nvme_device, host_obj.name))
+                    disconnected_nvme_devices.append(nvme_device)
+
+            for driver in self.NVME_HOST_MODULES[::-1]:
+                host_handle.sudo_command("rmmod {}".format(driver))
+            fun_test.add_checkpoint(checkpoint="Unload all NVMe drivers")
+
+            for driver in self.NVME_HOST_MODULES:
+                host_handle.modprobe(driver)
+                fun_test.add_checkpoint(checkpoint="Reload all NVMe drivers")
+
         for dut_index in self.topology.get_available_duts().keys():
             fs_obj = self.topology.get_dut_instance(index=dut_index)
-            storage_controller = fs_obj.get_storage_controller()
+            storage_controller = fs_obj.get_storage_controller(api_logging_level=self.api_logging_level)
             # volumes = storage_controller.storage_api.get_volumes()
             # WORKAROUND : get_volumes errors out.
             raw_sc_api = StorageControllerApi(api_server_ip=storage_controller.target_ip)
             get_volume_result = raw_sc_api.get_volumes()
             fun_test.test_assert(message="Get Volume Details", expression=get_volume_result["status"])
             for volume in get_volume_result["data"]:
-                    delete_volume = None
+                for port in get_volume_result["data"][volume]["ports"]:
+                    detach_volume = None
                     try:
-                        delete_volume = storage_controller.storage_api.delete_volume(volume_uuid=volume)
+                        detach_volume = storage_controller.storage_api.delete_port(port_uuid=port)
                     except ApiException as e:
                         fun_test.critical("Exception while detaching volume: {}\n".format(e))
                     except Exception as e:
                         fun_test.critical("Exception while detaching volume: {}\n".format(e))
-                    delete_volume_result = False
-                    if delete_volume:
-                        delete_volume_result = delete_volume.status
-                    fun_test.test_assert(expression=delete_volume.status, message="Delete Volume {}".format(volume))
-
+                    detach_result = False
+                    if detach_volume:
+                        detach_result = detach_volume.status
+                    fun_test.add_checkpoint(expected=True, actual=detach_result,
+                                            checkpoint="Detach Volume {} from host with host_nqn {}".format(
+                                                volume,
+                                                get_volume_result["data"][volume]['ports'][port]['host_nqn']))
+                delete_volume = None
+                try:
+                    delete_volume = storage_controller.storage_api.delete_volume(volume_uuid=volume)
+                except ApiException as e:
+                    fun_test.critical("Exception while detaching volume: {}\n".format(e))
+                except Exception as e:
+                    fun_test.critical("Exception while detaching volume: {}\n".format(e))
+                delete_volume_result = False
+                if delete_volume:
+                    delete_volume_result = delete_volume.status
+                fun_test.add_checkpoint(expected=True, actual=delete_volume_result,
+                                        checkpoint="Delete Volume {}".format(volume))
     def cleanup(self):
         fun_test.shared_variables["storage_controller_template"] = self.blt_template
+
+    def _get_nvme_device_namespace(self, namespace):
+        result = None
+        if "/dev/" in namespace:
+            nvme_device = namespace[:-2].split('/dev/')[1]
+        else:
+            nvme_device = namespace[:-2]
+        if nvme_device.split("nvme")[0] == '' and nvme_device.split("nvme")[1].isdigit():
+            result = nvme_device
+        return result
 
 
 class ConnectAllMaxVolumeFromOneHost(GenericCreateAttachDetachDelete):
@@ -318,6 +267,7 @@ class ConnectAllMaxHosts(GenericCreateAttachDetachDelete):
         def cleanup(self):
             super(ConnectAllMaxHosts, self).cleanup()
 
+
 class ConnectAllContinuous(GenericCreateAttachDetachDelete):
     def describe(self):
         self.set_test_details(id=3,
@@ -340,6 +290,7 @@ class ConnectAllContinuous(GenericCreateAttachDetachDelete):
 
         def cleanup(self):
             super(ConnectAllContinuous, self).cleanup()
+
 
 class ConnectAllContinuousMultiHost(GenericCreateAttachDetachDelete):
     def describe(self):
@@ -369,13 +320,13 @@ class ConnectAllContinuousMultiHost(GenericCreateAttachDetachDelete):
         def cleanup(self):
             super(ConnectAllContinuous, self).cleanup()
 
+
 if __name__ == "__main__":
     setup_bringup = BringupSetup()
     setup_bringup.add_test_case(ConnectAllMaxVolumeFromOneHost())
     # TO be done
-    #setup_bringup.add_test_case(ConnectAllMaxHosts())
-    #setup_bringup.add_test_case(ConnectAllContinuous())
-    #setup_bringup.add_test_case(ConnectAllContinuousMultiHost())
-
+    # setup_bringup.add_test_case(ConnectAllMaxHosts())
+    # setup_bringup.add_test_case(ConnectAllContinuous())
+    # setup_bringup.add_test_case(ConnectAllContinuousMultiHost())
 
     setup_bringup.run()
